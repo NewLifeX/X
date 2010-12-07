7048845f990b59e057d48f103d5bac250284988a
nnhy <nnhy@vip.qq.com>
2010-12-07 09:58:45 +0000

    添加架构设计图

---
+++ NewLife.Core/Messaging/NullMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewLife.Messaging
+{
+    /// <summary>
+    /// 空消息
+    /// </summary>
+    public class NullMessage : Message
+    {
+        /// <summary>
+        /// 消息编号
+        /// </summary>
+        public override int ID
+        {
+            get { return 0xFE; }
+        }
+    }
+}
