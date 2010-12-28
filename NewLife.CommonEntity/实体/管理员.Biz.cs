7f0b993851cc579d431c9d3bdcd660e4ba5cea21
xi3892 <xi3892@163.com>
2010-12-28 11:51:33 +0000

    修改查询

--- NewLife.CommonEntity/实体/管理员.Biz.cs
+++ NewLife.CommonEntity/实体/管理员.Biz.cs
@@ -6,6 +6,8 @@ using NewLife.Log;
 using NewLife.Security;
 using NewLife.Web;
 using XCode;
+using System.Text;
+using XCode.Configuration;
 
 namespace NewLife.CommonEntity
 {
@@ -409,6 +411,74 @@ namespace NewLife.CommonEntity
         {
             return Meta.Cache.Entities.FindAll(_.SSOUserID, id);
         }
+
+        /// <summary>
+        /// 查询满足条件的记录集，分页、排序
+        /// </summary>
+        /// <param name="key">关键字</param>
+        /// <param name="orderClause">排序，不带Order By</param>
+        /// <param name="startRowIndex">开始行，0开始</param>
+        /// <param name="maximumRows">最大返回行数</param>
+        /// <returns>实体集</returns>
+        [DataObjectMethod(DataObjectMethodType.Select, true)]
+        public static EntityList<TEntity> Search(String key, String orderClause, Int32 startRowIndex, Int32 maximumRows)
+        {
+            return FindAll(SearchWhere(key), orderClause, null, startRowIndex, maximumRows);
+        }
+
+        /// <summary>
+        /// 查询满足条件的记录总数，分页和排序无效，带参数是因为ObjectDataSource要求它跟Search统一
+        /// </summary>
+        /// <param name="key">关键字</param>
+        /// <param name="orderClause">排序，不带Order By</param>
+        /// <param name="startRowIndex">开始行，0开始</param>
+        /// <param name="maximumRows">最大返回行数</param>
+        /// <returns>记录数</returns>
+        public static Int32 SearchCount(String key, String orderClause, Int32 startRowIndex, Int32 maximumRows)
+        {
+            return FindCount(SearchWhere(key), null, null, 0, 0);
+        }
+
+        /// <summary>
+        /// 构造搜索条件
+        /// </summary>
+        /// <param name="key">关键字</param>
+        /// <returns></returns>
+        private static String SearchWhere(String key)
+        {
+            if (String.IsNullOrEmpty(key)) return null;
+            key = key.Replace("'", "''");
+            String[] keys = key.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("1=1");
+
+            //if (!String.IsNullOrEmpty(name)) sb.AppendFormat(" And {0} like '%{1}%'", _.Name, name.Replace("'", "''"));
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                sb.Append(" And ");
+
+                if (keys.Length > 1) sb.Append("(");
+                Int32 n = 0;
+                foreach (FieldItem item in Meta.Fields)
+                {
+                    if (item.Property.PropertyType != typeof(String)) continue;
+                    // 只要前五项
+                    if (++n > 10) break;
+
+                    if (n > 1) sb.Append(" Or ");
+                    sb.AppendFormat("{0} like '%{1}%'", item.Name, keys[i]);
+                }
+                if (keys.Length > 1) sb.Append(")");
+            }
+
+            if (sb.Length == "1=1".Length)
+                return null;
+            else
+                return sb.ToString();
+        }
+
         #endregion
 
         #region 扩展操作
