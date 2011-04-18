8b509eaf4528d9c170aa3aa6aafd993c83fb8dd2
whc888888 <whc888888@gmail.com>
2011-04-18 08:57:29 +0000

    必须填写至少10个字的日志

---
+++ NewLife.PeerToPeer/Messages/CommandMessage/ResetCommandClientMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using NewLife.Messaging;
+
+namespace NewLife.PeerToPeer.Messages
+{
+    /// <summary>
+    /// 重启客户端
+    /// </summary>
+    public class ResetCommandClientMessage : CommandMessageBase<ResetCommandClientMessage>
+    {
+        #region 属性
+        /// <summary>消息类型</summary>
+        public override MessageTypes MessageType { get { return MessageTypes.ResetCommandClient; } }
+
+        #endregion
+
+        #region 响应
+        /// <summary>
+        /// 响应
+        /// </summary>
+        public class Response : CommandMessageBase<Response>
+        {
+            /// <summary>消息类型</summary>
+            public override MessageTypes MessageType { get { return MessageTypes.ResetCommandClientResponse; } }
+
+        }
+        #endregion
+    }
+}
