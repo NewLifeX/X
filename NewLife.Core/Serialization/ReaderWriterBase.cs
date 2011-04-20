b787ca187c9686748f476ccece62c0ae62e89da7
nnhy <nnhy@vip.qq.com>
2011-04-20 00:36:53 +0000

    获取属性或字段，未完成

--- NewLife.Core/Serialization/ReaderWriterBase.cs
+++ NewLife.Core/Serialization/ReaderWriterBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
 using System.ComponentModel;
+using NewLife.Collections;
 
 namespace NewLife.Serialization
 {
@@ -88,6 +89,73 @@ namespace NewLife.Serialization
         /// 获取指定类型中需要序列化的成员时触发。使用者可以修改、排序要序列化的成员。
         /// </summary>
         public event EventHandler<EventArgs<Type, MemberInfo[]>> OnGotMembers;
+
+        static DictionaryCache<Type, MemberInfo[]> cache1 = new DictionaryCache<Type, MemberInfo[]>();
+        /// <summary>
+        /// 取得所有字段
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        internal protected static MemberInfo[] FindFields(Type type)
+        {
+            if (type == null) return null;
+
+            return cache1.GetItem(type, delegate(Type t)
+            {
+                List<MemberInfo> list = new List<MemberInfo>();
+
+                // GetFields只能取得本类的字段，没办法取得基类的字段
+                FieldInfo[] fis = t.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                if (fis != null && fis.Length > 0)
+                {
+                    foreach (FieldInfo item in fis)
+                    {
+                        list.Add(item);
+                    }
+                }
+
+                // 递归取父级的字段
+                if (type.BaseType != null && type.BaseType != typeof(Object))
+                {
+                    MemberInfo[] fis2 = FindFields(type.BaseType);
+                    //if (fis2 != null) list.AddRange(fis2);
+                    if (fis2 != null)
+                    {
+                        List<MemberInfo> list2 = new List<MemberInfo>(fis2);
+                        list2.AddRange(list);
+                        list = list2;
+                    }
+                }
+
+                if (list == null || list.Count < 1) return null;
+                return list.ToArray();
+            });
+        }
+
+        static DictionaryCache<Type, MemberInfo[]> cache2 = new DictionaryCache<Type, MemberInfo[]>();
+        /// <summary>
+        /// 取得所有属性
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        internal protected static MemberInfo[] FindProperties(Type type)
+        {
+            if (type == null) return null;
+
+            return cache2.GetItem(type, delegate(Type t)
+            {
+                MemberInfo[] pis = t.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                if (pis == null || pis.Length < 1) return null;
+
+                List<MemberInfo> list = new List<MemberInfo>();
+                foreach (MemberInfo item in pis)
+                {
+                    list.Add(item);
+                }
+                if (list == null || list.Count < 1) return null;
+                return list.ToArray();
+            });
+        }
 
         /// <summary>
         /// 过滤掉具有指定特性的成员
