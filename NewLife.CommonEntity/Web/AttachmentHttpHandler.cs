0343f65d4abafb5e840fe1f2159ad41506b257c5
nnhy <nnhy@vip.qq.com>
2011-05-11 10:08:14 +0000

    附件下载、图片显示等功能

---
+++ NewLife.CommonEntity/Web/AttachmentHttpHandler.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Caching;
+using NewLife.Log;
+
+namespace NewLife.CommonEntity.Web
+{
+    /// <summary>
+    /// 附件处理器
+    /// </summary>
+    public class AttachmentHttpHandler : IHttpHandler
+    {
+        #region IHttpHandler 成员
+        bool IHttpHandler.IsReusable
+        {
+            get { return false; }
+        }
+
+        void IHttpHandler.ProcessRequest(HttpContext context)
+        {
+            OnProcess(context);
+        }
+        #endregion
+
+        #region 属性
+        /// <summary>
+        /// 文件编号
+        /// </summary>
+        public Int32 ID
+        {
+            get
+            {
+                String str = HttpContext.Current.Request["ID"];
+                if (String.IsNullOrEmpty(str)) return 0;
+
+                Int32 n = 0;
+                if (!Int32.TryParse(str, out n)) n = 0;
+
+                return n;
+            }
+        }
+
+        //private Attachment _Attachment;
+        ///// <summary>附件</summary>
+        //public Attachment Attachment
+        //{
+        //    get
+        //    {
+        //        if (_Attachment == null && ID > 0)
+        //        {
+        //            _Attachment = Attachment.FindByID(ID);
+        //        }
+        //        return _Attachment;
+        //    }
+        //}
+        #endregion
+
+        #region 文件流
+        /// <summary>
+        /// 获取文件数据流。这里可以实现小文件缓存进入内容以减少磁盘IO
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="attachment"></param>
+        /// <returns></returns>
+        protected virtual Stream GetStream(HttpContext context, Attachment attachment)
+        {
+            if (attachment.Size > MaxFileSize) return new FileStream(attachment.FullFilePath, FileMode.Open, FileAccess.Read);
+
+            String key = "NewLife.Attachment.Cache_" + attachment.ID;
+            Stream stream = HttpRuntime.Cache[key] as Stream;
+            if (stream != null) return stream;
+
+            FileStream fs = new FileStream(attachment.FullFilePath, FileMode.Open, FileAccess.Read);
+            // 再次判断，防止记录的文件大小不正确而导致系统占用大量内存
+            if (fs.Length > MaxFileSize) return fs;
+
+            Byte[] buffer = new Byte[fs.Length];
+            fs.Read(buffer, 0, buffer.Length);
+            fs.Close();
+
+            stream = new MemoryStream(buffer);
+            HttpRuntime.Cache.Insert(key, stream, null, Cache.NoAbsoluteExpiration, CacheTime);
+            return stream;
+        }
+
+        /// <summary>
+        /// 小于指定大小才缓存文件
+        /// </summary>
+        protected virtual Int64 MaxFileSize { get { return 100 * 1024; } }
+
+        /// <summary>
+        /// 小文件缓存时间，默认10分钟
+        /// </summary>
+        /// <returns></returns>
+        protected virtual TimeSpan CacheTime { get { return new TimeSpan(0, 10, 0); } }
+        #endregion
+
+        #region 方法
+        /// <summary>
+        /// 处理
+        /// </summary>
+        /// <param name="context"></param>
+        protected virtual void OnProcess(HttpContext context)
+        {
+            Attachment attachment = GetAttachment();
+
+            if (attachment == null || !attachment.IsEnable || !File.Exists(attachment.FullFilePath))
+            {
+                OnNotFound(context);
+                return;
+            }
+
+            // 增加统计
+            attachment.Increment(null);
+
+            Stream stream = GetStream(context, attachment);
+            try
+            {
+                OnResponse(context, attachment, stream, null);
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                // 关闭文件流
+                if (stream is FileStream) stream.Close();
+            }
+        }
+
+        /// <summary>
+        /// 取得附件对象
+        /// </summary>
+        /// <returns></returns>
+        protected virtual Attachment GetAttachment()
+        {
+            if (ID > 0)
+                return Attachment.FindByID(ID);
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// 没找到附件
+        /// </summary>
+        /// <param name="context"></param>
+        protected virtual void OnNotFound(HttpContext context)
+        {
+            HttpResponse Response = context.Response;
+
+            Response.StatusCode = 400;
+            Response.Status = "文件未找到！";
+
+        }
+
+        /// <summary>
+        /// 响应
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="attachment"></param>
+        /// <param name="stream"></param>
+        /// <param name="dispositionMode"></param>
+        protected virtual void OnResponse(HttpContext context, Attachment attachment, Stream stream, String dispositionMode)
+        {
+            HttpRequest Request = context.Request;
+            HttpResponse Response = context.Response;
+
+            Response.Buffer = false;
+            long fileLength = stream.Length;
+
+            int pack = 1024000;
+
+            Response.AddHeader("Content-Length", fileLength.ToString());
+            if (!String.IsNullOrEmpty(attachment.ContentType)) Response.ContentType = attachment.ContentType;
+            if (String.IsNullOrEmpty(dispositionMode)) dispositionMode = "inline"; // attachment/inline
+            Response.AddHeader("Content-Disposition", dispositionMode + "; filename=" + HttpUtility.UrlEncode(attachment.FileName, Encoding.UTF8));
+
+            Byte[] buffer = new Byte[pack];
+            while (true)
+            {
+                if (Response.IsClientConnected) break;
+
+                Int32 count = stream.Read(buffer, 0, buffer.Length);
+                if (count <= 0) break;
+
+                if (count == pack)
+                    Response.BinaryWrite(buffer);
+                else
+                {
+                    Byte[] data = new Byte[count];
+                    Buffer.BlockCopy(buffer, 0, data, 0, count);
+                    Response.BinaryWrite(data);
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ NewLife.CommonEntity/Web/ShowPicture.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Reflection;
+
+namespace NewLife.CommonEntity.Web
+{
+    /// <summary>
+    /// 显示图片
+    /// </summary>
+    public class ShowPicture : AttachmentHttpHandler
+    {
+        #region 属性
+        private Int32 _Width = 320;
+        /// <summary>宽度</summary>
+        public virtual Int32 Width
+        {
+            get { return _Width; }
+            set { _Width = value; }
+        }
+
+        private Int32 _Height = 240;
+        /// <summary>高度</summary>
+        public virtual Int32 Height
+        {
+            get { return _Height; }
+            set { _Height = value; }
+        }
+        #endregion
+
+        ///// <summary>
+        ///// 无法取得附件对象时，使用默认附件
+        ///// </summary>
+        ///// <returns></returns>
+        //protected override Attachment GetAttachment()
+        //{
+        //    Attachment entity = base.GetAttachment();
+        //    if (entity != null && entity.IsEnable) return entity;
+
+        //    entity = new Attachment();
+        //    entity.FileName = "NoPic";
+        //    entity.ContentType = "image/jpeg";
+
+        //    entity.FilePath = GetNoPic();
+
+        //    //读取资源
+        //    Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("NewLife.CommonEntity.Web.nopic.jpg");
+
+        //    return entity;
+        //}
+
+        static Stream nopic = null;
+        /// <summary>
+        /// 未找到
+        /// </summary>
+        /// <param name="context"></param>
+        protected override void OnNotFound(HttpContext context)
+        {
+            //读取资源
+            if (nopic == null) nopic = Assembly.GetExecutingAssembly().GetManifestResourceStream("NewLife.CommonEntity.Web.nopic.jpg");
+
+            Attachment entity = new Attachment();
+            entity.FileName = "NoPic";
+            entity.ContentType = "image/jpeg";
+
+            OnResponse(context, entity, nopic, null);
+        }
+
+        /// <summary>
+        /// 取得文件流，判断是否小图片，特殊处理
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="attachment"></param>
+        /// <returns></returns>
+        protected override Stream GetStream(HttpContext context, Attachment attachment)
+        {
+            Stream stream = base.GetStream(context, attachment);
+
+            HttpRequest Request = context.Request;
+            String type = Request["Type"];
+            if (String.IsNullOrEmpty(type)) type = Request["t"];
+            if (!String.IsNullOrEmpty(type)) type = type.ToLower();
+            // 小图片
+            if (type == "small" || type == "s")
+            {
+                Int32 n = 0;
+                if (Int32.TryParse(Request["Width"], out n)) Width = n;
+                if (Int32.TryParse(Request["w"], out n)) Width = n;
+                if (Int32.TryParse(Request["Height"], out n)) Height = n;
+                if (Int32.TryParse(Request["h"], out n)) Height = n;
+
+                stream = MakeThumbnail(stream, Width, Height, "Cut");
+            }
+
+            return stream;
+        }
+
+        /// <summary>
+        /// 生成缩略图
+        /// </summary>
+        /// <param name="stream">源图</param>
+        /// <param name="width">缩略图宽度</param>
+        /// <param name="height">缩略图高度</param>
+        /// <param name="mode">生成缩略图的方式</param>    
+        /// <returns>缩略图</returns>
+        public static Stream MakeThumbnail(Stream stream, int width, int height, string mode)
+        {
+            Image originalImage = Image.FromStream(stream);
+
+            int towidth = width;
+            int toheight = height;
+
+            int x = 0;
+            int y = 0;
+            int ow = originalImage.Width;
+            int oh = originalImage.Height;
+
+            switch (mode)
+            {
+                case "HW"://指定高宽缩放（可能变形）                
+                    break;
+                case "W"://指定宽，高按比例                    
+                    toheight = originalImage.Height * width / originalImage.Width;
+                    break;
+                case "H"://指定高，宽按比例
+                    towidth = originalImage.Width * height / originalImage.Height;
+                    break;
+                case "Cut"://指定高宽裁减（不变形）                
+                    if ((double)originalImage.Width / (double)originalImage.Height > (double)towidth / (double)toheight)
+                    {
+                        oh = originalImage.Height;
+                        ow = originalImage.Height * towidth / toheight;
+                        y = 0;
+                        x = (originalImage.Width - ow) / 2;
+                    }
+                    else
+                    {
+                        ow = originalImage.Width;
+                        oh = originalImage.Width * height / towidth;
+                        x = 0;
+                        y = (originalImage.Height - oh) / 2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            // 是否有必要做缩略图
+            if (towidth == width && toheight == height)
+            {
+                originalImage.Dispose();
+                return stream;
+            }
+
+            //新建一个bmp图片
+            Image bitmap = new Bitmap(towidth, toheight);
+
+            //新建一个画板
+            Graphics g = Graphics.FromImage(bitmap);
+
+            //设置高质量插值法
+            g.InterpolationMode = InterpolationMode.High;
+
+            //设置高质量,低速度呈现平滑程度
+            g.SmoothingMode = SmoothingMode.HighQuality;
+
+            //清空画布并以透明背景色填充
+            g.Clear(Color.Transparent);
+
+            //在指定位置并且按指定大小绘制原图片的指定部分
+            g.DrawImage(originalImage, new Rectangle(0, 0, towidth, toheight), new Rectangle(x, y, ow, oh), GraphicsUnit.Pixel);
+
+            try
+            {
+                //以jpg格式保存缩略图
+                MemoryStream ms = new MemoryStream();
+                bitmap.Save(ms, ImageFormat.Jpeg);
+                return ms;
+            }
+            finally
+            {
+                originalImage.Dispose();
+                bitmap.Dispose();
+                g.Dispose();
+            }
+        }
+    }
+}
