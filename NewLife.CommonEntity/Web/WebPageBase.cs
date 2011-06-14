26078ab1906937e0b74b39b7a3573f6ccd9ff2ba
netwjx <netwjx@gmail.com>
2011-06-14 01:48:06 +0000

    修改系统初次加载时显示页面进度条

--- NewLife.CommonEntity/Web/WebPageBase.cs
+++ NewLife.CommonEntity/Web/WebPageBase.cs
@@ -451,20 +451,41 @@ namespace NewLife.CommonEntity.Web
 
             #region 输出脚本
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine("<html><head>");
-            sb.AppendLine("<script language=JavaScript type=text/javascript>");
-            sb.AppendLine("var t_id = setInterval(animate,20);");
-            sb.AppendLine("var pos=0;var dir=2;var len=0;");
-            sb.AppendLine("function animate(){");
-            sb.AppendLine("var elem = document.getElementById('progress');");
-            sb.AppendLine("if(elem != null) {");
-            sb.AppendLine("if (pos==0) len += dir;");
-            sb.AppendLine("if (len>32 || pos>79) pos += dir;");
-            sb.AppendLine("if (pos>79) len -= dir;");
-            sb.AppendLine("if (pos>79 && len==0) pos=0;");
-            sb.AppendLine("elem.style.left = pos;");
-            sb.AppendLine("elem.style.width = len;");
-            sb.AppendLine("}");
+            sb.Append(@"<html><head>
+<script language=JavaScript type=text/javascript>
+var t_id = setInterval(animate,20);
+var pos=0;var dir=2;var len=0;
+
+function animate(){
+    var elem = document.getElementById('progress');
+    if(elem != null) {
+    if (pos==0) len += dir;
+    if (len>32 || pos>79) pos += dir;
+    if (pos>79) len -= dir;
+    if (pos>79 && len==0) pos=0;
+    elem.style.left = pos;
+    elem.style.width = len;
+}
+</script>
+
+<style>
+#loader_container {text-align:center; position:absolute; top:40%; width:100%; left: 0;}
+#loader {font-family:Tahoma, Helvetica, sans; font-size:11.5px; color:#000000; background-color:#FFFFFF; padding:10px 0 16px 0; margin:0 auto; display:block; width:130px; border:1px solid #5a667b; text-align:left; z-index:2;}
+#progress {height:5px; font-size:1px; width:1px; position:relative; top:1px; left:0px; background-color:#8894a8;}
+#loader_bg {background-color:#e4e7eb; position:relative; top:8px; left:8px; height:7px; width:113px; font-size:1px;}
+</style>
+
+</head><body>
+<div id=loader_container>
+    <div id=loader>
+    <div align=center>系统正在启动中 ...</div>
+    <div id=loader_bg><div id=progress> </div></div>
+    </div>
+    <iframe src='?iframe=1' style='display:none' onload='location.reload()'></iframe>
+</div>
+</body></html>");
+            //还可以选择使用<script src="?script=1" type="text/javascript"></script>的方式 只是对返回内容有要求
+
             //sb.AppendLine("function remove_loading() {");
             //sb.AppendLine(" this.clearInterval(t_id);");
             //sb.AppendLine("var targelem = document.getElementById('loader_container');");
@@ -473,20 +494,6 @@ namespace NewLife.CommonEntity.Web
             //sb.AppendLine("}");
             //sb.AppendLine("document.onload=function(){ location.reload(); }");
             //sb.AppendLine("document.onload=remove_loading;");
-            sb.AppendLine("</script>");
-            sb.AppendLine("<style>");
-            sb.AppendLine("#loader_container {text-align:center; position:absolute; top:40%; width:100%; left: 0;}");
-            sb.AppendLine("#loader {font-family:Tahoma, Helvetica, sans; font-size:11.5px; color:#000000; background-color:#FFFFFF; padding:10px 0 16px 0; margin:0 auto; display:block; width:130px; border:1px solid #5a667b; text-align:left; z-index:2;}");
-            sb.AppendLine("#progress {height:5px; font-size:1px; width:1px; position:relative; top:1px; left:0px; background-color:#8894a8;}");
-            sb.AppendLine("#loader_bg {background-color:#e4e7eb; position:relative; top:8px; left:8px; height:7px; width:113px; font-size:1px;}");
-            sb.AppendLine("</style>");
-            sb.AppendLine("</head><body onload='location.reload();'>");
-            sb.AppendLine("<div id=loader_container>");
-            sb.AppendLine("<div id=loader>");
-            sb.AppendLine("<div align=center>系统正在启动中 ...</div>");
-            sb.AppendLine("<div id=loader_bg><div id=progress> </div></div>");
-            sb.AppendLine("</div></div>");
-            sb.AppendLine("</body></html>");
 
             Response.Write(sb.ToString());
             Response.Flush();
