6f6ea13c02b9e08c37188e20323078381b78b132
nnhy <nnhy@vip.qq.com>
2011-08-29 06:05:55 +0000

    新生命进程助手第一个版本

---
+++ NewLife.Update/UpdateHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace NewLife.Update
+{
+    /// <summary>
+    /// 更新处理器
+    /// </summary>
+    public class UpdateHandler : IHttpHandler
+    {
+        #region IHttpHandler 成员
+
+        public bool IsReusable
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
