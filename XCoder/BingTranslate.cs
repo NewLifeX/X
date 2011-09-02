d279e5887811fac170653f1d14fff952b497da5a
nnhy <nnhy@vip.qq.com>
2011-09-02 08:14:36 +0000

    增加自动在线翻译功能，调用Bing翻译

--- XCoder/BingTranslate.cs
+++ XCoder/BingTranslate.cs
@@ -8,18 +8,21 @@ namespace XCoder
     class BingTranslate : ITranslate
     {
         String appId = "A63C992CE2E44371559E8385947BBCDE758F7B10";
+        String url = "http://api.microsofttranslator.com/V2/soap.svc";
 
         #region ITranslate 成员
 
         public string Translate(string word)
         {
             SoapService client = new SoapService();
+            client.Url = url;
             return client.Translate(appId, word, "en", "cn", "text/plain", "general");
         }
 
         public string[] Translate(string[] words)
         {
             SoapService client = new SoapService();
+            client.Url = url;
             TranslateOptions options = new TranslateOptions();
             TranslateArrayResponse[] rs = client.TranslateArray(appId, words, "en", "zh-cn", options);
             if (rs == null || rs.Length < 1) return null;
