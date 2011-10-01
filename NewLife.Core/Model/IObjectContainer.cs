318fb30591409c91c70126cf47b0f8d66c0fc656
nnhy <nnhy@vip.qq.com>
2011-10-01 15:36:39 +0000

    对象容器IObjectContainer

--- NewLife.Core/Model/IObjectContainer.cs
+++ NewLife.Core/Model/IObjectContainer.cs
@@ -5,7 +5,28 @@ using System.Text;
 namespace NewLife.Model
 {
     /// <summary>对象容器</summary>
-    public interface IObjectContainer
+    interface IObjectContainer
     {
+        IObjectContainer Parent { get; }
+
+        IObjectContainer RegisterType(Type type);
+        IObjectContainer RegisterType(Type type, String name);
+        IObjectContainer RegisterType<T>();
+        IObjectContainer RegisterType<T>(String name);
+        IObjectContainer RegisterInstance(Type type, Object instance);
+        IObjectContainer RegisterInstance(Type type, String name, Object instance);
+        IObjectContainer RegisterInstance<TInterface>(Object instance);
+        IObjectContainer RegisterInstance<TInterface>(String name, Object instance);
+
+        Object Resolve(Type type);
+        Object Resolve(Type type, String name);
+        T Resolve<T>();
+        T Resolve<T>(String name);
+
+        IEnumerable<Object> ResolveAll(Type type);
+        IEnumerable<T> ResolveAll<T>();
+
+        IObjectContainer RemoveAllChildContainers();
+        IObjectContainer CreateChildContainer();
     }
 }
