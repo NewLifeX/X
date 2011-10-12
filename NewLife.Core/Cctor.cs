e7b9d27dc8bbef7906310b50756a76d7e64ead98
nnhy <nnhy@vip.qq.com>
2011-10-12 09:59:36 +0000

    增加编译后事件，自动生成模块静态构造函数

---
+++ NewLife.Core/Cctor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewLife
+{
+    class Cctor
+    {
+        static void Init()
+        {
+        }
+
+        static void Finish()
+        {
+        }
+    }
+}
