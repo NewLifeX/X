e021bf0e6b9fc678f559e749db43725b5ee32043
nnhy <nnhy@vip.qq.com>
2011-10-19 02:00:36 +0000

    v2.1.2011.1019   项目类型由类库修改为控制台项目，不仅可供其它程序集引用，也可直接作为服务代理，承载各种实现IServer接口的服务

---
+++ XAgent/AgentService.cs
@@ -0,0 +1,53 @@
+﻿using XAgent;
+using System.Diagnostics;
+using System;
+
+namespace XAgent
+{
+    public class AgentService : AgentServiceBase<AgentService>
+    {
+        #region 属性
+        public override int ThreadCount
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public override string DisplayName
+        {
+            get
+            {
+                return "测试服务";
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "这是一个用于测试XAgent的服务！";
+            }
+        }
+        #endregion
+
+        #region 构造函数
+        public AgentService()
+        {
+            // 一般在构造函数里面指定服务名
+            ServiceName = "XAgentTest";
+        }
+        #endregion
+
+        #region 核心
+        public override bool Work(int index)
+        {
+            // XAgent讲开启ThreadCount个线程，0<index<ThreadCount，本函数即为每个任务线程的主函数，间隔Interval循环调用
+            WriteLine("任务{0}，当前时间：{1}", index, DateTime.Now);
+
+            return false;
+        }
+        #endregion
+    }
+}
--- XAgent/Properties/AssemblyInfo.cs
+++ XAgent/Properties/AssemblyInfo.cs
@@ -32,10 +32,12 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.*")]
-[assembly: AssemblyFileVersion("2.0.2011.0720")]
+[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyFileVersion("2.1.2011.1019")]
 
 /*
+ * v2.1.2011.1019   项目类型由类库修改为控制台项目，不仅可供其它程序集引用，也可直接作为服务代理，承载各种实现IServer接口的服务
+ * 
  * v2.0.2011.0720   可以通过设置任务的时间间隔小于0来关闭指定任务
  *                  可以通过配置文件指定服务名、显示名、描述等信息
  *                  可以通过XAgent.AttachServers设置附加代理服务，任何XAgent服务程序都可以附加代理实现了IServer接口的服务
