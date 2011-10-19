bc229b28bc4c4c9c1d406e0a37ef2290632676e1
nnhy <nnhy@vip.qq.com>
2011-10-19 02:13:33 +0000

    v2.1.2011.1019   项目类型由类库修改为控制台项目，不仅可供其它程序集引用，也可直接作为服务代理，承载各种实现IServer接口的服务

--- XAgent/AgentService.cs
+++ XAgent/AgentService.cs
@@ -4,47 +4,41 @@ using System;
 
 namespace XAgent
 {
+    /// <summary>
+    /// 代理服务例子。自定义服务程序可参照该类实现。
+    /// </summary>
     public class AgentService : AgentServiceBase<AgentService>
     {
         #region 属性
-        public override int ThreadCount
-        {
-            get
-            {
-                return 2;
-            }
-        }
+        /// <summary>线程数</summary>
+        public override int ThreadCount { get { return 2; } }
 
-        public override string DisplayName
-        {
-            get
-            {
-                return "测试服务";
-            }
-        }
+        /// <summary>显示名</summary>
+        public override string DisplayName { get { return "新生命服务代理"; } }
 
-        public override string Description
-        {
-            get
-            {
-                return "这是一个用于测试XAgent的服务！";
-            }
-        }
+        /// <summary>描述</summary>
+        public override string Description { get { return "用于承载各种服务的服务代理！"; } }
         #endregion
 
         #region 构造函数
+        /// <summary>实例化一个代理服务</summary>
         public AgentService()
         {
             // 一般在构造函数里面指定服务名
-            ServiceName = "XAgentTest";
+            ServiceName = "XAgent";
         }
         #endregion
 
         #region 核心
+        /// <summary>
+        /// 核心工作方法。调度线程会定期调用该方法
+        /// </summary>
+        /// <param name="index">线程序号</param>
+        /// <returns>是否立即开始下一步工作。某些任务能达到满负荷，线程可以不做等待</returns>
         public override bool Work(int index)
         {
             // XAgent讲开启ThreadCount个线程，0<index<ThreadCount，本函数即为每个任务线程的主函数，间隔Interval循环调用
-            WriteLine("任务{0}，当前时间：{1}", index, DateTime.Now);
+            //WriteLine("任务{0}，当前时间：{1}", index, DateTime.Now);
 
             return false;
         }
