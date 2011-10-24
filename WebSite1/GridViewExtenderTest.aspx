56c0e48732a1bb72833da3c2f6a7f821efa1c886
netwjx <netwjx@gmail.com>
2011-10-24 02:38:43 +0000

    将GridViewExtenderTest测试页面添加到版本库 修改GridViewExtender.js 部分按照jslint的规则修改

---
+++ WebSite1/GridViewExtenderTest.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GridViewExtenderTest.aspx.cs"
+    Inherits="GridViewExtenderTest" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" AllowPaging="True"
+        PageSize="5" DataKeyNames="ID" DataSourceID="ObjectDataSource1" 
+        EnableModelValidation="True">
+        <Columns>
+            <asp:BoundField DataField="ID" HeaderText="ID" />
+            <asp:BoundField DataField="Code" HeaderText="Code" />
+            <asp:BoundField DataField="Name" HeaderText="Name" />
+            <asp:BoundField DataField="ParentCode" HeaderText="ParentCode" />
+            <asp:BoundField DataField="Description" HeaderText="Description" />
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <a href="#" onclick="GridViewExtender.HighlightRow(this,'red');stopEventPropagation(event);">
+                        red</a> <a href="#" onclick="GridViewExtender.HighlightRow(this,'blue');stopEventPropagation(event);">
+                            blue</a> <a href="#" onclick="GridViewExtender.HighlightRow(this);stopEventPropagation(event);">
+                                default</a>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <XCL:LinkBoxField HeaderText="编辑" Title="编辑窗口" Text="编辑" Width="480" Height="320"
+                DataNavigateUrlFields="ID" DataNavigateUrlFormatString="~/Test.aspx?ID={0}">
+            </XCL:LinkBoxField>
+        </Columns>
+    </asp:GridView>
+    <XCL:DataPager ID="DataPager1" runat="server" DataSourceID="ObjectDataSource1">
+    </XCL:DataPager>
+    <asp:ObjectDataSource ID="ObjectDataSource1" runat="server" DataObjectTypeName="NewLife.CommonEntity.Area"
+        DeleteMethod="Delete" InsertMethod="Insert" OldValuesParameterFormatString="original_{0}"
+        SelectMethod="FindAll" TypeName="NewLife.CommonEntity.Area" UpdateMethod="Update">
+        <SelectParameters>
+            <asp:Parameter Name="name" Type="String" />
+            <asp:Parameter Name="value" Type="Object" />
+            <asp:Parameter Name="startRowIndex" Type="Int32" />
+            <asp:Parameter Name="maximumRows" Type="Int32" />
+        </SelectParameters>
+    </asp:ObjectDataSource>
+    <XCL:GridViewExtender ID="GridViewExtender1" runat="server">
+    </XCL:GridViewExtender>
+<%--    <script type="text/javascript">
+        (function (ext) {
+            ext.ExtendDataRow('GridView1', {
+                HighlightRowMap: { '': 'yellow' },
+                EventMap: {
+                    click: ext.Highlight('aliceblue,greenyellow,beige,bisque,'),
+                    dblclick: ext.ClickElement('a', function (e) { return e.innerHTML === '编辑' })
+                }
+            });
+        } (GridViewExtender));
+    </script>
+    <br />
+    <a href="#" onclick="GridViewExtender.HighlightRow(this)">link</a>
+--%>    <asp:Button ID="Button1" runat="server" Text="Postback" />
+    <a href="#" onclick="alert(&quot;101&quot;)">&quot;link</a>
+    </form>
+</body>
+</html>
--- XControl/View/GridViewExtender.js
+++ XControl/View/GridViewExtender.js
@@ -1,11 +1,12 @@
-﻿;(function () {
-    var w = window, d = document, bindedEvents = [], objectData = new objectDict();
+﻿;
+(function () {
+    var w = window, d = document, bindedEvents = [], objectData;
 
     // 对象字典,可以将html元素和js变量关联起来,而不用在html元素中引用js元素
-    function objectDict() {
+    function ObjectDict() {
         this.__data = [];
     }
-    objectDict.prototype.get = function (n) {
+    ObjectDict.prototype.get = function (n) {
         var i, d;
         for (i = 0; i < this.__data.length; i++) {
             d = this.__data[i];
@@ -14,24 +15,28 @@
             }
         }
     };
-    objectDict.prototype.set = function (n, v) {
+    ObjectDict.prototype.set = function (n, v) {
         this.__data.push([n, v]);
         return this;
     };
 
     // 标签数组,提供类似html class属性的容器,使用push remove增删元素,使用last访问到末尾的元素,push会自动避免重复元素被加入
-    function tagArray(equal) {
+    function TagArray(equal) {
         this.__equal = equal;
     }
-    tagArray.prototype = [];
-    tagArray.prototype.push = function () {
+    TagArray.prototype = [];
+    TagArray.prototype.push = function () {
         var i, j, args = [];
         for (i = 0; i < arguments.length; i++) {
-            if (this.has(arguments[i])) continue; // 已有的不添加
+            if (this.has(arguments[i])) {
+                continue; // 已有的不添加
+            }
             if (args.length > 0) {
                 try {
                     for (j = 0; j < args.length; j++) {
-                        if (this.__equal(arguments[i], args[j])) throw ''; // push参数中重复的不添加
+                        if (this.__equal(arguments[i], args[j])) {
+                            throw ''; // push参数中重复的不添加
+                        }
                     }
                     args.push(arguments[i]);
                 } catch (e) { }
@@ -41,23 +46,29 @@
         }
         return Array.prototype.push.apply(this, args);
     };
-    tagArray.prototype.remove = function (obj) {
+    TagArray.prototype.remove = function (obj) {
         var i, s = 0, r = [];
-        while (i = this.has(obj, s)) {
+        while (true) {
+            i = this.has(obj, s);
+            if (!i) {
+                break;
+            }
             s = i[0];
             r.push(this.splice(i[0], 1)[0]);
         }
         return r;
     };
-    tagArray.prototype.has = function (obj, start) {
+    TagArray.prototype.has = function (obj, start) {
         var i;
         start = start || 0;
         for (i = start; i < this.length; i++) {
-            if (this.__equal(obj, this[i])) return [i, this[i]];
+            if (this.__equal(obj, this[i])) {
+                return [i, this[i]];
+            }
         }
         return false;
     };
-    tagArray.prototype.last = function () {
+    TagArray.prototype.last = function () {
         if (this.length > 0) {
             return this[this.length - 1];
         }
@@ -65,12 +76,22 @@
 
     function jsArray(arylike, s, n) {
         var ary, i;
-        if (Object.prototype.toString.call(arylike) === '[object Array]') return arylike;
+        if (Object.prototype.toString.call(arylike) === '[object Array]') {
+            return arylike;
+        }
         if (typeof arylike.length === 'number') {
-            if (typeof s === 'undefined') s = 0;
-            if (s < 0) s = arylike.length - s;
-            if (typeof n === 'undefined') n = arylike.length;
-            if (n < 0) n = arylike.length - n;
+            if (typeof s === 'undefined') {
+                s = 0;
+            }
+            if (s < 0) {
+                s = arylike.length - s;
+            }
+            if (typeof n === 'undefined') {
+                n = arylike.length;
+            }
+            if (n < 0) {
+                n = arylike.length - n;
+            }
             try {
                 return Array.prototype.slice.call(arylike, s, n);
             } catch (e) {
@@ -89,7 +110,9 @@
         ary = jsArray(ary);
         n = ary.length;
         for (i = 0; i < n; i++) {
-            if (func(ary[i], i)) ary.push(ary[i]);
+            if (func(ary[i], i)) {
+                ary.push(ary[i]);
+            }
         }
         ary.splice(0, n);
         return ary;
@@ -159,9 +182,9 @@
         return d[name];
     }
     function initBgColorArray() {
-        return new tagArray(function (a, b) {
+        return new TagArray(function (a, b) {
             return typeof a.t !== 'undefined' && a.t === b.t &&
-                        (a.c === b.c || typeof a.c === 'undefined');
+                (a.c === b.c || typeof a.c === 'undefined');
         });
     }
     // 返回指定元素的背景颜色的标签数组,其元素约定为{t:类型,c:'颜色'} 类型可以是任意类型,添加和删除时必须提供类型,(不指定颜色的删除即删除所有这个类型的元素),颜色在添加时应该提供,否则将有可能添加不进去
@@ -169,6 +192,7 @@
         return getOrInitObjectData(ele, 'BgColorArray', initBgColorArray);
     }
 
+    objectData = new ObjectDict();
     w.GridViewExtender = {
         // 对指定id的GridView 表格中数据行执行指定的操作
         ExtendDataRow: function (id, opt) {
@@ -184,19 +208,23 @@
                     while (cell !== null && cell.nodeType !== 1) {
                         cell = cell.nextSibling;
                     }
-                    if (cell === null) continue;
+                    if (cell === null) {
+                        continue;
+                    }
 
                     if (cell.tagName === 'TD' && cell.colSpan === 1) { // 行的第一个标签是td 并且这个td没有单元格合并
                         r = rows[i];
                         for (type in opt.EventMap) {
-                            bindEvent(r, type, wrapFunc(opt.EventMap[type], r, i));
+                            if (opt.EventMap.hasOwnProperty(type)) {
+                                bindEvent(r, type, wrapFunc(opt.EventMap[type], r, i));
+                            }
                         }
                     }
                 }
             }
         },
         IsInExtendDataRow: function (e) {
-            var r, i, d;
+            var r, d;
             try {
                 do {
                     if (e.tagName === 'TD') {
@@ -209,11 +237,11 @@
                     if (e.tagName === 'TABLE') {
                         d = objectData.get(e);
                         if (d && d.HighlightRowMap && d.EventMap) {
-                            return [e, r]
+                            return [e, r];
                         }
                     }
                     e = e.parentNode;
-                } while (e != null && e.tagName != 'BODY');
+                } while (e !== null && e.tagName !== 'BODY');
             } catch (ex) { }
             return null;
         },
@@ -221,7 +249,9 @@
             if (typeof colors === 'string') {
                 colors = colors.split(/\s*,\s*/);
             }
-            if (colors.length == 1) colors.push('');
+            if (colors.length === 1) {
+                colors.push('');
+            }
             return function (e, row, i) {
                 var s = row.style, d = getBgColorArray(row), c = d.has({ t: colors }), addfunc = 'push';
                 if (c) {
@@ -239,7 +269,7 @@
             };
         },
         HighlightRow: function (ele, name, toggle) {
-            var color, map, row, d, c, cc, gv = GridViewExtender.IsInExtendDataRow(ele);
+            var color, map, row, d, c, gv = w.GridViewExtender.IsInExtendDataRow(ele);
             if (gv && gv[0] && gv[1]) {
                 row = gv[1];
                 gv = gv[0];
@@ -248,17 +278,22 @@
                 if (!name) {
                     name = '';
                 }
-                if (map[name]) color = map[name];
-                else color = name;
+                if (map[name]) {
+                    color = map[name];
+                } else {
+                    color = name;
+                }
 
                 d = getBgColorArray(row);
                 c = d.has({ t: 'HighlightRow' }); // TODO 考虑是否有需要提供分组,而不是使用单一的HighlightRow的组
 
                 if (typeof toggle === 'undefined') {
-                    toggle = !c || c[1].c && color && c[1].c.toLowerCase() !== color.toLowerCase();
+                    toggle = !c || (c[1].c && color && c[1].c.toLowerCase() !== color.toLowerCase());
                 }
 
-                if (c) d.remove({ t: 'HighlightRow' });
+                if (c) {
+                    d.remove({ t: 'HighlightRow' });
+                }
                 if (toggle && typeof color !== 'undefined' && color !== '') {
                     d.push({ t: 'HighlightRow', c: color });
                 }
@@ -272,12 +307,12 @@
         },
         ClickElement: function (tag, func) {
             return function (e, row, i) {
-                var e, eles = filter(row.getElementsByTagName(tag), func);
-                e = eles[0];
-                if (e) {
-                    fireClickEvent(e);
+                var ele, eles = filter(row.getElementsByTagName(tag), func);
+                ele = eles[0];
+                if (ele) {
+                    fireClickEvent(ele);
                 }
-            }
+            };
         }
     };
 
@@ -288,4 +323,4 @@
             unbindEvent.apply(this, args);
         }
     });
-} ());
+}());
