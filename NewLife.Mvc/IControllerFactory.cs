1cb9684da943e44cfd7a1d3aae096e71de19e26d
nnhy <nnhy@vip.qq.com>
2011-11-01 06:34:05 +0000

    v1.0.2011.1101   创建组件。                  支持Url路由到具体控制器，也支持Url路由到其它Url                  提供控制器接口                  提供模版引擎接口                  提供默认简单控制器实现，通过模版引擎接口实现页面生成

---
+++ NewLife.Mvc/IControllerFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewLife.Mvc
+{
+    public interface IControllerFactory
+    {
+        IController Create();
+    }
+}
