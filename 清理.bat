dd0801c6f3c9702170fda843f934a3ecc2c8d65b
nnhy <nnhy@vip.qq.com>
2011-11-15 06:06:10 +0000

    必须填写至少10个字的日志

--- 清理.bat
+++ 清理.bat
@@ -1,9 +1,9 @@
 for /f "tokens=*" %%a in ('dir obj /b /ad /s ^|sort') do rd "%%a" /s/q
 for /f "tokens=*" %%a in ('dir bin /b /ad /s ^|sort') do rd "%%a" /s/q
 
-rd WebSite1\Log /s/q
-rd WebSite1\App_Data /s/q
+rd Web\Log /s/q
+rd Web\App_Data /s/q
 
-md WebSite1\Bin
+md Web\Bin
 
-Copy ..\DLL\XControl.* WebSite1\Bin\
+Copy ..\DLL\XControl.* Web\Bin\
