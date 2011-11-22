17cc79aaf8fbd285e4e0005f0b159b5d729f5a83
Stone <nnhy@vip.qq.com>
2011-11-22 00:37:12 +0000

    必须填写至少10个字的日志

---
+++ NewLife.Core/Scripting/ScriptEngine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewLife.Scripting
+{
+    /// <summary>脚本引擎</summary>
+    public class ScriptEngine
+    {
+        #region 属性
+        #endregion
+
+        #region 传入表达式或代码
+        public ScriptEngine CreateScriptSourceFromString(String expression)
+        {
+            return this;
+        }
+
+        public ScriptEngine CreateScriptSourceFromFile(String file)
+        {
+            return this;
+        }
+        #endregion
+
+        #region 执行方法
+        public Object Execute()
+        {
+            return null;
+        }
+        #endregion
+
+        #region 快速静态方法
+        /// <summary>执行表达式，返回结果</summary>
+        /// <param name="expression"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static Object Execute(String expression, params Object[] parameters)
+        {
+            return null;
+        }
+
+        /// <summary>执行代码，返回结果</summary>
+        /// <param name="code"></param>
+        /// <param name="parameterTypes"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static Object Execute(String code, Type[] parameterTypes, params Object[] parameters)
+        {
+            return null;
+        }
+        #endregion
+    }
+}
