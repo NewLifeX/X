18c66a4042d6e3b8f99ee02d82a34fdc67201264
nnhy <nnhy@vip.qq.com>
2011-11-25 00:45:27 +0000

    将来的系统设计将会朝着模块化的方向发展，每个模块一个独立的子目录，最好包括配置文件的所有内容也一起在里面

--- NewLife.Core/Web/Modules/HttpModuleLoader.cs
+++ NewLife.Core/Web/Modules/HttpModuleLoader.cs
@@ -5,6 +5,9 @@ using NewLife.Exceptions;
 namespace NewLife.Web
 {
     /// <summary>子目录HttpModule加载模块。用于加载子目录web.config中配置的IHttpModule。</summary>
+    /// <remarks>
+    /// 将来的系统设计将会朝着模块化的方向发展，每个模块一个独立的子目录，最好包括配置文件的所有内容也一起在里面
+    /// </remarks>
     public class HttpModuleLoader : IHttpModule
     {
         #region IHttpModule Members
