b31957ff940d0c284b95fbd15091f1f84fa9fd0f
nnhy <nnhy@vip.qq.com>
2011-11-25 00:43:39 +0000

    考虑设计一个HttpModuleLoader的IHttpModule，用于加载子目录web.config中配置的IHttpModule

---
+++ NewLife.Core/Web/Modules/HttpModuleLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web;
+using NewLife.Exceptions;
+
+namespace NewLife.Web
+{
+    /// <summary>子目录HttpModule加载模块。用于加载子目录web.config中配置的IHttpModule。</summary>
+    public class HttpModuleLoader : IHttpModule
+    {
+        #region IHttpModule Members
+        void IHttpModule.Dispose() { }
+
+        /// <summary>
+        /// 初始化模块，准备拦截请求。
+        /// </summary>
+        /// <param name="context"></param>
+        void IHttpModule.Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(context_BeginRequest);
+        }
+
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            Process();
+        }
+        #endregion
+
+        #region 加载
+        /// <summary>根据路径判断是否加载</summary>
+        protected virtual void Process()
+        {
+            //todo 根据路径判断是否加载
+            throw new XException("本功能未完成！");
+        }
+        #endregion
+    }
+}
