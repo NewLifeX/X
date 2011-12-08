e21241d0d7f126481b75771044c1148ce170829e
nnhy <nnhy@vip.qq.com>
2011-12-08 10:29:39 +0000

    枚举助手增加一个获取整型字典的方法

--- NewLife.Core/Extension/EnumHelper.cs
+++ NewLife.Core/Extension/EnumHelper.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.ComponentModel;
 using System.Reflection;
 using NewLife.Reflection;
+using NewLife.Linq;
 
 namespace System
 {
@@ -39,35 +40,63 @@ namespace System
             return null;
         }
 
-        //static Dictionary<TEnum, String> flagCache;
-        /// <summary>
-        /// 获取枚举类型的所有字段注释
-        /// </summary>
+        /// <summary>获取枚举类型的所有字段注释</summary>
         /// <typeparam name="TEnum"></typeparam>
         /// <returns></returns>
         public static Dictionary<TEnum, String> GetDescriptions<TEnum>()
         {
-            //if (flagCache != null) return flagCache;
+            Dictionary<TEnum, String> dic = new Dictionary<TEnum, string>();
 
-            Dictionary<TEnum, String> flagCache = new Dictionary<TEnum, string>();
+            //TypeX type = typeof(TEnum);
+            //foreach (FieldInfo item in type.BaseType.GetFields(BindingFlags.Public | BindingFlags.Static))
+            //{
+            //    if (!item.IsStatic) continue;
 
-            TypeX type = typeof(TEnum);
-            foreach (FieldInfo item in type.BaseType.GetFields(BindingFlags.Public | BindingFlags.Static))
+            //    // 这里的快速访问方法会报错
+            //    //FieldInfoX fix = FieldInfoX.Create(item);
+            //    //PermissionFlags value = (PermissionFlags)fix.GetValue(null);
+            //    TEnum value = (TEnum)item.GetValue(null);
+
+            //    String des = item.Name;
+            //    DescriptionAttribute att = AttributeX.GetCustomAttribute<DescriptionAttribute>(item, false);
+            //    if (att != null && !String.IsNullOrEmpty(att.Description)) des = att.Description;
+            //    dic.Add(value, des);
+            //}
+
+            foreach (var item in GetDescriptions(typeof(TEnum)))
+            {
+                dic.Add((TEnum)(Object)item.Key, item.Value);
+            }
+
+            return dic;
+        }
+
+        /// <summary>获取枚举类型的所有字段注释</summary>
+        /// <param name="enumType"></param>
+        /// <returns></returns>
+        public static Dictionary<Int32, String> GetDescriptions(Type enumType)
+        {
+            var dic = new Dictionary<Int32, String>();
+            foreach (FieldInfo item in enumType.BaseType.GetFields(BindingFlags.Public | BindingFlags.Static))
             {
                 if (!item.IsStatic) continue;
 
                 // 这里的快速访问方法会报错
                 //FieldInfoX fix = FieldInfoX.Create(item);
                 //PermissionFlags value = (PermissionFlags)fix.GetValue(null);
-                TEnum value = (TEnum)item.GetValue(null);
+                Int32 value = Convert.ToInt32(item.GetValue(null));
 
                 String des = item.Name;
+
+                DisplayNameAttribute dna = AttributeX.GetCustomAttribute<DisplayNameAttribute>(item, false);
+                if (dna != null && !String.IsNullOrEmpty(dna.DisplayName)) des = dna.DisplayName;
+
                 DescriptionAttribute att = AttributeX.GetCustomAttribute<DescriptionAttribute>(item, false);
                 if (att != null && !String.IsNullOrEmpty(att.Description)) des = att.Description;
-                flagCache.Add(value, des);
+                dic.Add(value, des);
             }
 
-            return flagCache;
+            return dic;
         }
     }
 }
