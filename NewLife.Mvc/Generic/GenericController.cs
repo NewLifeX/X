bed9ef66dea20ed448e1df694487e9c6f28deb95
netwjx <netwjx@gmail.com>
2011-12-12 09:49:28 +0000

    修正GenericController的ProcessRequest和IsReusable修饰为可重载

--- NewLife.Mvc/Generic/GenericController.cs
+++ NewLife.Mvc/Generic/GenericController.cs
@@ -104,14 +104,14 @@ namespace NewLife.Mvc
 
         /// <summary>通过实现 <see cref="T:NewLife.Mvc.IController" /> 接口的自定义 Controller 启用 HTTP Web 请求的处理。</summary>
         /// <param name="context"><see cref="T:NewLife.Mvc.IRouteContext" /> 对象，它提供对用于为 HTTP 请求提供服务的内部服务器对象的引用。</param>
-        public void ProcessRequest(IRouteContext context)
+        public virtual void ProcessRequest(IRouteContext context)
         {
             Render(null);
         }
 
         /// <summary>获取一个值，该值指示其他请求是否可以使用 <see cref="T:NewLife.Mvc.IController" /> 实例。</summary>
         /// <returns>如果 <see cref="T:NewLife.Mvc.IController" /> 实例可再次使用，则为 true；否则为 false。</returns>
-        public bool IsReusable
+        public virtual bool IsReusable
         {
             get { return false; }
         }
