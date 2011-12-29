7c2e2679945c4340ed50412e7d36b4a0984164ed
Stone <nnhy@vip.qq.com>
2011-12-29 15:36:35 +0000

    增加Http头部实体
    StreamWriter太恶心了，自动把流给关闭了，还没有地方设置

---
+++ NewLife.Net/Protocols/Http/HttpHeader.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace NewLife.Net.Protocols.Http
+{
+    /// <summary>Http头部</summary>
+    public class HttpHeader
+    {
+        #region 属性
+        private HttpVerb _Verb;
+        /// <summary>谓语</summary>
+        public HttpVerb Verb { get { return _Verb; } set { _Verb = value; } }
+
+        private String _Url;
+        /// <summary>请求文档</summary>
+        public String Url { get { return _Url; } set { _Url = value; } }
+
+        private String _Version;
+        /// <summary>协议版本</summary>
+        public String Version { get { return _Version; } set { _Version = value; } }
+
+        private IDictionary<String, String> _Headers;
+        /// <summary>头部</summary>
+        public IDictionary<String, String> Headers { get { return _Headers ?? (_Headers = new Dictionary<String, String>(StringComparer.OrdinalIgnoreCase)); } }
+
+        const String VersionPrefix = "HTTP/";
+        #endregion
+
+        #region 方法
+        /// <summary>从流中读取Http头部对象</summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static HttpHeader Read(Stream stream)
+        {
+            var entity = new HttpHeader();
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                String line = reader.ReadLine();
+                var ss = line.Split(" ");
+                entity.Verb = (HttpVerb)Enum.Parse(typeof(HttpVerb), ss[0], true);
+                entity.Url = ss[1];
+                entity.Version = ss[2];
+                if (entity.Version.StartsWith(VersionPrefix, StringComparison.OrdinalIgnoreCase)) entity.Version = entity.Version.Substring(VersionPrefix.Length);
+
+                while (!String.IsNullOrEmpty(line = reader.ReadLine()))
+                {
+                    Int32 p = line.IndexOf(":");
+                    entity.Headers.Add(line.Substring(0, p).Trim(), line.Substring(p + 1).Trim());
+                }
+            }
+
+            return entity;
+        }
+
+        public void Write(Stream stream)
+        {
+            // StreamWriter太恶心了，自动把流给关闭了，还没有地方设置
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                var ver = Version;
+                if (!ver.StartsWith(VersionPrefix, StringComparison.OrdinalIgnoreCase)) ver = VersionPrefix + ver;
+                writer.WriteLine("{0} {1} {2}", Verb, Url, ver);
+                foreach (var item in Headers)
+                {
+                    writer.WriteLine("{0}: {1}", item.Key, item.Value);
+                }
+                writer.WriteLine();
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            var ver = Version;
+            if (!ver.StartsWith(VersionPrefix, StringComparison.OrdinalIgnoreCase)) ver = VersionPrefix + ver;
+            sb.AppendFormat("{0} {1} {2}", Verb, Url, ver);
+            sb.AppendLine();
+            foreach (var item in Headers)
+            {
+                sb.AppendFormat("{0}: {1}", item.Key, item.Value);
+                sb.AppendLine();
+            }
+            sb.AppendLine();
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+
+    /// <summary>Http谓语</summary>
+    public enum HttpVerb
+    {
+        /// <summary>未解析</summary>
+        Unparsed,
+
+        /// <summary>未知</summary>
+        Unknown,
+
+        /// <summary>获取</summary>
+        GET,
+
+        /// <summary>推送</summary>
+        PUT,
+
+        /// <summary>跟GET一样，只不过响应包只包括头部而没有内容</summary>
+        HEAD,
+
+        /// <summary>提交</summary>
+        POST,
+
+        /// <summary>调试</summary>
+        DEBUG,
+
+        /// <summary>跟踪</summary>
+        TRACE,
+
+        /// <summary>连接</summary>
+        CONNECT,
+
+        /// <summary>选项</summary>
+        OPTIONS,
+
+        /// <summary>删除</summary>
+        DELETE
+    }
+}
--- NewLife.Net/Proxy/Filters/HttpFilter.cs
+++ NewLife.Net/Proxy/Filters/HttpFilter.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using NewLife.IO;
 using NewLife.Net.Sockets;
+using NewLife.Net.Protocols.Http;
 
 namespace NewLife.Net.Proxy
 {
@@ -19,23 +20,32 @@ namespace NewLife.Net.Proxy
         public override Stream OnClientToServer(IProxySession session, Stream stream, NetEventArgs e)
         {
             // 解析请求头
-            String header = stream.ToStr(Encoding.ASCII);
-            //String src = String.Format("HOST: {0}", session.Server.Address);
-            //if (session.Server.Port != 80) src += ":" + session.Server.Port;
-            //String des = String.Format("HOST: {0}", "www.qq.com");
-            String[] ss = header.Split(new String[] { Environment.NewLine }, StringSplitOptions.None);
-            for (int i = 0; i < ss.Length; i++)
-            {
-                if (ss[i].StartsWith("host:", StringComparison.OrdinalIgnoreCase))
-                {
-                    ss[i] = String.Format("HOST: {0}", "www.baidu.com");
-                    break;
-                }
-            }
+            var entity = HttpHeader.Read(stream);
 
-            //header = header.Replace(src, des);
-            header = String.Join(Environment.NewLine, ss);
-            stream = new MemoryStream(Encoding.ASCII.GetBytes(header));
+            //String header = stream.ToStr(Encoding.ASCII);
+            ////String src = String.Format("HOST: {0}", session.Server.Address);
+            ////if (session.Server.Port != 80) src += ":" + session.Server.Port;
+            ////String des = String.Format("HOST: {0}", "www.qq.com");
+            //String[] ss = header.Split(new String[] { Environment.NewLine }, StringSplitOptions.None);
+            //for (int i = 0; i < ss.Length; i++)
+            //{
+            //    if (ss[i].StartsWith("host:", StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        ss[i] = String.Format("HOST: {0}", "www.baidu.com");
+            //        break;
+            //    }
+            //}
+
+            ////header = header.Replace(src, des);
+            //header = String.Join(Environment.NewLine, ss);
+            //stream = new MemoryStream(Encoding.ASCII.GetBytes(header));
+
+            entity.Headers["Host"] = "www.baidu.com";
+
+            var ms = new MemoryStream();
+            entity.Write(ms);
+            stream.CopyTo(ms);
+            stream = ms;
 
             return stream;
         }
