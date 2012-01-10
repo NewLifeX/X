bf24f8a6a6e1902453dc8241bca9ffd70557c3cc
nnhy <nnhy@vip.qq.com>
2012-01-10 05:53:11 +0000

    太复杂了，精简一下

--- NewLife.Net/Application/SerialServer.cs
+++ NewLife.Net/Application/SerialServer.cs
@@ -19,47 +19,9 @@ namespace NewLife.Net.Application
         public String PortName { get { return _PortName; } set { _PortName = value; } }
         #endregion
 
-        #region 扩展属性
-        private SerialPort _SerialPort;
-        /// <summary>串口</summary>
-        public SerialPort SerialPort
-        {
-            get
-            {
-                if (_SerialPort == null)
-                {
-                    var sp = new SerialPort(PortName);
-                    //sp.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
-                    sp.ReadTimeout = 1000;
-                    sp.Open();
-
-                    _SerialPort = sp;
-                }
-                return _SerialPort;
-            }
-            set { _SerialPort = value; }
-        }
-        #endregion
-
         #region 构造
         /// <summary>实例化一个串口服务器</summary>
-        public SerialServer()
-        {
-            Port = 23;
-        }
-
-        /// <summary>子类重载实现资源释放逻辑</summary>
-        /// <param name="disposing">从Dispose调用（释放所有资源）还是析构函数调用（释放非托管资源）</param>
-        protected override void OnDispose(bool disposing)
-        {
-            base.OnDispose(disposing);
-
-            if (_SerialPort != null)
-            {
-                _SerialPort.Dispose();
-                _SerialPort = null;
-            }
-        }
+        public SerialServer() { Port = 24; }
         #endregion
 
         #region 业务
@@ -71,7 +33,11 @@ namespace NewLife.Net.Application
             base.OnAccepted(sender, e);
 
             var session = e.Socket as ISocketSession;
-            ReadAndSend(session, e.RemoteEndPoint);
+            using (var sp = new SerialPort(PortName))
+            {
+                sp.Open();
+                ReadAndSend(sp, session, e.RemoteEndPoint);
+            }
         }
 
         /// <summary>收到数据时</summary>
@@ -82,25 +48,27 @@ namespace NewLife.Net.Application
             base.OnReceived(sender, e);
 
             var session = e.Socket as ISocketSession;
-            if (e.BytesTransferred > 0)
+            using (var sp = new SerialPort(PortName))
             {
-                WriteLog("Net=>SerialPort: {0}", e.Buffer.ToHex(e.Offset, e.BytesTransferred));
-                SerialPort.Write(e.Buffer, e.Offset, e.BytesTransferred);
-            }
+                sp.Open();
+                if (e.BytesTransferred > 0)
+                {
+                    WriteLog("Net=>SerialPort: {0}", e.Buffer.ToHex(e.Offset, e.BytesTransferred));
+                    sp.Write(e.Buffer, e.Offset, e.BytesTransferred);
+                }
 
-            ReadAndSend(session, e.RemoteEndPoint);
+                ReadAndSend(sp, session, e.RemoteEndPoint);
+            }
         }
 
-        void ReadAndSend(ISocketSession session, EndPoint remote)
+        void ReadAndSend(SerialPort sp, ISocketSession session, EndPoint remote)
         {
             // 读取数据
             var ms = new MemoryStream();
-            while (SerialPort.BytesToRead > 0)
+            while (sp.BytesToRead > 0)
             {
-                Int32 d = SerialPort.ReadByte();
-                if (d != -1) break;
-
-                ms.WriteByte((Byte)d);
+                Int32 d = sp.ReadByte();
+                if (d != -1) ms.WriteByte((Byte)d);
             }
 
             if (ms.Length > 0)
@@ -111,14 +79,6 @@ namespace NewLife.Net.Application
                 session.Send(ms, remote);
             }
         }
-
-        //private ReadWriteMemoryStream stream = new ReadWriteMemoryStream();
-
-        //void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        //{
-        //    Int32 d = SerialPort.ReadByte();
-        //    if (d != -1) stream.WriteByte((Byte)d);
-        //}
         #endregion
     }
 }
