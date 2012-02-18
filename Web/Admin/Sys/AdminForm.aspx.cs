721747f4b55e9d597efed43e7620fba70876fffe
Stone <nnhy@vip.qq.com>
2012-02-18 09:00:05 +0000

    修正管理员表单无法修改密码的错误

--- Web/Admin/Sys/AdminForm.aspx.cs
+++ Web/Admin/Sys/AdminForm.aspx.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using NewLife.CommonEntity;
+using NewLife.Security;
 
 public partial class Pages_AdminForm : MyEntityForm
 {
     /// <summary>实体类型</summary>
     public override Type EntityType { get { return CommonManageProvider.Provider.AdminstratorType; } set { base.EntityType = value; } }
 
+    protected override void OnInit(EventArgs e)
+    {
+        base.OnInit(e);
+
+        EntityForm.OnSetForm += EntityForm_OnSetForm;
+        EntityForm.OnGetForm += EntityForm_OnGetForm;
+    }
+
+    void EntityForm_OnSetForm(object sender, EntityFormEventArgs e)
+    {
+        frmPassword.Text = null;
+    }
+
+    void EntityForm_OnGetForm(object sender, EntityFormEventArgs e)
+    {
+        if (!String.IsNullOrEmpty(frmPassword.Text)) EntityForm.Entity.SetItem("Password", DataHelper.Hash(frmPassword.Text));
+    }
+    
     protected override void OnInitComplete(EventArgs e)
     {
         base.OnInitComplete(e);
