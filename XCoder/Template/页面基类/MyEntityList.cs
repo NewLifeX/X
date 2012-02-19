41b642c6aa75d8be510619c288b7c41666041b79
Stone <nnhy@vip.qq.com>
2012-02-19 08:35:01 +0000

    升级页面基类，支持给添加按钮附加参数

--- XCoder/Template/页面基类/MyEntityList.cs
+++ XCoder/Template/页面基类/MyEntityList.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿﻿using System;
+using System.Text;
 using System.Web.UI;
+using NewLife.Collections;
 using NewLife.CommonEntity;
+using NewLife.Web;
 using XCode;
+using XControl;
 
 /// <summary>实体列表页面基类</summary>
 public abstract class MyEntityList : Page
@@ -21,6 +25,44 @@ public abstract class MyEntityList : Page
         base.OnPreInit(e);
     }
     #endregion
+
+    protected override void OnPreLoad(EventArgs e)
+    {
+        base.OnPreLoad(e);
+
+        AutoAddParamForAdd();
+    }
+
+    /// <summary>
+    /// 自动为添加按钮附加参数，Request中，只要是当前实体成员的参数，否附加上去
+    /// </summary>
+    void AutoAddParamForAdd()
+    {
+        if (Request.QueryString == null || Request.QueryString.Count < 1) return;
+        if (EntityType == null) return;
+
+        LinkBox lb = ControlHelper.FindControlByField<LinkBox>(Page, "lbAdd");
+        if (lb == null) return;
+
+        StringBuilder sb = new StringBuilder();
+        IEntityOperate op = EntityFactory.CreateOperate(EntityType);
+        HashSet<String> hs = new HashSet<string>(op.FieldNames, StringComparer.OrdinalIgnoreCase);
+        foreach (String item in Request.QueryString.Keys)
+        {
+            // 仅接受实体类成员
+            if (!hs.Contains(item)) continue;
+
+            if (sb.Length > 0) sb.Append("&");
+            sb.AppendFormat("{0}={1}", item, Request.QueryString[item]);
+        }
+        if (sb.Length > 0)
+        {
+            if (!lb.Url.Contains("?"))
+                lb.Url += "?" + sb;
+            else
+                lb.Url += "&" + sb;
+        }
+    }
 }
 
 /// <summary>实体列表页面基类</summary>
