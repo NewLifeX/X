926b8805c6e8560e5358f32569df1b70b0cc0c18
nnhy <nnhy@vip.qq.com>
2012-03-19 04:10:00 +0000

    预置单寄存器。把具体二进值装入一个保持寄存器

---
+++ NewLife.Net/ModBus/WriteSingleRegister.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewLife.Net.ModBus
+{
+    /// <summary>预置单寄存器。把具体二进值装入一个保持寄存器</summary>
+    public class WriteSingleRegister : MBEntity, IModBusRequest, IModBusResponse
+    {
+        #region 属性
+        private UInt16 _DataAddress;
+        /// <summary>数据地址</summary>
+        public UInt16 DataAddress { get { return _DataAddress; } set { _DataAddress = value; } }
+
+        private Byte[] _Data;
+        /// <summary>数据</summary>
+        public Byte[] Data { get { return _Data; } set { _Data = value; } }
+        #endregion
+
+        /// <summary>实例化</summary>
+        public WriteSingleRegister() { Function = MBFunction.WriteSingleRegister; }
+    }
+}
