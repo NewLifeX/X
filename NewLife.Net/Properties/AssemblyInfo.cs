68a1bade626673fc325760a8e69c5b4fc0d03341
nnhy <nnhy@vip.qq.com>
2012-04-11 07:11:11 +0000

    v2.1.2012.0411   精简核心结构，进一步完善网络模型

--- NewLife.Net/Properties/AssemblyInfo.cs
+++ NewLife.Net/Properties/AssemblyInfo.cs
@@ -32,10 +32,12 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.*")]
-[assembly: AssemblyFileVersion("2.0.2012.0229")]
+[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyFileVersion("2.1.2012.0411")]
 
 /*
+ * v2.1.2012.0411   精简核心结构，进一步完善网络模型
+ * 
  * v2.0.2012.0229   完全重构网络模型。划分为四层：Socket层、封装层ISocket、数据会话层ISocketSession、网络会话INetSession
  * 
  * v1.7.2012.0209   完善串口服务器和DNS服务器
