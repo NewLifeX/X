c88349f026a237e0d0974e3c4c5823583a4e4f8f
nnhy <nnhy@vip.qq.com>
2012-04-17 07:26:19 +0000

    增加模型助手类，用于模型扩展 获取指定类型的服务对象

---
+++ NewLife.Core/Model/ModelHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewLife.Model
+{
+    /// <summary>模型扩展</summary>
+    public static class ModelHelper
+    {
+        /// <summary>获取指定类型的服务对象</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="provider"></param>
+        /// <returns></returns>
+        public static T GetService<T>(this IServiceProvider provider)
+        {
+            return (T)provider.GetService(typeof(T));
+        }
+    }
+}
