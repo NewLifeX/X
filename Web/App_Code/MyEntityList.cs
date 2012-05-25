40d78211e72ebc5dac117b31b7853f65334e15d6
nnhy <nnhy@vip.qq.com>
2012-05-25 07:01:51 +0000

    必须填写至少10个字的日志

--- Web/App_Code/MyEntityList.cs
+++ Web/App_Code/MyEntityList.cs
@@ -5,9 +5,12 @@
  * 版权：版权所有 (C) 新生命开发团队 2011
 */
 ﻿using System;
+using System.Text;
 using System.Web.UI;
+using NewLife.Collections;
 using NewLife.CommonEntity;
 using XCode;
+using XControl;
 
 /// <summary>实体列表页面基类</summary>
 public abstract class MyEntityList : Page
@@ -27,10 +30,48 @@ public abstract class MyEntityList : Page
         base.OnPreInit(e);
     }
     #endregion
+
+    protected override void OnPreLoad(EventArgs e)
+    {
+        base.OnPreLoad(e);
+
+        AutoAddParamForAdd();
+    }
+
+    /// <summary>
+    /// 自动为添加按钮附加参数，Request中，只要是当前实体成员的参数，否附加上去
+    /// </summary>
+    void AutoAddParamForAdd()
+    {
+        if (Request.QueryString == null || Request.QueryString.Count < 1) return;
+        if (EntityType == null) return;
+
+        LinkBox lb = ControlHelper.FindControlByField<LinkBox>(Page, "lbAdd");
+        if (lb == null) return;
+
+        StringBuilder sb = new StringBuilder();
+        IEntityOperate op = EntityFactory.CreateOperate(EntityType);
+        HashSet<String> hs = new HashSet<string>(op.FieldNames, StringComparer.OrdinalIgnoreCase);
+        foreach (String item in Request.QueryString.Keys)
+        {
+            // 仅接受实体类成员
+            if (!hs.Contains(item)) continue;
+
+            if (sb.Length > 0) sb.Append("&");
+            sb.AppendFormat("{0}={1}", item, Request.QueryString[item]);
+        }
+        if (sb.Length > 0)
+        {
+            if (!lb.Url.Contains("?"))
+                lb.Url += "?" + sb;
+            else
+                lb.Url += "&" + sb;
+        }
+    }
 }
 
 /// <summary>实体列表页面基类</summary>
-public class MyEntityList<TEntity> : MyEntityForm where TEntity : Entity<TEntity>, new()
+public class MyEntityList<TEntity> : MyEntityList where TEntity : Entity<TEntity>, new()
 {
     /// <summary>实体类</summary>
     public override Type EntityType { get { return base.EntityType ?? (base.EntityType = typeof(TEntity)); } set { base.EntityType = value; } }
