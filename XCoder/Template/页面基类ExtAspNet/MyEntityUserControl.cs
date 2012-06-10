3af3479d5acb53ec2b6fb7ae2bc556ed8cde4a07
Stone <nnhy@vip.qq.com>
2012-06-10 16:56:55 +0000

    用户自定义控件基类不需要检查权限，只有页面基类需要

--- XCoder/Template/页面基类ExtAspNet/MyEntityUserControl.cs
+++ XCoder/Template/页面基类ExtAspNet/MyEntityUserControl.cs
@@ -11,16 +11,11 @@ public class MyEntityUserControl : UserControl
     /// <summary>实体类</summary>
     public virtual Type EntityType { get { return _EntityType; } set { _EntityType = value; } }
 
-    ///// <summary>管理页控制器</summary>
-    //protected IManagePage Manager;
-
     /// <summary>表单控制器</summary>
     protected IEntityForm EntityForm;
 
     protected override void OnInit(EventArgs e)
     {
-        //// 让页面管理器先注册，因为页面管理器要控制权限
-        //Manager = ManageProvider.Provider.GetService<IManagePage>().Init(this, EntityType);
         EntityForm = ManageProvider.Provider.GetService<IEntityForm>().Init(this, EntityType);
 
         base.OnInit(e);
