87d9e0dcaad94fb6b08c621f8e3299bafd40a0ab
nnhy <nnhy@vip.qq.com>
2012-06-18 03:12:51 +0000

    各个模块应该实现自己的手册类，指定专用的数据库连接。

---
+++ NewLife.CommonEntity/帮助/手册.Biz.cs
@@ -0,0 +1,182 @@
+﻿/*
+ * XCoder v4.8.4548.28140
+ * 作者：nnhy/NEWLIFE
+ * 时间：2012-06-18 11:07:37
+ * 版权：版权所有 (C) 新生命开发团队 2012
+*/
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Xml.Serialization;
+using NewLife.CommonEntity;
+using NewLife.Log;
+using NewLife.Web;
+using XCode;
+using XCode.Configuration;
+
+namespace NewLife.CommonEntity
+{
+    // 禁止直接使用，要求各个模块自己实现
+    ///// <summary>手册</summary>
+    //[ModelCheckMode(ModelCheckModes.CheckTableWhenFirstUse)]
+    //public class Manual : Manual<Manual> { }
+    
+    /// <summary>手册</summary>
+    /// <remarks>
+    /// 各个模块应该实现自己的手册类，指定专用的数据库连接。
+    /// </remarks>
+    public partial class Manual<TEntity> : Entity<TEntity> where TEntity : Manual<TEntity>, new()
+    {
+        #region 对象操作﻿
+        static Manual()
+        {
+            // 用于引发基类的静态构造函数，所有层次的泛型实体类都应该有一个
+            TEntity entity = new TEntity();
+        }
+
+        /// <summary>验证数据，通过抛出异常的方式提示验证失败。</summary>
+        /// <param name="isNew"></param>
+        public override void Valid(Boolean isNew)
+        {
+            // 这里验证参数范围，建议抛出参数异常，指定参数名，前端用户界面可以捕获参数异常并聚焦到对应的参数输入框
+            //if (String.IsNullOrEmpty(Name)) throw new ArgumentNullException(_.Name, _.Name.DisplayName + "无效！");
+            //if (!isNew && ID < 1) throw new ArgumentOutOfRangeException(_.ID, _.ID.DisplayName + "必须大于0！");
+
+            // 建议先调用基类方法，基类方法会对唯一索引的数据进行验证
+            base.Valid(isNew);
+
+            // 在新插入数据或者修改了指定字段时进行唯一性验证，CheckExist内部抛出参数异常
+            //if (isNew || Dirtys[_.Name]) CheckExist(_.Name);
+            
+        }
+
+        ///// <summary>首次连接数据库时初始化数据，仅用于实体类重载，用户不应该调用该方法</summary>
+        //[EditorBrowsable(EditorBrowsableState.Never)]
+        //protected override void InitData()
+        //{
+        //    base.InitData();
+
+        //    // InitData一般用于当数据表没有数据时添加一些默认数据，该实体类的任何第一次数据库操作都会触发该方法，默认异步调用
+        //    // Meta.Count是快速取得表记录数
+        //    if (Meta.Count > 0) return;
+
+        //    // 需要注意的是，如果该方法调用了其它实体类的首次数据库操作，目标实体类的数据初始化将会在同一个线程完成
+        //    if (XTrace.Debug) XTrace.WriteLine("开始初始化{0}手册数据……", typeof(TEntity).Name);
+
+        //    var entity = new Manual();
+        //    entity.Url = "abc";
+        //    entity.Summary = "abc";
+        //    entity.Content = "abc";
+        //    entity.Insert();
+
+        //    if (XTrace.Debug) XTrace.WriteLine("完成初始化{0}手册数据！", typeof(TEntity).Name);
+        //}
+
+
+        ///// <summary>已重载。基类先调用Valid(true)验证数据，然后在事务保护内调用OnInsert</summary>
+        ///// <returns></returns>
+        //public override Int32 Insert()
+        //{
+        //    return base.Insert();
+        //}
+
+        ///// <summary>已重载。在事务保护范围内处理业务，位于Valid之后</summary>
+        ///// <returns></returns>
+        //protected override Int32 OnInsert()
+        //{
+        //    return base.OnInsert();
+        //}
+        #endregion
+
+        #region 扩展属性﻿
+        #endregion
+
+        #region 扩展查询﻿
+        /// <summary>根据资源查找</summary>
+        /// <param name="url">资源</param>
+        /// <returns></returns>
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public static TEntity FindByUrl(String url)
+        {
+            if (Meta.Count >= 1000)
+                return Find(_.Url, url);
+            else // 实体缓存
+                return Meta.Cache.Entities.Find(_.Url, url);
+            // 单对象缓存
+            //return Meta.SingleCache[url];
+        }
+
+        /// <summary>根据编号查找</summary>
+        /// <param name="id">编号</param>
+        /// <returns></returns>
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public static TEntity FindByID(Int32 id)
+        {
+            if (Meta.Count >= 1000)
+                return Find(_.ID, id);
+            else // 实体缓存
+                return Meta.Cache.Entities.Find(_.ID, id);
+            // 单对象缓存
+            //return Meta.SingleCache[id];
+        }
+        #endregion
+
+        #region 高级查询
+        // 以下为自定义高级查询的例子
+
+        ///// <summary>
+        ///// 查询满足条件的记录集，分页、排序
+        ///// </summary>
+        ///// <param name="key">关键字</param>
+        ///// <param name="orderClause">排序，不带Order By</param>
+        ///// <param name="startRowIndex">开始行，0表示第一行</param>
+        ///// <param name="maximumRows">最大返回行数，0表示所有行</param>
+        ///// <returns>实体集</returns>
+        //[DataObjectMethod(DataObjectMethodType.Select, true)]
+        //public static EntityList<TEntity> Search(String key, String orderClause, Int32 startRowIndex, Int32 maximumRows)
+        //{
+        //    return FindAll(SearchWhere(key), orderClause, null, startRowIndex, maximumRows);
+        //}
+
+        ///// <summary>
+        ///// 查询满足条件的记录总数，分页和排序无效，带参数是因为ObjectDataSource要求它跟Search统一
+        ///// </summary>
+        ///// <param name="key">关键字</param>
+        ///// <param name="orderClause">排序，不带Order By</param>
+        ///// <param name="startRowIndex">开始行，0表示第一行</param>
+        ///// <param name="maximumRows">最大返回行数，0表示所有行</param>
+        ///// <returns>记录数</returns>
+        //public static Int32 SearchCount(String key, String orderClause, Int32 startRowIndex, Int32 maximumRows)
+        //{
+        //    return FindCount(SearchWhere(key), null, null, 0, 0);
+        //}
+
+        /// <summary>构造搜索条件</summary>
+        /// <param name="key">关键字</param>
+        /// <returns></returns>
+        private static String SearchWhere(String key)
+        {
+            // WhereExpression重载&和|运算符，作为And和Or的替代
+            var exp = new WhereExpression();
+
+            // SearchWhereByKeys系列方法用于构建针对字符串字段的模糊搜索
+            if (!String.IsNullOrEmpty(key)) SearchWhereByKeys(exp.Builder, key);
+
+            // 以下仅为演示，2、3行是同一个意思的不同写法，Field（继承自FieldItem）重载了==、!=、>、<、>=、<=等运算符（第4行）
+            //exp &= _.Name == "testName"
+            //    & !String.IsNullOrEmpty(key) & _.Name == key
+            //    .AndIf(!String.IsNullOrEmpty(key), _.Name == key)
+            //    | _.ID > 0;
+
+            return exp;
+        }
+        #endregion
+
+        #region 扩展操作
+        #endregion
+
+        #region 业务
+        #endregion
+    }
+}
---
+++ NewLife.CommonEntity/帮助/手册.cs
@@ -0,0 +1,156 @@
+﻿/*
+ * XCoder v4.8.4548.28140
+ * 作者：nnhy/NEWLIFE
+ * 时间：2012-06-18 11:05:27
+ * 版权：版权所有 (C) 新生命开发团队 2012
+*/
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using XCode;
+using XCode.Configuration;
+using XCode.DataAccessLayer;
+
+namespace NewLife.CommonEntity
+{
+    /// <summary>手册</summary>
+    [Serializable]
+    [DataObject]
+    [Description("手册")]
+    [BindIndex("IX_Manual_Url", false, "Url")]
+    [BindIndex("PK_Manual", true, "ID")]
+    [BindTable("Manual", Description = "手册", ConnName = "Manual", DbType = DatabaseType.SqlServer)]
+    public partial class Manual<TEntity> : IManual
+    {
+        #region 属性
+        private Int32 _ID;
+        /// <summary>编号</summary>
+        [DisplayName("编号")]
+        [Description("编号")]
+        [DataObjectField(true, true, false, 10)]
+        [BindColumn(1, "ID", "编号", null, "int", 10, 0, false)]
+        public virtual Int32 ID
+        {
+            get { return _ID; }
+            set { if (OnPropertyChanging("ID", value)) { _ID = value; OnPropertyChanged("ID"); } }
+        }
+
+        private String _Url;
+        /// <summary>资源</summary>
+        [DisplayName("资源")]
+        [Description("资源")]
+        [DataObjectField(false, false, true, 100)]
+        [BindColumn(2, "Url", "资源", null, "nvarchar(100)", 0, 0, true)]
+        public virtual String Url
+        {
+            get { return _Url; }
+            set { if (OnPropertyChanging("Url", value)) { _Url = value; OnPropertyChanged("Url"); } }
+        }
+
+        private String _Summary;
+        /// <summary>摘要</summary>
+        [DisplayName("摘要")]
+        [Description("摘要")]
+        [DataObjectField(false, false, false, 200)]
+        [BindColumn(3, "Summary", "摘要", null, "nvarchar(200)", 0, 0, true)]
+        public virtual String Summary
+        {
+            get { return _Summary; }
+            set { if (OnPropertyChanging("Summary", value)) { _Summary = value; OnPropertyChanged("Summary"); } }
+        }
+
+        private String _Content;
+        /// <summary>内容</summary>
+        [DisplayName("内容")]
+        [Description("内容")]
+        [DataObjectField(false, false, true, 1073741823)]
+        [BindColumn(4, "Content", "内容", null, "ntext", 0, 0, true)]
+        public virtual String Content
+        {
+            get { return _Content; }
+            set { if (OnPropertyChanging("Content", value)) { _Content = value; OnPropertyChanged("Content"); } }
+        }
+		#endregion
+
+        #region 获取/设置 字段值
+        /// <summary>
+        /// 获取/设置 字段值。
+        /// 一个索引，基类使用反射实现。
+        /// 派生实体类可重写该索引，以避免反射带来的性能损耗
+        /// </summary>
+        /// <param name="name">字段名</param>
+        /// <returns></returns>
+        public override Object this[String name]
+        {
+            get
+            {
+                switch (name)
+                {
+                    case "ID" : return _ID;
+                    case "Url" : return _Url;
+                    case "Summary" : return _Summary;
+                    case "Content" : return _Content;
+                    default: return base[name];
+                }
+            }
+            set
+            {
+                switch (name)
+                {
+                    case "ID" : _ID = Convert.ToInt32(value); break;
+                    case "Url" : _Url = Convert.ToString(value); break;
+                    case "Summary" : _Summary = Convert.ToString(value); break;
+                    case "Content" : _Content = Convert.ToString(value); break;
+                    default: base[name] = value; break;
+                }
+            }
+        }
+        #endregion
+
+        #region 字段名
+        /// <summary>取得手册字段信息的快捷方式</summary>
+        public class _
+        {
+            ///<summary>编号</summary>
+            public static readonly Field ID = FindByName("ID");
+
+            ///<summary>资源</summary>
+            public static readonly Field Url = FindByName("Url");
+
+            ///<summary>摘要</summary>
+            public static readonly Field Summary = FindByName("Summary");
+
+            ///<summary>内容</summary>
+            public static readonly Field Content = FindByName("Content");
+
+            static Field FindByName(String name) { return Meta.Table.FindByName(name); }
+        }
+        #endregion
+    }
+
+    /// <summary>手册接口</summary>
+    public partial interface IManual
+    {
+        #region 属性
+        /// <summary>编号</summary>
+        Int32 ID { get; set; }
+
+        /// <summary>资源</summary>
+        String Url { get; set; }
+
+        /// <summary>摘要</summary>
+        String Summary { get; set; }
+
+        /// <summary>内容</summary>
+        String Content { get; set; }
+        #endregion
+
+        #region 获取/设置 字段值
+        /// <summary>获取/设置 字段值。</summary>
+        /// <param name="name">字段名</param>
+        /// <returns></returns>
+        Object this[String name] { get; set; }
+        #endregion
+    }
+}
