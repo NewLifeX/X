3678c99af223c507c261a573d810fba9aea2cb0c
nnhy <nnhy@vip.qq.com>
2012-07-24 10:13:23 +0000

    完成同步机制的拟订

---
+++ XCode/Sync/ISyncMaster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NewLife.Reflection;
+
+namespace XCode.Sync
+{
+    /// <summary>同步架构主方</summary>
+    public interface ISyncMaster
+    {
+        #region 方法
+        Object[] CheckUpdate(DateTime last, Int32 start, Int32 max);
+
+        IIndexAccessor FindByKey(Object key);
+
+        IIndexAccessor[] FindAllByKeys(Object[] key);
+        #endregion
+    }
+}
---
+++ XCode/Sync/ISyncSlave.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XCode.Sync
+{
+    /// <summary>同步框架从方</summary>
+    public interface ISyncSlave
+    {
+        #region 属性
+        /// <summary>最后修改时间。包括修改同步状态为假删除</summary>
+        DateTime LastUpdate { get; set; }
+
+        /// <summary>最后同步时间。包括向主方询问数据是否已删除</summary>
+        DateTime LastSync { get; set; }
+
+        /// <summary>同步状态。默认0添加1删除2</summary>
+        Int32 SyncStatus { get; set; }
+        #endregion
+    }
+}
