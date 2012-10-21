dec308fe6a45f6eab5b4c47999e68b4145405817
nnhy <nnhy@vip.qq.com>
2012-10-21 03:18:36 +0000

    为X组件增加单元测试项目，将来统一使用单元测试

---
+++ NewLife.Core.Test/Serialization/BinaryTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewLife.Serialization;
+
+namespace NewLife.Core.Test.Serialization
+{
+    /// <summary>
+    /// BinaryWriterTest 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class BinaryTest
+    {
+        public BinaryTest()
+        {
+            //
+            //TODO: 在此处添加构造函数逻辑
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///获取或设置测试上下文，该上下文提供
+        ///有关当前测试运行及其功能的信息。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 附加测试特性
+        //
+        // 编写测试时，可以使用以下附加特性:
+        //
+        // 在运行类中的第一个测试之前使用 ClassInitialize 运行代码
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // 在类中的所有测试都已运行之后使用 ClassCleanup 运行代码
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // 在运行每个测试之前，使用 TestInitialize 来运行代码
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 在每个测试运行完之后，使用 TestCleanup 来运行代码
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestWriter()
+        {
+            var obj = SimpleObj.Create();
+
+            var writer = new BinaryWriterX();
+            writer.WriteObject(obj);
+
+            Assert.AreNotEqual(writer.Stream.Length, 0);
+        }
+    }
+}
---
+++ NewLife.Core.Test/Serialization/SimpleObj.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace NewLife.Core.Test.Serialization
+{
+    class SimpleObj
+    {
+        #region 属性
+        private Boolean _B;
+        /// <summary>属性说明</summary>
+        public Boolean B { get { return _B; } set { _B = value; } }
+
+        private Char _C;
+        /// <summary>属性说明</summary>
+        public Char C { get { return _C; } set { _C = value; } }
+
+        private SByte _SB;
+        /// <summary>属性说明</summary>
+        public SByte SB { get { return _SB; } set { _SB = value; } }
+
+        private Byte _Bt;
+        /// <summary>属性说明</summary>
+        public Byte Bt { get { return _Bt; } set { _Bt = value; } }
+
+        private Int16 _I16;
+        /// <summary>属性说明</summary>
+        public Int16 I16 { get { return _I16; } set { _I16 = value; } }
+
+        private UInt16 _U16;
+        /// <summary>属性说明</summary>
+        public UInt16 U16 { get { return _U16; } set { _U16 = value; } }
+
+        private Int32 _I32;
+        /// <summary>属性说明</summary>
+        public Int32 I32 { get { return _I32; } set { _I32 = value; } }
+
+        private UInt32 _U32;
+        /// <summary>属性说明</summary>
+        public UInt32 U32 { get { return _U32; } set { _U32 = value; } }
+
+        private Int64 _I64;
+        /// <summary>属性说明</summary>
+        public Int64 I64 { get { return _I64; } set { _I64 = value; } }
+
+        private UInt64 _U64;
+        /// <summary>属性说明</summary>
+        public UInt64 U64 { get { return _U64; } set { _U64 = value; } }
+
+        private Single _S;
+        /// <summary>属性说明</summary>
+        public Single S { get { return _S; } set { _S = value; } }
+
+        private Double _D;
+        /// <summary>属性说明</summary>
+        public Double D { get { return _D; } set { _D = value; } }
+
+        private Decimal _Dec;
+        /// <summary>属性说明</summary>
+        public Decimal Dec { get { return _Dec; } set { _Dec = value; } }
+
+        private DateTime _Dt;
+        /// <summary>属性说明</summary>
+        public DateTime Dt { get { return _Dt; } set { _Dt = value; } }
+
+        private String _Str;
+        /// <summary>属性说明</summary>
+        public String Str { get { return _Str; } set { _Str = value; } }
+        #endregion
+
+        #region 方法
+        public static SimpleObj Create()
+        {
+            var obj = new SimpleObj();
+            obj.B = true;
+
+            return obj;
+        }
+
+        public Stream GetBinaryStream()
+        {
+            var ms = new MemoryStream();
+            var writer = new BinaryWriter(ms);
+            writer.Write(B);
+
+            ms.Position = 0;
+
+            return ms;
+        }
+        #endregion
+    }
+}
