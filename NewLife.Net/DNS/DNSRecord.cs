f675f5d0263d6bb9b07d4f4db2eee74b4e663f6b
nnhy <nnhy@vip.qq.com>
2012-10-24 02:12:57 +0000

    这里是设计上的一个难点，也是一个亮点！ 按照DNS协议，长度之后应该是一个由该长度指定的数据区域， 如果我们按照一般二进制的做法，这里应该为这个数据区域专门定义多个不同的实体类， DNS记录的DNSQueryType可以确定应该采用哪个实体类来进行读取。 但是这样就让实体类变得非常难用了，难道还需要dr.Body.Address？不如直接的dr.Address来得好！
    因此，读写器需要支持备份恢复机制，我们在中途更换了数据流，让后续的读取按照新数据流读取。
    
    不过话说回来，如果仅仅因为这个目的，也是不需要切换数据流的，好像是因为字符串！
    DNS字符串采用了一种很恶心的避免冗余的方式，它指定的位移是相对于局部数据区域的位移，所以必须更换数据流，让其以为从0开始。
    
    谨记：DNSRecord只是基类，拥有众多派生类，各自的扩展字段都不一样！

--- NewLife.Net/DNS/DNSRecord.cs
+++ NewLife.Net/DNS/DNSRecord.cs
@@ -22,14 +22,12 @@ namespace NewLife.Net.DNS
         /// <summary>协议组</summary>
         public DNSQueryClass Class { get { return _Class; } set { _Class = value; } }
 
-        //private Int32 _TTL;
-        ///// <summary>生存时间。4字节，指示RDATA中的资源记录在缓存的生存时间。</summary>
-        //public TimeSpan TTL { get { return new TimeSpan(0, 0, _TTL); } set { _TTL = (Int32)value.TotalSeconds; } }
-
         private TimeSpan _TTL;
         /// <summary>生存时间。4字节，指示RDATA中的资源记录在缓存的生存时间。</summary>
         public TimeSpan TTL { get { return _TTL; } set { _TTL = value; } }
 
+        /// <summary>长度</summary>
+        /// <remarks>后面应该是一个数据区域，留给派生类</remarks>
         private Int16 _Length;
 
         //[FieldSize("_Length")]
@@ -43,6 +41,11 @@ namespace NewLife.Net.DNS
         private String _Text;
         /// <summary>文本信息</summary>
         public virtual String Text { get { return _Text; } set { _Text = value; } }
+        #endregion
+
+        #region 静态
+        const String LENGTH = "_Length";
+        const String POSITION = "Position";
         #endregion
 
         #region IAccessor 成员
@@ -59,11 +62,25 @@ namespace NewLife.Net.DNS
 
         void reader_OnMemberReaded(object sender, ReadMemberEventArgs e)
         {
-            if (e.Member.Name == "_Length" && (Int16)e.Value > 0)
+            /*
+             * 这里是设计上的一个难点，也是一个亮点！
+             * 按照DNS协议，长度之后应该是一个由该长度指定的数据区域，
+             * 如果我们按照一般二进制的做法，这里应该为这个数据区域专门定义多个不同的实体类，
+             * DNS记录的DNSQueryType可以确定应该采用哪个实体类来进行读取。
+             * 但是这样就让实体类变得非常难用了，难道还需要dr.Body.Address？不如直接的dr.Address来得好！
+             * 
+             * 因此，读写器需要支持备份恢复机制，我们在中途更换了数据流，让后续的读取按照新数据流读取。
+             * 
+             * 不过话说回来，如果仅仅因为这个目的，也是不需要切换数据流的，好像是因为字符串！
+             * DNS字符串采用了一种很恶心的避免冗余的方式，它指定的位移是相对于局部数据区域的位移，所以必须更换数据流，让其以为从0开始。
+             * 
+             * 谨记：DNSRecord只是基类，拥有众多派生类，各自的扩展字段都不一样！
+             */
+            if (e.Member.Name == LENGTH && (Int16)e.Value > 0)
             {
                 var reader = sender as IReader2;
                 // 记住数据流位置，读取字符串的时候需要用到
-                reader.Items["Position"] = reader.Stream.Position;
+                reader.Items[POSITION] = reader.Stream.Position;
                 var data = reader.ReadBytes((Int16)e.Value);
                 // 切换数据流，使用新数据流完成余下字段的序列化
                 reader.Backup();
@@ -79,7 +96,7 @@ namespace NewLife.Net.DNS
             if (_Length > 0)
             {
                 reader.Restore();
-                if (reader.Items.Contains("Position")) reader.Items.Remove("Position");
+                if (reader.Items.Contains(POSITION)) reader.Items.Remove(POSITION);
             }
             reader.OnMemberReaded -= new EventHandler<ReadMemberEventArgs>(reader_OnMemberReaded);
 
@@ -99,12 +116,12 @@ namespace NewLife.Net.DNS
 
         void writer_OnMemberWriting(object sender, WriteMemberEventArgs e)
         {
-            if (e.Name == "_Length")
+            if (e.Name == LENGTH)
             {
                 var writer = sender as IWriter;
                 // 记住数据流位置，读取字符串的时候需要用到
                 // +2是为了先对长度进行占位，这个位置会影响DNS字符串的相对位置
-                writer.Items["Position"] = writer.Stream.Position + 2;
+                writer.Items[POSITION] = writer.Stream.Position + 2;
                 // 切换数据流，使用新数据流完成余下字段的序列化
                 writer.Backup();
                 writer.Stream = new MemoryStream();
@@ -117,7 +134,7 @@ namespace NewLife.Net.DNS
         bool IAccessor.WriteComplete(IWriter writer, bool success)
         {
             // 先处理这个再移除过滤，因为这里需要依据被过滤的项来判断
-            if (!writer.Settings.IgnoreMembers.Contains("_Length"))
+            if (!writer.Settings.IgnoreMembers.Contains(LENGTH))
             {
                 // 附加数据
                 var ms = writer.Stream;
@@ -126,7 +143,7 @@ namespace NewLife.Net.DNS
 
                 // 恢复环境
                 writer.Restore();
-                if (writer.Items.Contains("Position")) writer.Items.Remove("Position");
+                if (writer.Items.Contains(POSITION)) writer.Items.Remove(POSITION);
 
                 var wr = writer as IWriter2;
                 // 写入_Length和数据
