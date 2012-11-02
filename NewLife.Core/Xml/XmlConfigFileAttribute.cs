bdcbfc39c0eb1dace5730bce2cabead16ef0bf55
nnhy <nnhy@vip.qq.com>
2012-11-02 09:05:38 +0000

    必须填写至少10个字的日志

--- NewLife.Core/Xml/XmlConfigFileAttribute.cs
+++ NewLife.Core/Xml/XmlConfigFileAttribute.cs
@@ -12,28 +12,15 @@ namespace NewLife.Xml
         /// <summary>配置文件名</summary>
         public String FileName { get { return _FileName; } set { _FileName = value; } }
 
-        //private Boolean _AutoReload;
-        ///// <summary>当文件改变时是否重新加载</summary>
-        //public Boolean AutoReload { get { return _AutoReload; } set { _AutoReload = value; } }
-
         private Int32 _ReloadTime;
-        /// <summary>重新加载时间。单位：秒</summary>
+        /// <summary>重新加载时间。单位：毫秒</summary>
         public Int32 ReloadTime { get { return _ReloadTime; } set { _ReloadTime = value; } }
 
         /// <summary>指定配置文件名</summary>
         /// <param name="fileName"></param>
         public XmlConfigFileAttribute(String fileName) { FileName = fileName; }
 
-        ///// <summary>指定配置文件名和是否自动重新加载</summary>
-        ///// <param name="fileName"></param>
-        ///// <param name="autoReload"></param>
-        //public XmlConfigFileAttribute(String fileName, Boolean autoReload)
-        //{
-        //    FileName = fileName;
-        //    AutoReload = autoReload;
-        //}
-
-        /// <summary>指定配置文件名和重新加载时间</summary>
+        /// <summary>指定配置文件名和重新加载时间（毫秒）</summary>
         /// <param name="fileName"></param>
         /// <param name="reloadTime"></param>
         public XmlConfigFileAttribute(String fileName, Int32 reloadTime)
