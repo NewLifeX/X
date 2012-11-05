7f16fbaef93e65dfe6a94ac9ea724a21c660be11
nnhy <nnhy@vip.qq.com>
2012-11-05 03:51:29 +0000

    必须关闭Normalization，否则字符串的\r\n会变为\n

--- NewLife.Core/Xml/XmlHelper.cs
+++ NewLife.Core/Xml/XmlHelper.cs
@@ -118,8 +118,11 @@ namespace NewLife.Xml
 
             var serial = new XmlSerializer(type);
             using (var reader = new StringReader(xml))
+            using (var xr = new XmlTextReader(reader))
             {
-                return serial.Deserialize(reader);
+                // 必须关闭Normalization，否则字符串的\r\n会变为\n
+                //xr.Normalization = true;
+                return serial.Deserialize(xr);
             }
         }
 
@@ -148,8 +151,11 @@ namespace NewLife.Xml
 
             var serial = new XmlSerializer(type);
             using (var reader = new StreamReader(stream, encoding))
+            using (var xr = new XmlTextReader(reader))
             {
-                return serial.Deserialize(reader);
+                // 必须关闭Normalization，否则字符串的\r\n会变为\n
+                //xr.Normalization = true;
+                return serial.Deserialize(xr);
             }
         }
 
