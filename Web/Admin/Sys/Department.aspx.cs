a5a49fdc57bc74026a25cc425f6ff430c8f70f3e
nnhy <nnhy@vip.qq.com>
2012-12-20 03:56:26 +0000

    部门和用户的管理页面，使用者根据需要使用

---
+++ Web/Admin/Sys/Department.aspx.cs
@@ -0,0 +1,13 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NewLife.CommonEntity;
+
+public partial class Common_Department : MyEntityList<Department>
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+}
---
+++ Web/Admin/Sys/UserForm.aspx.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NewLife.Log;
+using NewLife.Web;
+using NewLife.CommonEntity;
+
+public partial class Common_UserForm : MyEntityForm<User>
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+}
