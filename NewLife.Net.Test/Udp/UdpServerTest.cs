48efe8c24e91df7cd8ec9e80fade76b8c122a1ae
Stone <nnhy@vip.qq.com>
2013-06-27 07:22:49 +0000

    增加单元测试UdpServerTest

---
+++ NewLife.Net.Test/Udp/UdpServerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewLife.Net.Udp;
+
+namespace NewLife.Net.Test.Udp
+{
+    [TestClass]
+    public class UdpServerTest
+    {
+        [TestMethod]
+        public void CtorTest()
+        {
+            var rnd = new Random((Int32)DateTime.Now.Ticks);
+
+            var address = new IPAddress(rnd.Next(1, 0xFFFF));
+            var port = rnd.Next(1, 0xFFFF);
+
+            var server = new UdpServer(port);
+
+            Assert.AreEqual(ProtocolType.Udp, server.ProtocolType);
+            Assert.AreEqual(ProtocolType.Udp, server.LocalUri.ProtocolType);
+            Assert.AreEqual(port, server.Port);
+        }
+    }
+}
