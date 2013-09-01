d707cb437cb9b1c81ebbd63352be74c3afb1a56b
nnhy <nnhy@vip.qq.com>
2013-09-01 15:08:55 +0000

    v3.9.2013.0901   明确PathHelper.EnsureDirectory的用法，基础类库的用法应该有明确的用途，而不是通过某些小伎俩去让人猜测

--- NewLife.Core/IO/PathHelper.cs
+++ NewLife.Core/IO/PathHelper.cs
@@ -6,7 +6,8 @@ namespace System.IO
     {
         #region 路径操作辅助
         /// <summary>获取文件或目录的全路径，过滤相对目录</summary>
-        /// <param name="path"></param>
+        /// <remarks>不确保目录后面一定有分隔符，是否有分隔符由原始路径末尾决定</remarks>
+        /// <param name="path">文件或目录</param>
         /// <returns></returns>
         public static String GetFullPath(this String path)
         {
@@ -18,17 +19,33 @@ namespace System.IO
         }
 
         /// <summary>确保目录存在，若不存在则创建</summary>
-        /// <param name="path">文件路径或目录路径（斜杠结尾）</param>
+        /// <param name="path">文件路径或目录路径，斜杠结尾的路径一定是目录，无视第二参数</param>
+        /// <param name="isfile">该路径是否是否文件路径。文件路径需要取目录部分</param>
         /// <returns></returns>
-        public static String EnsureDirectory(this String path)
+        public static String EnsureDirectory(this String path, Boolean isfile = false)
         {
             if (String.IsNullOrEmpty(path)) return path;
 
-            path = path.GetFullPath();
-            if (File.Exists(path) || Directory.Exists(path)) return path;
+            var dir = path.GetFullPath();
+            if (File.Exists(dir) || Directory.Exists(dir)) return dir;
 
-            var dir = path;
-            if (dir[dir.Length - 1] != Path.PathSeparator) dir = Path.GetDirectoryName(path);
+            // 斜杠结尾的路径一定是目录，无视第二参数
+            if (dir[dir.Length - 1] == Path.PathSeparator)
+                dir = Path.GetDirectoryName(path);
+            else if (isfile)
+                dir = Path.GetDirectoryName(path);
+
+            /*!!! 基础类库的用法应该有明确的用途，而不是通过某些小伎俩去让人猜测 !!!*/
+
+            //// 如果有圆点说明可能是文件
+            //var p1 = dir.LastIndexOf('.');
+            //if (p1 >= 0)
+            //{
+            //    // 要么没有斜杠，要么圆点必须在最后一个斜杠后面
+            //    var p2 = dir.LastIndexOf('\\');
+            //    if (p2 < 0 || p2 < p1) dir = Path.GetDirectoryName(path);
+            //}
+
             if (!String.IsNullOrEmpty(dir) && !Directory.Exists(dir)) Directory.CreateDirectory(dir);
 
             return path;
