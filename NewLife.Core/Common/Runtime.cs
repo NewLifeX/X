86c691e98d89ecf578c77b883e2202db2ab155d8
Stone <nnhy@vip.qq.com>
2014-06-21 08:39:33 +0000

    有专门的PEImage类，这里不再使用

--- NewLife.Core/Common/Runtime.cs
+++ NewLife.Core/Common/Runtime.cs
@@ -52,55 +52,55 @@ namespace NewLife
             }
         }
 
-        /// <summary>获取PE文件类型。扩展方法</summary>
-        /// <param name="e"></param>
-        /// <returns></returns>
-        public static PEFileKinds GetPEFileKinds(this MemberInfo e)
-        {
-            return GetPEFileKinds(Path.GetFullPath(e.Module.Assembly.Location));
+        ///// <summary>获取PE文件类型。扩展方法</summary>
+        ///// <param name="e"></param>
+        ///// <returns></returns>
+        //public static PEFileKinds GetPEFileKinds(this MemberInfo e)
+        //{
+        //    return GetPEFileKinds(Path.GetFullPath(e.Module.Assembly.Location));
 
-        }
+        //}
 
-        /// <summary>Parses the PE header and determines whether the given assembly is a console application.</summary>
-        /// <param name="assemblyPath">The path of the assembly to check.</param>
-        /// <remarks>The magic numbers in this method are extracted from the PE/COFF file
-        /// format specification available from http://www.microsoft.com/whdc/system/platform/firmware/pecoff.mspx
-        /// </remarks>
-        static PEFileKinds GetPEFileKinds(string assemblyPath)
-        {
-            using (var s = new FileStream(assemblyPath, FileMode.Open, FileAccess.Read))
-            {
-                return GetPEFileKinds(s);
-            }
-        }
+        ///// <summary>分析PE头并检测程序集类型</summary>
+        ///// <param name="assemblyPath">The path of the assembly to check.</param>
+        ///// <remarks>The magic numbers in this method are extracted from the PE/COFF file
+        ///// format specification available from http://www.microsoft.com/whdc/system/platform/firmware/pecoff.mspx
+        ///// </remarks>
+        //static PEFileKinds GetPEFileKinds(string assemblyPath)
+        //{
+        //    using (var s = new FileStream(assemblyPath, FileMode.Open, FileAccess.Read))
+        //    {
+        //        return GetPEFileKinds(s);
+        //    }
+        //}
 
-        private static PEFileKinds GetPEFileKinds(Stream s)
-        {
-            var rawPeSignatureOffset = new byte[4];
-            s.Seek(0x3c, SeekOrigin.Begin);
-            s.Read(rawPeSignatureOffset, 0, 4);
-            int peSignatureOffset = rawPeSignatureOffset[0];
-            peSignatureOffset |= rawPeSignatureOffset[1] << 8;
-            peSignatureOffset |= rawPeSignatureOffset[2] << 16;
-            peSignatureOffset |= rawPeSignatureOffset[3] << 24;
-            var coffHeader = new byte[24];
-            s.Seek(peSignatureOffset, SeekOrigin.Begin);
-            s.Read(coffHeader, 0, 24);
-            byte[] signature = { (byte)'P', (byte)'E', (byte)'\0', (byte)'\0' };
-            for (int index = 0; index < 4; index++)
-            {
-                if (coffHeader[index] != signature[index]) throw new InvalidOperationException("Attempted to check a non PE file for the console subsystem!");
-            }
-            var subsystemBytes = new byte[2];
-            s.Seek(68, SeekOrigin.Current);
-            s.Read(subsystemBytes, 0, 2);
-            int subSystem = subsystemBytes[0] | subsystemBytes[1] << 8;
-            return
-                // http://support.microsoft.com/kb/90493
-                subSystem == 3 ? PEFileKinds.ConsoleApplication :
-                subSystem == 2 ? PEFileKinds.WindowApplication :
-                PEFileKinds.Dll; /*IMAGE_SUBSYSTEM_WINDOWS_CUI*/
-        }
+        //private static PEFileKinds GetPEFileKinds(Stream stream)
+        //{
+        //    var rawPeSignatureOffset = new byte[4];
+        //    stream.Seek(0x3c, SeekOrigin.Begin);
+        //    stream.Read(rawPeSignatureOffset, 0, 4);
+        //    int peSignatureOffset = rawPeSignatureOffset[0];
+        //    peSignatureOffset |= rawPeSignatureOffset[1] << 8;
+        //    peSignatureOffset |= rawPeSignatureOffset[2] << 16;
+        //    peSignatureOffset |= rawPeSignatureOffset[3] << 24;
+        //    var coffHeader = new byte[24];
+        //    stream.Seek(peSignatureOffset, SeekOrigin.Begin);
+        //    stream.Read(coffHeader, 0, 24);
+        //    byte[] signature = { (byte)'P', (byte)'E', (byte)'\0', (byte)'\0' };
+        //    for (int index = 0; index < 4; index++)
+        //    {
+        //        if (coffHeader[index] != signature[index]) throw new InvalidOperationException("Attempted to check a non PE file for the console subsystem!");
+        //    }
+        //    var subsystemBytes = new byte[2];
+        //    stream.Seek(68, SeekOrigin.Current);
+        //    stream.Read(subsystemBytes, 0, 2);
+        //    int subSystem = subsystemBytes[0] | subsystemBytes[1] << 8;
+        //    return
+        //        // http://support.microsoft.com/kb/90493
+        //        subSystem == 3 ? PEFileKinds.ConsoleApplication :
+        //        subSystem == 2 ? PEFileKinds.WindowApplication :
+        //        PEFileKinds.Dll; /*IMAGE_SUBSYSTEM_WINDOWS_CUI*/
+        //}
         #endregion
 
         #region Web环境
