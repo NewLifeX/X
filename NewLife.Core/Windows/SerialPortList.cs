c781e0312a5e66e6df3620b60820c597407bc7e6
Stone <nnhy@vip.qq.com>
2014-10-17 15:49:44 +0000

    增加串口配置类 增加串口列表控件 编译通过，未测试

---
+++ NewLife.Core/Windows/SerialPortList.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+using NewLife.Log;
+using NewLife.Net;
+
+namespace NewLife.Windows
+{
+    /// <summary>串口列表控件</summary>
+    public partial class SerialPortList : UserControl
+    {
+        private SerialTransport _Port;
+        /// <summary>端口</summary>
+        public SerialTransport Port { get { return _Port; } set { _Port = value; } }
+
+        /// <summary>选择的端口</summary>
+        public String SelectedPort { get { return cbName.SelectedItem + ""; } set { } }
+
+        #region 构造
+        /// <summary></summary>
+        public SerialPortList()
+        {
+            InitializeComponent();
+        }
+
+        private void SerialPortList_Load(object sender, EventArgs e)
+        {
+            LoadInfo();
+        }
+        #endregion
+
+        #region 加载保存信息
+        void LoadInfo()
+        {
+            ShowPorts();
+
+            var ti = FindMenu("数据位");
+            BindMenu(mi数据位, new Int32[] { 5, 6, 7, 8 }, On数据位Click);
+            BindMenu(mi停止位, Enum.GetValues(typeof(StopBits)), On停止位Click);
+            BindMenu(mi校验, Enum.GetValues(typeof(Parity)), On校验Click);
+
+            cbBaundrate.DataSource = new Int32[] { 1200, 2400, 4800, 9600, 14400, 19200, 38400, 56000, 57600, 115200, 194000 };
+
+            var cfg = SerialPortConfig.Current;
+            cbName.SelectedItem = cfg.PortName;
+            cbBaundrate.SelectedItem = cfg.BaudRate;
+            SetMenuItem(mi数据位, cfg.DataBits);
+            SetMenuItem(mi停止位, cfg.StopBits);
+            SetMenuItem(mi校验, cfg.Parity);
+
+            //cbEncoding.DataSource = new String[] { Encoding.Default.WebName, Encoding.ASCII.WebName, Encoding.UTF8.WebName };
+            // 添加编码子菜单
+            var encs = new Encoding[] { Encoding.Default, Encoding.ASCII, Encoding.UTF8 };
+        }
+
+        void SaveInfo()
+        {
+            try
+            {
+                var cfg = SerialPortConfig.Current;
+                cfg.PortName = cbName.SelectedItem + "";
+                cfg.BaudRate = (Int32)cbBaundrate.SelectedItem;
+                //cfg.DataBits = (Int32)cbDataBit.SelectedItem;
+                //cfg.StopBits = (StopBits)cbStopBit.SelectedItem;
+                //cfg.Parity = (Parity)cbParity.SelectedItem;
+                //cfg.Encoding = (Encoding)cbEncoding.SelectedItem;
+                //cfg.WebEncoding = cbEncoding.SelectedItem + "";
+
+                //cfg.HexSend = chkHEXSend.Checked;
+                //cfg.HexShow = chkHEXShow.Checked;
+
+                cfg.Save();
+            }
+            catch (Exception ex)
+            {
+                XTrace.WriteException(ex);
+            }
+        }
+
+        String _ports = null;
+        DateTime _nextport = DateTime.MinValue;
+        /// <summary>下拉框显示串口</summary>
+        public void ShowPorts()
+        {
+            if (_nextport > DateTime.Now) return;
+            _nextport = DateTime.Now.AddSeconds(1);
+
+            var ps = SerialTransport.GetPortNames();
+            var str = String.Join(",", ps);
+            // 如果端口有所改变，则重新绑定
+            if (_ports != str)
+            {
+                _ports = str;
+                var old = cbName.SelectedItem + "";
+                cbName.DataSource = ps;
+                if (!String.IsNullOrEmpty(old) && Array.IndexOf(ps, old) >= 0) cbName.SelectedItem = old;
+            }
+        }
+        #endregion
+
+        #region 菜单设置
+        void On数据位Click(object sender, EventArgs e)
+        {
+
+        }
+        void On停止位Click(object sender, EventArgs e)
+        {
+
+        }
+        void On校验Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 窗体
+        //public new Boolean Focus() { return cbName.Focus(); }
+
+        /// <summary>连接串口</summary>
+        public void Connect()
+        {
+            var name = cbName.SelectedItem + "";
+            if (String.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("请选择串口！", this.Text);
+                cbName.Focus();
+                return;
+            }
+            var p = name.IndexOf("(");
+            if (p > 0) name = name.Substring(0, p);
+
+            SaveInfo();
+            var cfg = SerialPortConfig.Current;
+
+            if (Port == null)
+                Port = new SerialTransport();
+            else
+                // 如果上次没有关闭，则关闭
+                Port.Close();
+            Port.PortName = name;
+            Port.BaudRate = cfg.BaudRate;
+            Port.Parity = cfg.Parity;
+            Port.DataBits = cfg.DataBits;
+            Port.StopBits = cfg.StopBits;
+            Port.Open();
+        }
+
+        /// <summary>断开串口连接</summary>
+        public void Disconnect()
+        {
+            if (Port != null) Port.Close();
+
+            //this.Enabled = true;
+            ShowPorts();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.Enabled) ShowPorts();
+        }
+        #endregion
+
+        #region 菜单辅助
+        ToolStripItem FindMenu(String name)
+        {
+            return contextMenuStrip1.Items.Find(name, false)[0];
+        }
+
+        void BindMenu(ToolStripMenuItem ti, IEnumerable em, EventHandler handler)
+        {
+            foreach (var item in em)
+            {
+                var tsi = ti.DropDownItems.Add(item + "");
+                tsi.Tag = item;
+                tsi.Click += handler;
+            }
+        }
+
+        void SetMenuItem(ToolStripMenuItem ti, Object value)
+        {
+            foreach (ToolStripMenuItem item in ti.DropDownItems)
+            {
+                item.Checked = item + "" == value + "";
+            }
+        }
+        #endregion
+    }
+}
---
+++ NewLife.Core/Windows/SerialPortList.designer.cs
@@ -0,0 +1,143 @@
+﻿namespace NewLife.Windows
+{
+    partial class SerialPortList
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbBaundrate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbName = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mi数据位 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mi停止位 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mi校验 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbBaundrate
+            // 
+            this.cbBaundrate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBaundrate.FormattingEnabled = true;
+            this.cbBaundrate.Location = new System.Drawing.Point(274, 3);
+            this.cbBaundrate.Name = "cbBaundrate";
+            this.cbBaundrate.Size = new System.Drawing.Size(62, 20);
+            this.cbBaundrate.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "波特率：";
+            // 
+            // cbName
+            // 
+            this.cbName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbName.FormattingEnabled = true;
+            this.cbName.Location = new System.Drawing.Point(41, 3);
+            this.cbName.Name = "cbName";
+            this.cbName.Size = new System.Drawing.Size(162, 20);
+            this.cbName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "端口：";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mi数据位,
+            this.mi停止位,
+            this.mi校验});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 92);
+            // 
+            // mi数据位
+            // 
+            this.mi数据位.Name = "mi数据位";
+            this.mi数据位.Size = new System.Drawing.Size(152, 22);
+            this.mi数据位.Text = "数据位";
+            // 
+            // mi停止位
+            // 
+            this.mi停止位.Name = "mi停止位";
+            this.mi停止位.Size = new System.Drawing.Size(152, 22);
+            this.mi停止位.Text = "停止位";
+            // 
+            // mi校验
+            // 
+            this.mi校验.Name = "mi校验";
+            this.mi校验.Size = new System.Drawing.Size(152, 22);
+            this.mi校验.Text = "校验";
+            // 
+            // SerialPortList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.cbBaundrate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbName);
+            this.Controls.Add(this.label3);
+            this.Name = "SerialPortList";
+            this.Size = new System.Drawing.Size(341, 29);
+            this.Load += new System.EventHandler(this.SerialPortList_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbBaundrate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mi数据位;
+        private System.Windows.Forms.ToolStripMenuItem mi停止位;
+        private System.Windows.Forms.ToolStripMenuItem mi校验;
+
+    }
+}
