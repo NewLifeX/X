ce5bb81c7a5b6ba43f24a531f5b14088b44ddc6b
Stone <nnhy@vip.qq.com>
2014-11-02 04:26:27 +0000

    挂载发送框的右键菜单

--- XCoder/XCom/FrmMain.designer.cs
+++ XCoder/XCom/FrmMain.designer.cs
@@ -115,6 +115,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtSend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(33)))), ((int)(((byte)(28)))));
+            this.txtSend.ContextMenuStrip = this.menuSend;
             this.txtSend.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.txtSend.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
             this.txtSend.HideSelection = false;
