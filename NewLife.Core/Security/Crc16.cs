c115dd6b17be9edaf756ead45652a72efb259c70
Stone <nnhy@vip.qq.com>
2014-12-08 16:25:19 +0000

    改善Crc的使用方式

--- NewLife.Core/Security/Crc16.cs
+++ NewLife.Core/Security/Crc16.cs
@@ -153,22 +153,42 @@ namespace NewLife.Security
             return crc.Value;
         }
 
-        /// <summary>计算校验码</summary>
+        /// <summary>计算数据流校验码</summary>
+        /// <param name="stream"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public static UInt16 Compute(Stream stream, Int32 count = 0)
+        {
+            var crc = new Crc16();
+            crc.Update(stream, count);
+            return crc.Value;
+        }
+
+        /// <summary>计算数据流校验码，指定起始位置和字节数偏移量</summary>
+        /// <remarks>
+        /// 一般用于计算数据包校验码，需要回过头去开始校验，并且可能需要跳过最后的校验码长度。
+        /// position小于0时，数据流从当前位置开始计算校验，流指针不后退；
+        /// position大于等于0时，数据流移到该位置开始计算校验，最后由count决定可能差几个字节不参与计算，流指针回到原始位置；
+        /// </remarks>
         /// <param name="stream"></param>
         /// <param name="position">如果大于等于0，则表示从该位置开始计算</param>
-        /// <param name="count"></param>
+        /// <param name="count">字节数偏移量，一般用负数表示</param>
         /// <returns></returns>
         public static UInt16 Compute(Stream stream, Int64 position = -1, Int32 count = 0)
         {
+            var p = stream.Position;
             if (position >= 0)
             {
-                count = (Int32)(stream.Position - position);
+                if (count > 0) count = -count;
+                count += (Int32)(stream.Position - position);
                 stream.Position = position;
             }
 
             var crc = new Crc16();
             crc.Update(stream, count);
-            return crc.Value;
+            var v = crc.Value;
+            if (position >= 0) stream.Position = p;
+            return v;
         }
     }
 }
