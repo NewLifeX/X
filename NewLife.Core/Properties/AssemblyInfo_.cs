64859fd711f90fd0c3213a49f95b9242b37bdec0
nnhy <nnhy@vip.qq.com>
2015-02-08 17:58:38 +0000

    完善版本信息

--- NewLife.Core/Properties/AssemblyInfo_.cs
+++ NewLife.Core/Properties/AssemblyInfo_.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyCompany("新生命开发团队")]
-[assembly: AssemblyCopyright("©2002-2014 新生命开发团队 http://www.NewLifeX.com")]
+[assembly: AssemblyCopyright("©2002-2015 新生命开发团队 http://www.NewLifeX.com")]
 [assembly: AssemblyTrademark("四叶草")]
