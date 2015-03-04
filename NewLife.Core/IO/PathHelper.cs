f28840ee733f40137c71fd2304e29cb76898879f
nnhy <nnhy@vip.qq.com>
2015-03-04 13:17:44 +0000

    合并多段路径时，允许原始路径为null

--- NewLife.Core/IO/PathHelper.cs
+++ NewLife.Core/IO/PathHelper.cs
@@ -83,7 +83,8 @@ namespace System.IO
         /// <returns></returns>
         public static String CombinePath(this String path, params String[] ps)
         {
-            if (path == null || ps == null || ps.Length < 1) return path;
+            if (ps == null || ps.Length < 1) return path;
+            if (path == null) path = String.Empty;
 
             //return Path.Combine(path, path2);
             foreach (var item in ps)
