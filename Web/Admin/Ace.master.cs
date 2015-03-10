05824e8136cb85d15444afd21cf36b7152f8aa7c
Stone <nnhy@vip.qq.com>
2015-03-10 15:35:11 +0000

    必须填写至少10个字的日志

---
+++ Web/Admin/Ace.master.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.UI.HtmlControls;
+using NewLife.CommonEntity;
+using NewLife.Reflection;
+
+public partial class ManagerPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //IManagePage manager = Reflect.GetValue(Page, "Manager", false) as IManagePage;
+        //if (manager != null) Navigation.Text = manager.Navigation;
+    }
+}
