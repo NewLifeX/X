15e54f833ee800c1596632c5292878afaa64a1dc
Stone <nnhy@vip.qq.com>
2015-03-22 12:53:02 +0000

    为新的消息框架增加消息接口

---
+++ NewLife.Core/Messaging/IMessage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NewLife.Messaging
+{
+    /// <summary>消息接口</summary>
+    public interface IMessage
+    {
+        /// <summary>从数据流中读取消息</summary>
+        /// <param name="stream"></param>
+        /// <returns>是否成功</returns>
+        Boolean Read(Stream stream);
+
+        /// <summary>把消息写入到数据流中</summary>
+        /// <param name="stream"></param>
+        void Write(Stream stream);
+    }
+
+    /// <summary>消息助手</summary>
+    public static class MessageHelper
+    {
+        /// <summary>获取消息的数据流表示。指针位置为0</summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static Stream GetStream(this IMessage msg)
+        {
+            var ms = new MemoryStream();
+            msg.Write(ms);
+            ms.Position = 0;
+            return ms;
+        }
+
+        /// <summary>获取消息的字节数组表示。</summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static Byte[] ToArray(this IMessage msg)
+        {
+            return msg.GetStream().ToArray();
+        }
+    }
+}
---
+++ NewLife.Core/Messaging/MessageBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NewLife.Serialization;
+
+namespace NewLife.Messaging
+{
+    /// <summary>消息基类</summary>
+    public abstract class MessageBase : IMessage
+    {
+        /// <summary>从数据流中读取消息</summary>
+        /// <param name="stream"></param>
+        /// <returns>是否成功</returns>
+        public virtual Boolean Read(Stream stream)
+        {
+            var fm = GetFormatter(true);
+            Object obj = this;
+            return fm.TryRead(this.GetType(), ref obj);
+        }
+
+        /// <summary>把消息写入到数据流中</summary>
+        /// <param name="stream"></param>
+        public virtual void Write(Stream stream)
+        {
+            var fm = GetFormatter(false);
+            fm.Write(this);
+        }
+
+        /// <summary>获取序列化器</summary>
+        /// <param name="isRead"></param>
+        /// <returns></returns>
+        protected virtual IFormatterX GetFormatter(Boolean isRead)
+        {
+            var binary = new Binary();
+
+            return binary;
+        }
+    }
+
+    ///// <summary>消息泛型基类</summary>
+    ///// <typeparam name="TMessage"></typeparam>
+    //public abstract class Message<TMessage> : MessageBase where TMessage : Message<TMessage>, new()
+    //{
+    //    public static TMessage Read(Stream stream)
+    //    {
+    //        var msg = new TMessage();
+    //        if (msg.Read(stream)) return msg;
+
+    //        return default(TMessage);
+    //    }
+    //}
+}
