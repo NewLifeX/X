fb42b088fe4db508a8d8212cccd653ed5058293c
nnhy <nnhy@vip.qq.com>
2015-03-26 16:42:29 +0000

    v5.3.2015.0327   网络库增加PacketStream，用于Tcp粘包拆包，测试通过

--- NewLife.Core/Properties/AssemblyInfo.cs
+++ NewLife.Core/Properties/AssemblyInfo.cs
@@ -30,10 +30,12 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.2.*")]
-[assembly: AssemblyFileVersion("5.2.2015.0314")]
+[assembly: AssemblyVersion("5.3.*")]
+[assembly: AssemblyFileVersion("5.3.2015.0327")]
 
 /*
+ * v5.3.2015.0327   网络库增加PacketStream，用于Tcp粘包拆包，测试通过
+ * 
  * v5.2.2015.0314   如果已经打开异步接收，还要使用同步接收，则同步Receive内部不再调用底层Socket，而是等待截走异步数据。
  * 
  * v5.2.2015.0307   增加Link，扩展WebClientX.GetLinks，用于从网页分析超链接以便于下载
