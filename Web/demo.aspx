c74a1cf7f588eb1d32e3a5cce6ca55c6e293263f
laoqiuChina <130990851@qq.com>
2015-04-02 22:02:18 +0000

    要增加这个引用

---
+++ Web/demo.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="demo.aspx.cs" Inherits="demo" %>
+
---
+++ Web/file.aspx.cs
@@ -0,0 +1,18 @@
+﻿using RazorEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class file : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string template = File.ReadAllText(Server.MapPath("~/file.cshtml"));
+        string result2 = Razor.Parse(template, new { Name = "World" }, "Sample");
+        Response.Write(result2);
+    }
+}
