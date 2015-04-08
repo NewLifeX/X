0274aba8642ec86ca9952f11cea8f60598344d29
Stone <nnhy@vip.qq.com>
2015-04-08 18:05:22 +0000

    增加日志控制器和菜单控制器，共用列表模板和表单目标 如果需要定制，可以为其创建专属模板

---
+++ NewLife.Cube/Admin/Controllers/LogController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.Mvc;
+using NewLife.Cube.Controllers;
+using XCode.Membership;
+
+namespace NewLife.Cube.Admin.Controllers
+{
+    [DisplayName("日志")]
+    public class LogController : EntityController<XCode.Membership.Log> { }
+}
---
+++ NewLife.Cube/Admin/Controllers/MenuController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.Mvc;
+using NewLife.Cube.Controllers;
+using XCode.Membership;
+
+namespace NewLife.Cube.Admin.Controllers
+{
+    [DisplayName("菜单")]
+    public class MenuController : EntityController<Menu> { }
+}
