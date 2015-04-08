0b4dc2c7f8dafa4e70f976799ac4f980a89925b4
nnhy <nnhy@vip.qq.com>
2015-04-08 11:35:16 +0000

    Main主界面出来了

--- NewLife.Cube/Admin/Index/IndexController.cs
+++ NewLife.Cube/Admin/Index/IndexController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Web;
 using System.Web.Mvc;
 using NewLife.Common;
-using NewLife.Cube.Controllers;
+using NewLife.Cube.Filters;
 using XCode.Membership;
 
 namespace NewLife.Cube.Admin.Controllers
 {
+    [EntityAuthorize]
     public class IndexController : Controller
     {
         public ActionResult Index()
@@ -13,6 +16,27 @@ namespace NewLife.Cube.Admin.Controllers
             ViewBag.User = ManageProvider.User;
             ViewBag.Config = SysConfig.Current;
             ViewBag.Main = Url.Action("Main");
+
+            return View();
+        }
+
+        public ActionResult Main(String act)
+        {
+            ViewBag.Act = act;
+            ViewBag.User = ManageProvider.User;
+            ViewBag.Config = SysConfig.Current;
+
+            String name = Request.ServerVariables["Server_SoftWare"];
+            if (String.IsNullOrEmpty(name)) name = Process.GetCurrentProcess().ProcessName;
+
+            // 检测集成管道，低版本.Net不支持，请使用者根据情况自行注释
+            try
+            {
+                if (HttpRuntime.UsingIntegratedPipeline) name += " [集成管道]";
+            }
+            catch { }
+
+            ViewBag.WebServerName = name;
 
             return View();
         }
