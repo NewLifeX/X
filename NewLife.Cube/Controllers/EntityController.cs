1e674350ed050ed7c3cc0ca9001bac4bc2242018
Stone <nnhy@vip.qq.com>
2015-04-10 17:03:16 +0000

    列头排序通过，数据分页通过 分页跳转未通过，查询未测试

--- NewLife.Cube/Controllers/EntityController.cs
+++ NewLife.Cube/Controllers/EntityController.cs
@@ -17,14 +17,39 @@ namespace NewLife.Cube.Controllers
             ViewBag.Title = Entity<TEntity>.Meta.Table.Description + "管理";
         }
 
+        ///// <summary>执行操作之前</summary>
+        ///// <param name="filterContext"></param>
+        //protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        //{
+        //    var methodInfo = ((ReflectedActionDescriptor)filterContext.ActionDescriptor).MethodInfo;
+        //    foreach (var p in methodInfo.GetParameters())
+        //    {
+        //        // 把所有值类型参数设为默认值，避免Int32等报null错误
+        //        if (p.ParameterType.IsValueType)
+        //        {
+        //            filterContext.ActionParameters[p.Name] = Activator.CreateInstance(p.ParameterType);
+        //        }
+        //    }
+        //}
+
         /// <summary>数据列表首页</summary>
         /// <returns></returns>
         [DisplayName("数据列表")]
-        public virtual ActionResult Index()
+        public virtual ActionResult Index(String q, String sort, Int32 desc = 0, Int32 page = 1, Int32 pageSize = 20)
         {
-            ViewBag.Grid = new EntityGrid(Entity<TEntity>.Meta.Factory);
+            if (page <= 0) page = 1;
+            if (pageSize <= 0) pageSize = 20;
 
-            var list = Entity<TEntity>.FindAllWithCache();
+            var grid = new EntityGrid(Entity<TEntity>.Meta.Factory);
+            grid.PageIndex = page;
+            grid.PageSize = pageSize;
+            grid.Sort = sort;
+            grid.SortDesc = desc != 0;
+
+            ViewBag.Grid = grid;
+
+            if (desc != 0 && !sort.IsNullOrEmpty()) sort += " Desc";
+            var list = Entity<TEntity>.Search(q, sort, (page - 1) * pageSize, pageSize);
 
             return View(list);
         }
@@ -34,7 +59,7 @@ namespace NewLife.Cube.Controllers
         /// <returns></returns>
         //[HttpPost]
         [DisplayName("删除")]
-        public virtual ActionResult Delete(String id)
+        public virtual ActionResult Delete(Int32 id)
         {
             var entity = Entity<TEntity>.FindByKey(id);
             entity.Delete();
