9c898ad27860dbbf2ddb6ebe97c58c4ea3e3c911
Stone <nnhy@vip.qq.com>
2015-04-25 04:14:49 +0000

    为满足XCode升级需要，更新生成器模版

--- XCoder/Template/实体业务/中文名.Biz.cs
+++ XCoder/Template/实体业务/中文名.Biz.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 using System.Xml.Serialization;
-using NewLife.CommonEntity;
 using NewLife.Log;
 using NewLife.Web;
 using XCode;
 using XCode.Configuration;
+using XCode.Membership;
 
 namespace <#=Config.NameSpace#>
 {<#
@@ -17,7 +17,9 @@ namespace <#=Config.NameSpace#>
     if(!String.IsNullOrEmpty(tdes)) tdes=tdes.Replace("\r\n"," ").Replace("\\", "\\\\").Replace("'", "").Replace("\"", "");
     if(String.IsNullOrEmpty(tdis)) tdis=tdes;
 
-    String baseType = !String.IsNullOrEmpty(Table.BaseType) ? Table.BaseType : Config.BaseClass;
+    String myClassName=Config.RenderGenEntity?"TEntity":Table.Name;
+
+   String baseType = !String.IsNullOrEmpty(Table.BaseType) ? Table.BaseType : Config.BaseClass;
     if(Config.RenderGenEntity)
 {#>
     /// <summary><#=tdis#></summary><# if(tdis!=tdes){#>
@@ -43,37 +45,14 @@ if(Config.RenderGenEntity){#>
         #region 高级查询
         // 以下为自定义高级查询的例子
 
-        ///// <summary>
-        ///// 查询满足条件的记录集，分页、排序
-        ///// </summary>
-        ///// <param name="key">关键字</param>
-        ///// <param name="orderClause">排序，不带Order By</param>
-        ///// <param name="startRowIndex">开始行，0表示第一行</param>
-        ///// <param name="maximumRows">最大返回行数，0表示所有行</param>
-        ///// <returns>实体集</returns>
-        //[DataObjectMethod(DataObjectMethodType.Select, true)]
-        //public static EntityList<<#=Config.RenderGenEntity?"TEntity":Table.Name#>> Search(String key, String orderClause, Int32 startRowIndex, Int32 maximumRows)
-        //{
-        //    return FindAll(SearchWhere(key), orderClause, null, startRowIndex, maximumRows);
-        //}
-
-        ///// <summary>
-        ///// 查询满足条件的记录总数，分页和排序无效，带参数是因为ObjectDataSource要求它跟Search统一
-        ///// </summary>
-        ///// <param name="key">关键字</param>
-        ///// <param name="orderClause">排序，不带Order By</param>
-        ///// <param name="startRowIndex">开始行，0表示第一行</param>
-        ///// <param name="maximumRows">最大返回行数，0表示所有行</param>
-        ///// <returns>记录数</returns>
-        //public static Int32 SearchCount(String key, String orderClause, Int32 startRowIndex, Int32 maximumRows)
-        //{
-        //    return FindCount(SearchWhere(key), null, null, 0, 0);
-        //}
-
-        /// <summary>构造搜索条件</summary>
+        /// <summary>查询满足条件的记录集，分页、排序</summary>
+        /// <param name="userid">用户编号</param>
+        /// <param name="start">开始时间</param>
+        /// <param name="end">结束时间</param>
         /// <param name="key">关键字</param>
-        /// <returns></returns>
-        private static String SearchWhere(String key)
+        /// <param name="param">分页排序参数，同时返回满足条件的总记录数</param>
+        /// <returns>实体集</returns>
+        public static EntityList<<#=myClassName#>> Search(Int32 userid, DateTime start, DateTime end, String key, PageParameter param)
         {
             // WhereExpression重载&和|运算符，作为And和Or的替代
             // SearchWhereByKeys系列方法用于构建针对字符串字段的模糊搜索
@@ -82,10 +61,9 @@ if(Config.RenderGenEntity){#>
             // 以下仅为演示，Field（继承自FieldItem）重载了==、!=、>、<、>=、<=等运算符（第4行）
             //if (userid > 0) exp &= _.OperatorID == userid;
             //if (isSign != null) exp &= _.IsSign == isSign.Value;
-            //if (start > DateTime.MinValue) exp &= _.OccurTime >= start;
-            //if (end > DateTime.MinValue) exp &= _.OccurTime < end.AddDays(1).Date;
+            //exp &= _.OccurTime.Between(start, end); // 大于等于start，小于end，当start/end大于MinValue时有效
 
-            return exp;
+            return FindAll(exp, param);
         }
         #endregion
 
