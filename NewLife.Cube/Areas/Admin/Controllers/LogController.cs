d2c62fbb5bfc0ed887a54da2d4d995ffa60118ed
Stone <nnhy@vip.qq.com>
2015-04-29 15:26:27 +0000

    不允许添加修改删除日志

--- NewLife.Cube/Areas/Admin/Controllers/LogController.cs
+++ NewLife.Cube/Areas/Admin/Controllers/LogController.cs
@@ -11,12 +11,18 @@ namespace NewLife.Cube.Admin.Controllers
     [DisplayName("日志")]
     public class LogController : EntityController<XLog>
     {
+        /// <summary>不允许添加修改日志</summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public override ActionResult Save(XLog entity)
         {
             //return base.Save(entity);
             throw new Exception("不允许添加/修改日志");
         }
 
+        /// <summary>不允许删除日志</summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public override ActionResult Delete(int id)
         {
             //return base.Delete(id);
