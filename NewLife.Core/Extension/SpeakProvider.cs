66a50c38607b96540b0359bf2431e8b5854c73a6
nnhy <nnhy@vip.qq.com>
2015-05-07 08:11:36 +0000

    优化语音库提供者，驱动程序集和语音库分开下载 很多时候机器已经安装语音库，只是因为不同目录而没有驱动库

--- NewLife.Core/Extension/SpeakProvider.cs
+++ NewLife.Core/Extension/SpeakProvider.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
+using System.Collections;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using System.Text;
 using NewLife.Compression;
 using NewLife.Log;
 using NewLife.Reflection;
@@ -36,7 +34,7 @@ namespace NewLife.Extension
                     var dir = Path.GetDirectoryName(file);
                     var sw = new Stopwatch();
                     sw.Start();
-                    var file2 = client.DownloadLink(url, "Speech", dir);
+                    var file2 = client.DownloadLink(url, "Microsoft.Speech", dir);
                     sw.Stop();
 
                     if (!file2.IsNullOrEmpty())
@@ -45,21 +43,76 @@ namespace NewLife.Extension
 
                         ZipFile.Extract(file2, dir);
 
-                        // 安装语音库
-                        var msi = "SpeechPlatformRuntime_x{0}.msi".F(Runtime.Is64BitOperatingSystem ? 64 : 86);
-                        msi = dir.CombinePath(msi);
-                        if (File.Exists(msi)) "msiexec".Run("/i \"" + msi + "\" /passive /norestart", 5000);
-
-                        msi = "MSSpeech_TTS_zh-CN_HuiHui.msi";
-                        msi = dir.CombinePath(msi);
-                        if (File.Exists(msi)) "msiexec".Run("/i \"" + msi + "\" /passive /norestart", 5000);
+                        // 尝试加载，如果成功，则说明已经安装运行时，仅仅缺类库
+                        LoadType(file);
                     }
+
                 }
-                if (File.Exists(file))
+                else
+                    LoadType(file);
+            }
+            CheckVoice();
+        }
+
+        Boolean LoadType(String file)
+        {
+            if (!File.Exists(file)) return false;
+
+            var assembly = Assembly.LoadFrom(file);
+            if (assembly == null) return false;
+
+            _type = assembly.GetType(typeName);
+            if (_type == null) return false;
+
+            return true;
+        }
+
+        /// <summary>检查是否安装了语音库</summary>
+        void CheckVoice()
+        {
+            if (_type == null) return;
+
+            var synth = _type.CreateInstance(new Object[0]);
+            var vs = synth.Invoke("GetInstalledVoices") as IList;
+            if (vs != null && vs.Count > 0)
+            {
+                var flag = false;
+                foreach (var item in vs)
                 {
-                    var assembly = Assembly.LoadFrom(file);
-                    if (assembly != null) _type = assembly.GetType(typeName);
+                    XTrace.WriteLine("语音库：{0}", item.GetValue("VoiceInfo").GetValue("Description"));
+
+                    if ((Boolean)item.GetValue("Enabled")) flag = true;
                 }
+                if (flag) return;
+            }
+
+            var url = "http://www.newlifex.com/showtopic-51.aspx";
+            XTrace.WriteLine("没有找到语音运行时，准备联网获取 {0}", url);
+
+            var dir = ".".GetFullPath();
+            if (Runtime.IsWeb) dir = dir.CombinePath("Bin");
+            dir.EnsureDirectory();
+
+            var client = new WebClientX(true, true);
+            var sw = new Stopwatch();
+            sw.Start();
+            var file2 = client.DownloadLink(url, "SpeechRuntime", dir);
+            sw.Stop();
+
+            if (!file2.IsNullOrEmpty())
+            {
+                XTrace.WriteLine("下载完成，共{0:n0}字节，耗时{1}毫秒", file2.AsFile().Length, sw.ElapsedMilliseconds);
+
+                ZipFile.Extract(file2, dir);
+
+                // 安装语音库
+                var msi = "SpeechPlatformRuntime_x{0}.msi".F(Runtime.Is64BitOperatingSystem ? 64 : 86);
+                msi = dir.CombinePath(msi);
+                if (File.Exists(msi)) "msiexec".Run("/i \"" + msi + "\" /passive /norestart", 5000);
+
+                msi = "MSSpeech_TTS_zh-CN_HuiHui.msi";
+                msi = dir.CombinePath(msi);
+                if (File.Exists(msi)) "msiexec".Run("/i \"" + msi + "\" /passive /norestart", 5000);
             }
         }
 
