908212c24a56cae8c7620453baf1651b7d4f4d74
nnhy <nnhy@vip.qq.com>
2015-05-13 10:05:25 +0000

    注册外围路由时，指定命名空间

--- NewLife.Cube/App_Start/RouteConfig.cs
+++ NewLife.Cube/App_Start/RouteConfig.cs
@@ -22,7 +22,8 @@ namespace NewLife.Cube
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { typeof(RouteConfig).Namespace + ".Controllers" }
             );
         }
     }
--- NewLife.Cube/Global.asax.cs
+++ NewLife.Cube/Global.asax.cs
@@ -18,12 +18,8 @@ namespace NewLife.Cube
         {
             AreaRegistration.RegisterAllAreas();
 
-            //WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            //AuthConfig.RegisterAuth();
-
         }
     }
 }
