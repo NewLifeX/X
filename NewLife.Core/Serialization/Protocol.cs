9965f7c335301a37460fb9d10f79f3958d0abe69
Stone <nnhy@vip.qq.com>
2015-05-24 23:38:47 +0000

    增加协议基类，便于协议开发

---
+++ NewLife.Core/Serialization/Protocol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NewLife.Messaging;
+
+namespace NewLife.Serialization
+{
+    /// <summary>协议基类</summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Protocol<T> : MessageBase where T : Protocol<T>, new()
+    {
+        #region 读写方法
+        #endregion
+    }
+}
