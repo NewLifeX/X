ee4bfb29e2bfa441892b9efde9274281f315721f
nnhy <nnhy@vip.qq.com>
2015-06-01 13:01:14 +0000

    附加的独立Action菜单，遍历所有权限位 测试通过

--- NewLife.Cube/Common/ControllerBaseX.cs
+++ NewLife.Cube/Common/ControllerBaseX.cs
@@ -37,16 +37,13 @@ namespace NewLife.Cube
                 else
                 {
                     var name = att.ResourceName;
+                    var pm = (Int32)att.Permission;
                     if (name.IsNullOrEmpty() || name == type.Name.TrimEnd("Controller"))
                     {
-                        dic.Add(method, (Int32)att.Permission);
+                        dic.Add(method, pm);
                     }
                     else
                     {
-                        // 权限名
-                        var pm = att.Permission.GetDescription();
-                        if ((Int32)att.Permission >= 0x10) pm = method.GetDisplayName() ?? method.Name;
-
                         // 指定了资源名称，也就是专有菜单
                         var nodeName = method.Name;
                         var node = menu.Parent.FindByPath(nodeName);
@@ -55,7 +52,25 @@ namespace NewLife.Cube
                             XTrace.WriteLine("为控制器{0}添加独立菜单{1}[{2}]", type.FullName, nodeName, name);
                             node = menu.Parent.Add(nodeName, method.GetDisplayName(), menu.Url + "/" + nodeName);
                         }
-                        node.Permissions[(Int32)att.Permission] = pm;
+
+                        // 权限名
+                        if (pm >= 0x10)
+                            node.Permissions[pm] = method.GetDisplayName() ?? method.Name;
+                        else if (att.Permission.ToString().Contains("|"))
+                            node.Permissions[pm] = att.Permission.GetDescription();
+                        else
+                        {
+                            // 附加的独立Action菜单，遍历所有权限位
+                            var n = 1;
+                            for (int i = 0; i < 8; i++)
+                            {
+                                var v = (PermissionFlags)n;
+                                if (att.Permission.Has(v)) node.Permissions[n] = v.GetDescription();
+
+                                n <<= 1;
+                            }
+                        }
+
                         node.Save();
                     }
                 }
