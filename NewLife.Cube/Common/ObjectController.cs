bcec711815197e001c605ed5b5130583dc9a427f
nnhy <nnhy@vip.qq.com>
2015-06-13 07:58:08 +0000

    处理字符串相等

--- NewLife.Cube/Common/ObjectController.cs
+++ NewLife.Cube/Common/ObjectController.cs
@@ -71,7 +71,7 @@ namespace NewLife.Cube
 
                 var v1 = obj.GetValue(pi);
                 var v2 = cfg.GetValue(pi);
-                if (!Object.Equals(v1, v2))
+                if (!Object.Equals(v1, v2) && (pi.PropertyType != typeof(String) || v1 + "" != v2 + ""))
                 {
                     if (sb.Length > 0) sb.Append(", ");
 
