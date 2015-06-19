f9a15085d6f29e29353e7a7c31374149c374c2e6
nnhy <nnhy@vip.qq.com>
2015-06-19 04:06:01 +0000

    消息基类支持整个消息输出为字符串

--- NewLife.Core/Messaging/MessageBase.cs
+++ NewLife.Core/Messaging/MessageBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
+using System.Xml.Serialization;
+using NewLife.Reflection;
 using NewLife.Serialization;
 
 namespace NewLife.Messaging
@@ -9,12 +11,14 @@ namespace NewLife.Messaging
     /// <summary>消息基类</summary>
     public abstract class MessageBase : IMessage
     {
+        #region 核心读写方法
         /// <summary>从数据流中读取消息</summary>
         /// <param name="stream"></param>
         /// <returns>是否成功</returns>
         public virtual Boolean Read(Stream stream)
         {
             var fm = GetFormatter(true);
+            fm.Stream = stream;
             Object obj = this;
             return fm.TryRead(this.GetType(), ref obj);
         }
@@ -24,6 +28,7 @@ namespace NewLife.Messaging
         public virtual void Write(Stream stream)
         {
             var fm = GetFormatter(false);
+            fm.Stream = stream;
             fm.Write(this);
         }
 
@@ -36,6 +41,55 @@ namespace NewLife.Messaging
 
             return binary;
         }
+        #endregion
+
+        #region 辅助
+        /// <summary>输出消息实体</summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var pi in this.GetType().GetProperties())
+            {
+                if (pi.GetCustomAttribute<XmlIgnoreAttribute>() != null) continue;
+
+                GetMember(pi, -16, sb);
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>获取成员输出</summary>
+        /// <param name="pi"></param>
+        /// <param name="len"></param>
+        /// <param name="sb"></param>
+        protected virtual void GetMember(PropertyInfo pi, Int32 len, StringBuilder sb)
+        {
+            if (sb.Length > 0) sb.AppendLine();
+
+            var v = GetMemberValue(pi);
+            sb.AppendFormat("{0," + len + "}: {1}", pi.Name, v);
+        }
+
+        /// <summary>获取用于输出的成员值</summary>
+        /// <param name="pi"></param>
+        /// <returns></returns>
+        protected virtual Object GetMemberValue(PropertyInfo pi)
+        {
+            var v = this.GetValue(pi);
+
+            if (pi.PropertyType == typeof(Byte[]) && v != null)
+            {
+                var buf = (Byte[])v;
+                var len = buf.Length;
+
+                var att = pi.GetCustomAttribute<FieldSizeAttribute>();
+                if (att != null && att.Size > 0) len = att.Size;
+                v = buf.ToHex();
+            }
+
+            return v;
+        }
+        #endregion
     }
 
     ///// <summary>消息泛型基类</summary>
