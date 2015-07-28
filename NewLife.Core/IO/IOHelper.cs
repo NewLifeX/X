33a418ea8e3384940d2708dec70916d2957f0568
nnhy <nnhy@vip.qq.com>
2015-07-28 10:26:05 +0000

    无法从长度只有{0}的数据流里面读取{1}字节的数据

--- NewLife.Core/IO/IOHelper.cs
+++ NewLife.Core/IO/IOHelper.cs
@@ -362,10 +362,10 @@ namespace System
             if (length == 0) return new Byte[0];
 
             #region 规避内存读取错误
+            if (length > 0 && stream.CanSeek && stream.Length - stream.Position < length)
+                throw new XException("无法从长度只有{0}的数据流里面读取{1}字节的数据", stream.Length - stream.Position, length);
             if (XTrace.Debug)
             {
-                if (length > 0 && stream.CanSeek && stream.Length - stream.Position < length)
-                    XTrace.WriteLine("设计错误！无法从长度只有{0}的数据流里面读取{1}字节的数据", stream.Length - stream.Position, length);
                 if (length > 2048)
                     XTrace.WriteLine("设计错误！读取数据{0}字节超大，很有可能是上层代码逻辑出错，如果上层无错而需要屏蔽当前提示，建议关闭NewLife.Debug调试开关", length);
             }
