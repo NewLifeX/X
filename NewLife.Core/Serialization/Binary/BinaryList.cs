1d05a5e79606215e30ea3f56a59abf335e24d807
nnhy <nnhy@vip.qq.com>
2016-02-13 09:37:31 +0000

    修正一个空引用的BUG

--- NewLife.Core/Serialization/Binary/BinaryList.cs
+++ NewLife.Core/Serialization/Binary/BinaryList.cs
@@ -25,7 +25,7 @@ namespace NewLife.Serialization
 
             var list = value as IList;
             // 先写入长度
-            if (list.Count == 0)
+            if (list == null || list.Count == 0)
             {
                 Host.WriteSize(0);
                 return true;
