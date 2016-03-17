9a32d19c00063ac6bb02373e569525f02f61d279
Stone <nnhy@vip.qq.com>
2016-03-17 03:06:22 +0000

    新增消息队列

--- NewLife.MQ/Properties/AssemblyInfo.cs
+++ NewLife.MQ/Properties/AssemblyInfo.cs
@@ -5,21 +5,21 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("新生命IP物理地址库")]
-[assembly: AssemblyDescription("IP物理地址库，自动下载数据库")]
+[assembly: AssemblyTitle("新生命消息队列")]
+[assembly: AssemblyDescription("消息发布订阅持久化")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("NewLife.IP")]
+[assembly: AssemblyProduct("NewLife.MQ")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
-// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
+// 对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
 // 则将该类型上的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("cc92b08a-f9c9-4c57-820e-8a19ab533386")]
+[assembly: Guid("3627e8d6-49d1-4cbe-8c75-4a46f89bf8b4")]
 
-// 程序集的版本信息由下面四个值组成:
+// 程序集的版本信息由下面四个值组成: 
 //
 //      主版本
 //      次版本 
@@ -27,7 +27,11 @@ using System.Runtime.InteropServices;
 //      修订号
 //
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
-// 方法是按如下所示使用“*”:
+// 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.2015.0403")]
+[assembly: AssemblyFileVersion("1.0.2016.0317")]
+
+/*
+ * v1.0.2016.0317   创建消息队列
+ */
--- XTemplate/Properties/AssemblyInfo.cs
+++ XTemplate/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("新生命模版")]
+[assembly: AssemblyTitle("新生命模版引擎")]
 [assembly: AssemblyDescription("编译型模版引擎")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("XTemplate")]
