5efeb52bf6cb0ad55194d13ffe8f085ca7fa524b
nnhy <nnhy@vip.qq.com>
2016-04-13 15:33:14 +0000

    性能测试完成，FastJson略胜Json.Net一筹，比二进制序列化还快

--- NewLife.Core/Serialization/Json/JsonTest.cs
+++ NewLife.Core/Serialization/Json/JsonTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading;
 using NewLife.Log;
@@ -14,42 +17,66 @@ namespace NewLife.Serialization
     {
         public static void Start()
         {
+            // 提升进程优先级
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.Idle;
+
             // 为所有Json宿主创建实例
             var hosts = typeof(IJsonHost).GetAllSubclasses().Select(e => e.CreateInstance() as IJsonHost).ToArray();
-
-            var obj = Create();
-            //var json = hosts[2].Write(obj);
             Console.Clear();
-            //Console.WriteLine(JsonHelper.Format(json));
-            //Thread.Sleep(1000);
-            //Console.Clear();
 
-            var json = "";
-            Console.WriteLine("Json序列化大小");
-            foreach (var item in hosts)
+            for (int i = 0; i < 2; i++)
             {
-                json = item.Write(obj, true);
-                Console.WriteLine("{0}\t{1:n0}", item.GetType().Name, json.GetBytes().Length);
-                Console.WriteLine(json);
+                var obj = Create(i > 0);
 
-                item.Read(json, obj.GetType());
-            }
+                var json = "";
+                Console.WriteLine("{0} Json序列化大小", i == 0 ? "普通" : "高级");
+                foreach (var item in hosts)
+                {
+                    json = item.Write(obj, true);
+                    Console.WriteLine("{0}\t大小：{1:n0}", item.GetType().Name, json.GetBytes().Length);
+                    Console.WriteLine(json);
 
-            CodeTimer.ShowHeader("Json序列化性能测试");
-            foreach (var item in hosts)
-            {
-                CodeTimer.TimeLine(item.GetType().Name, 100000, n => { item.Write(obj); });
-            }
+                    item.Read(json, obj.GetType());
+                }
+                {
+                    var bn = new BinaryFormatter();
+                    var ms = new MemoryStream();
+                    bn.Serialize(ms, obj);
+                    Console.WriteLine("{0}\t大小：{1:n0}", bn.GetType().Name, ms.Length);
+                }
+                {
+                    var bn = new Binary();
+                    bn.Write(obj);
+                    Console.WriteLine("{0}\t大小：{1:n0}", bn.GetType().Name, bn.Stream.Length);
+                }
 
-            Console.WriteLine();
-            CodeTimer.ShowHeader("Json反序列化性能测试");
-            foreach (var item in hosts)
-            {
-                CodeTimer.TimeLine(item.GetType().Name, 100000, n => { item.Read(json, obj.GetType()); });
+                CodeTimer.ShowHeader("Json序列化性能测试");
+                foreach (var item in hosts)
+                {
+                    CodeTimer.TimeLine(item.GetType().Name, 100000, n => { item.Write(obj); });
+                }
+                CodeTimer.TimeLine("BinaryFormatter", 100000, n =>
+                {
+                    var bn = new BinaryFormatter();
+                    var ms = new MemoryStream();
+                    bn.Serialize(ms, obj);
+                });
+                CodeTimer.TimeLine("Binary", 100000, n =>
+                {
+                    var bn = new Binary();
+                    bn.Write(obj);
+                });
+
+                Console.WriteLine();
+                CodeTimer.ShowHeader("Json反序列化性能测试");
+                foreach (var item in hosts)
+                {
+                    CodeTimer.TimeLine(item.GetType().Name, 100000, n => { item.Read(json, obj.GetType()); });
+                }
             }
         }
 
-        static JsObject Create()
+        static JsObject Create(Boolean ext)
         {
             var obj = new JsObject();
 
@@ -60,31 +87,35 @@ namespace NewLife.Serialization
             obj.Time = DateTime.Now;
             //obj.Data = Rand.NextBytes(16);
 
-            var n = Rand.Next(2, 10);
-            obj.Points = new Double[n];
-            for (int i = 0; i < n; i++)
+            if (ext)
             {
-                obj.Points[i] = (Double)Rand.Next() / 10000;
-            }
+                var n = Rand.Next(2, 10);
+                obj.Points = new Double[n];
+                for (int i = 0; i < n; i++)
+                {
+                    obj.Points[i] = (Double)Rand.Next() / 10000;
+                }
 
-            obj.Items = new List<String>();
-            n = Rand.Next(2, 6);
-            for (int i = 0; i < n; i++)
-            {
-                obj.Items.Add(Rand.NextString(32));
-            }
+                obj.Items = new List<String>();
+                n = Rand.Next(2, 10);
+                for (int i = 0; i < n; i++)
+                {
+                    obj.Items.Add(Rand.NextString(32));
+                }
 
-            obj.Container = new Dictionary<String, String>();
-            n = Rand.Next(2, 6);
-            for (int i = 0; i < n; i++)
-            {
-                obj.Container.Add("元素" + (i + 1), Rand.NextString(32));
+                obj.Container = new Dictionary<String, String>();
+                n = Rand.Next(2, 10);
+                for (int i = 0; i < n; i++)
+                {
+                    obj.Container.Add("元素" + (i + 1), Rand.NextString(32));
+                }
             }
 
             return obj;
         }
     }
 
+    [Serializable]
     class JsObject
     {
         public Int32 ID { get; set; }
