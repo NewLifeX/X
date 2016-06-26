efe2950eed3b1b9e7ca6324b72284edc8c583882
nnhy <nnhy@vip.qq.com>
2016-06-26 04:17:23 +0000

    修正命令

--- X.bat
+++ X.bat
@@ -5,9 +5,16 @@ title 自动同步
 
 set url=https://svn.newlifex.com/svn/X/trunk/Src
 for %%i in (NewLife.Core NewLife.Net XCode XAgent XCoder XControl XTemplate) do (
-	git svn clone %url%/%%i --no-metadata --authors-file=user.txt %%i
-	git svn fetch %url%/%%i --authors-file=user.txt
+	pushd %%i
 
-	git remote add origin https://github.com/NewLifeX/%%i.git
+	if not exist .git (
+		git svn clone %url%/%%i --no-metadata --authors-file=..\user.txt .\
+		git remote add origin https://github.com/NewLifeX/%%i.git
+	) else (
+		git svn fetch
+	)
+
 	git push --all --force-with-lease --progress "origin"
+
+	popd
 )
