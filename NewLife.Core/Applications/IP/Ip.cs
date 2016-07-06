95df6355a9599faf889d8c667986c2b22fd3737d
Stone <nnhy@vip.qq.com>
2016-07-06 08:25:03 +0000

    合并NewLife.IP，作为核心库的一部分

---
+++ NewLife.Core/Applications/IP/Ip.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using NewLife.Log;
+using NewLife.Web;
+
+namespace NewLife.IP
+{
+    /// <summary>IP搜索</summary>
+    public static class Ip
+    {
+        private static object lockHelper = new object();
+        private static Zip zip;
+
+        /// <summary>数据文件</summary>
+        public static String DbFile { get; set; }
+
+        static Ip()
+        {
+            var ns = new String[] { "qqwry.dat", "qqwry.gz", "ip.gz", "ip.gz.config", "ipdata.config" };
+            foreach (var item in ns)
+            {
+                var fs = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, item, SearchOption.AllDirectories);
+                if (fs != null && fs.Length > 0)
+                {
+                    DbFile = Path.GetFullPath(fs[0]);
+                    break;
+                }
+            }
+
+            // 如果本地没有IP数据库，则从网络下载
+            if (DbFile.IsNullOrWhiteSpace())
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    var url = Setting.Current.PluginServer;
+                    XTrace.WriteLine("没有找到IP数据库，准备联网获取 {0}", url);
+
+                    var client = new WebClientX();
+                    client.Log = XTrace.Log;
+                    var dir = Runtime.IsWeb ? "App_Data" : "Data";
+                    var file = client.DownloadLink(url, "ip.gz", dir.GetFullPath());
+
+                    if (File.Exists(file))
+                    {
+                        DbFile = file.GetFullPath();
+                        zip = null;
+                        // 让它重新初始化
+                        _inited = null;
+                    }
+                }, TaskCreationOptions.LongRunning).LogException();
+            }
+        }
+
+        static Boolean? _inited;
+        static Boolean Init()
+        {
+            if (_inited != null) return _inited.Value;
+            lock (typeof(Ip))
+            {
+                if (_inited != null) return _inited.Value;
+                _inited = false;
+
+                var z = new Zip();
+
+                if (!File.Exists(DbFile))
+                {
+                    //throw new InvalidOperationException("无法找到IP数据库" + DbFile + "！");
+                    XTrace.WriteLine("无法找到IP数据库{0}", DbFile);
+                    return false;
+                }
+                XTrace.WriteLine("使用IP数据库{0}", DbFile);
+                using (var fs = File.OpenRead(DbFile))
+                {
+                    try
+                    {
+                        z.SetStream(fs);
+                    }
+                    catch (Exception ex)
+                    {
+                        XTrace.WriteException(ex);
+
+                        return false;
+                    }
+                }
+                zip = z;
+            }
+
+            if (zip.Stream == null) throw new InvalidOperationException("无法打开IP数据库" + DbFile + "！");
+
+            _inited = true;
+            return true;
+        }
+
+        /// <summary>获取IP地址</summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        public static String GetAddress(String ip)
+        {
+            if (String.IsNullOrEmpty(ip)) return "";
+
+            if (!Init()) return "";
+
+            var ip2 = IPToUInt32(ip.Trim());
+            lock (lockHelper)
+            {
+                return zip.GetAddress(ip2) + "";
+            }
+        }
+
+        /// <summary>获取IP地址</summary>
+        /// <param name="addr"></param>
+        /// <returns></returns>
+        public static String GetAddress(IPAddress addr)
+        {
+            if (addr == null) return "";
+
+            if (!Init()) return "";
+
+            var ip2 = (UInt32)addr.GetAddressBytes().Reverse().ToInt();
+            lock (lockHelper)
+            {
+                return zip.GetAddress(ip2) + "";
+            }
+        }
+
+        static uint IPToUInt32(String IpValue)
+        {
+            var ss = IpValue.Split('.');
+            var buf = new Byte[4];
+            for (int i = 0; i < 4; i++)
+            {
+                var n = 0;
+                if (i < ss.Length && Int32.TryParse(ss[i], out n))
+                {
+                    buf[3 - i] = (Byte)n;
+                }
+            }
+            return BitConverter.ToUInt32(buf, 0);
+        }
+    }
+
+    class MyIpProvider : NetHelper.IpProvider
+    {
+        public string GetAddress(IPAddress addr)
+        {
+            return Ip.GetAddress(addr);
+        }
+    }
+}
