717c1b4ea3b72cd4af5e0592a38387b0380883cb
nnhy <nnhy@vip.qq.com>
2016-07-17 06:03:14 +0000

    放弃一批不再需要的项目

--- XControl/TextBox/VerifyCode.js
+++
@@ -1,7 +0,0 @@
-﻿function VerifyCodeBox_Refresh(img, input) {
-    var rnd = "&rnd=" + (+new Date());
-    img.src = img.src.replace(/&rnd=.+?(?=&|$)/gi, rnd);
-    var e=document.getElementById(input);
-    e.select();
-    e.focus();
-}
--- 清理.bat
+++ 清理.bat
@@ -1,9 +1,2 @@
 for /f "tokens=*" %%a in ('dir obj /b /ad /s ^|sort') do rd "%%a" /s/q
 for /f "tokens=*" %%a in ('dir bin /b /ad /s ^|sort') do rd "%%a" /s/q
-
-rd Web\Log /s/q
-rd Web\App_Data /s/q
-
-md Web\Bin
-
-Copy ..\DLL\XControl.* Web\Bin\
