ad45c31f7075cc016f3b27de3041a275bc642dad
Soar、毅 <Soar360@live.com>
2016-10-08 12:52:58 +0800

    修正iOS下，处于4G环境，获取不到IP地址的问题
    在4G环境下，硬件类型为Unknown，去掉类型过滤，就可以了。

--- NewLife.Core/Net/NetHelper.cs
+++ NewLife.Core/Net/NetHelper.cs
@@ -264,15 +264,11 @@ namespace System
 #if __IOS__
             foreach (var netInterface in NetworkInterface.GetAllNetworkInterfaces())
             {
-                if (netInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
-                    netInterface.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                foreach (var addrInfo in netInterface.GetIPProperties().UnicastAddresses)
                 {
-                    foreach (var addrInfo in netInterface.GetIPProperties().UnicastAddresses)
+                    if (addrInfo.Address.AddressFamily == AddressFamily.InterNetwork)
                     {
-                        if (addrInfo.Address.AddressFamily == AddressFamily.InterNetwork)
-                        {
-                            yield return addrInfo.Address;
-                        }
+                        yield return addrInfo.Address;
                     }
                 }
             }
