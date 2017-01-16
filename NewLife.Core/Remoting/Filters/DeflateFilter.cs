d27a305c3d933d3833279166e7b65d82447f1102
大石头X2 <nnhy@vip.qq.com>
2017-01-16 18:01:52 +0800

    太小的数据包不压缩。 使用压缩的最小长度。默认64字节

--- NewLife.Core/Remoting/Filters/DeflateFilter.cs
+++ NewLife.Core/Remoting/Filters/DeflateFilter.cs
@@ -9,6 +9,9 @@ namespace NewLife.Remoting
     /// <summary>压缩过滤器</summary>
     public class DeflateFilter : FilterBase
     {
+        /// <summary>使用压缩的最小长度。默认64字节</summary>
+        public Int32 MinSize { get; set; } = 64;
+
         /// <summary>执行压缩或解压缩</summary>
         /// <param name="context"></param>
         /// <returns></returns>
@@ -23,6 +26,9 @@ namespace NewLife.Remoting
             var pk = ctx.Packet;
             if (ctx.IsSend)
             {
+                // 太小的数据包不压缩
+                if (pk.Count < MinSize) return true;
+
                 // 压缩标记位
                 msg.Flag |= 0x10;
 
