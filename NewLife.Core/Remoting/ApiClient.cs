94a1fd1705a196a4f992489986396b6bd3bc4429
大石头X2 <nnhy@vip.qq.com>
2017-01-17 12:04:10 +0800

    客户端默认重用控制器实例

--- NewLife.Core/Remoting/ApiClient.cs
+++ NewLife.Core/Remoting/ApiClient.cs
@@ -53,7 +53,7 @@ namespace NewLife.Remoting
         public virtual Object this[String key] { get { return Items[key]; } set { Items[key] = value; } }
 
         /// <summary>是否在会话上复用控制器。复用控制器可确保同一个会话多次请求路由到同一个控制器对象实例</summary>
-        public Boolean IsReusable { get; set; }
+        public Boolean IsReusable { get; set; } = true;
         #endregion
 
         #region 构造
