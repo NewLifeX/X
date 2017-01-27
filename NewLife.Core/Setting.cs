f40373f78296f5f2fef7e34908b56ffe7b1c0142
大石头 <nnhy@vip.qq.com>
2017-01-27 23:13:51 +0800

    默认打开全局调试开关，默认日志等级Info，不输出Debug日志

--- NewLife.Core/Setting.cs
+++ NewLife.Core/Setting.cs
@@ -14,25 +14,25 @@ namespace NewLife
     public class Setting : XmlConfig<Setting>
     {
         #region 属性
-        /// <summary>是否启用全局调试。默认为不启用</summary>
+        /// <summary>是否启用全局调试。默认启用</summary>
         [Description("全局调试。XTrace.Debug")]
-        public Boolean Debug { get; set; }
+        public Boolean Debug { get; set; } = true;
 
         /// <summary>日志等级，只输出大于等于该级别的日志</summary>
         [Description("日志等级。只输出大于等于该级别的日志")]
-        public LogLevel LogLevel { get; set; }
+        public LogLevel LogLevel { get; set; } = LogLevel.Info;
 
         /// <summary>文件日志目录</summary>
         [Description("文件日志目录")]
-        public String LogPath { get; set; }
+        public String LogPath { get; set; } = "";
 
         /// <summary>网络日志。本地子网日志广播255.255.255.255:514</summary>
         [Description("网络日志。本地子网日志广播255.255.255.255:514")]
-        public String NetworkLog { get; set; }
+        public String NetworkLog { get; set; } = "";
 
         /// <summary>临时目录</summary>
         [Description("临时目录")]
-        public String TempPath { get; set; } = "XTemp";
+        public String TempPath { get; set; } = "";
 
         /// <summary>插件目录</summary>
         [Description("插件目录")]
@@ -44,7 +44,7 @@ namespace NewLife
 
         /// <summary>插件缓存目录。默认位于系统盘的X\Cache</summary>
         [Description("插件缓存目录。默认位于系统盘的X\\Cache")]
-        public String PluginCache { get; set; }
+        public String PluginCache { get; set; } = "";
 
         /// <summary>网络调试</summary>
         [Description("网络调试")]
@@ -69,6 +69,13 @@ namespace NewLife
         /// <summary>加载完成后</summary>
         protected override void OnLoaded()
         {
+            if (TempPath.IsNullOrEmpty())
+            {
+                if (Runtime.IsWeb)
+                    TempPath = "..\\XTemp";
+                else
+                    TempPath = "XTemp";
+            }
 #if !__MOBILE__
             if (PluginCache.IsNullOrWhiteSpace())
             {
