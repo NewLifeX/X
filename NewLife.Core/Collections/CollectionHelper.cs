bc13783358492f9c3aad6577085f9a7808b73913
大石头 <nnhy@vip.qq.com>
2017-03-26 22:27:44 +0800

    增加匿名对象到字典的扩展方法，便于控制器架构传递参数

--- NewLife.Core/Collections/CollectionHelper.cs
+++ NewLife.Core/Collections/CollectionHelper.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NewLife.Reflection;
 
 namespace System.Collections.Generic
 {
@@ -61,6 +59,52 @@ namespace System.Collections.Generic
                 collection.Values.CopyTo(arr, index);
                 return arr;
             }
+        }
+
+        /// <summary>目标匿名参数对象转为字典</summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static IDictionary<String, Object> ToDictionary(this Object target)
+        {
+            var dic = target as IDictionary<String, Object>;
+            if (dic != null) return dic;
+
+            dic = new Dictionary<String, Object>(StringComparer.OrdinalIgnoreCase);
+            if (target != null)
+            {
+                // 修正字符串字典的支持问题
+                var dic2 = target as IDictionary;
+                if (dic2 != null)
+                {
+                    foreach (DictionaryEntry item in dic2)
+                    {
+                        dic[item.Key + ""] = item.Value;
+                    }
+                }
+                else
+                {
+                    foreach (var pi in target.GetType().GetProperties())
+                    {
+                        dic[pi.Name] = target.GetValue(pi);
+                    }
+                }
+            }
+
+            return dic;
+        }
+
+        /// <summary>合并字典参数</summary>
+        /// <param name="dic"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static IDictionary<String, Object> Merge(this IDictionary<String, Object> dic, Object target)
+        {
+            foreach (var item in target.ToDictionary())
+            {
+                dic[item.Key] = item.Value;
+            }
+
+            return dic;
         }
     }
 }
