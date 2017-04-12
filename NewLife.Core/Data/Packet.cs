a799c04d736759f88eb7fddb49a8f6ed819d4ff7
大石头 <nnhy@vip.qq.com>
2017-04-12 22:59:35 +0800

    增强封包Packet，支持链式数据包

--- NewLife.Core/Data/Packet.cs
+++ NewLife.Core/Data/Packet.cs
@@ -16,6 +16,12 @@ namespace NewLife.Data
 
         /// <summary>长度</summary>
         public Int32 Count { get; private set; }
+
+        /// <summary>下一个链式包</summary>
+        public Packet Next { get; set; }
+
+        /// <summary>总长度</summary>
+        public Int32 Total { get { return Count + (Next != null ? Next.Total : 0); } }
         #endregion
 
         #region 构造
@@ -65,24 +71,88 @@ namespace NewLife.Data
         /// <returns></returns>
         public virtual Byte[] ToArray()
         {
-            if (Offset == 0 && (Count < 0 || Offset + Count == Data.Length)) return Data;
+            if (Offset == 0 && (Count < 0 || Offset + Count == Data.Length) && Next == null) return Data;
 
-            return Data.ReadBytes(Offset, Count);
+            if (Next == null) Data.ReadBytes(Offset, Count);
+
+            // 链式包输出
+            var ms = new MemoryStream(Data, Offset, Count);
+            Next.WriteTo(ms);
+
+            return ms.ToArray();
+        }
+
+        /// <summary>从封包中读取指定数据</summary>
+        /// <param name="offset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public Byte[] ReadBytes(Int32 offset = 0, Int32 count = -1)
+        {
+            if (offset == 0 && count < 0) return ToArray();
+
+            if (Next == null) return Data.ReadBytes(Offset + offset, count < 0 ? Count : count);
+
+            // 当前包足够长
+            if (count >= 0 && offset + count <= Count) return Data.ReadBytes(Offset + offset, count);
+
+            // 链式包输出
+            if (count < 0) count = Total - offset;
+            var ms = new MemoryStream();
+
+            // 遍历
+            var cur = this;
+            while (cur != null && count > 0)
+            {
+                var len = cur.Count;
+                // 当前包不够用
+                if (len < offset)
+                    offset -= len;
+                else
+                {
+                    len -= offset;
+                    if (len > count) len = count;
+                    ms.Write(cur.Data, cur.Offset + offset, len);
+
+                    offset = 0;
+                    count -= len;
+                }
+
+                cur = cur.Next;
+            }
+            return ms.ToArray();
+
+            //// 以上算法太复杂，直接来
+            //return ToArray().ReadBytes(offset, count);
         }
 
         /// <summary>获取封包的数据流形式</summary>
         /// <returns></returns>
-        public virtual Stream GetStream() { return new MemoryStream(Data, Offset, Count, false); }
+        public virtual Stream GetStream()
+        {
+            if (Next == null) return new MemoryStream(Data, Offset, Count, false);
+
+            var ms = new MemoryStream();
+            WriteTo(ms);
+            ms.Position = 0;
+
+            return ms;
+        }
 
         /// <summary>把封包写入到数据流</summary>
         /// <param name="stream"></param>
-        public void WriteTo(Stream stream) { stream.Write(Data, Offset, Count); }
+        public void WriteTo(Stream stream)
+        {
+            stream.Write(Data, Offset, Count);
+            Next?.WriteTo(stream);
+        }
 
         /// <summary>深度克隆一份数据包，拷贝数据区</summary>
         /// <returns></returns>
         public Packet Clone()
         {
-            return new Packet(Data.ReadBytes(Offset, Count));
+            if (Next == null) return new Packet(Data.ReadBytes(Offset, Count));
+
+            return new Packet(ToArray());
         }
 
         /// <summary>以字符串表示</summary>
@@ -91,9 +161,11 @@ namespace NewLife.Data
         public String ToStr(Encoding encoding = null)
         {
             if (Data == null) return null;
-            if (Count == 0) return String.Empty;
+            //if (Count == 0) return String.Empty;
 
-            return Data.ToStr(encoding ?? Encoding.UTF8, Offset, Count);
+            if (Next == null) return Data.ToStr(encoding ?? Encoding.UTF8, Offset, Count);
+
+            return ToArray().ToStr(encoding ?? Encoding.UTF8);
         }
 
         /// <summary>以十六进制编码表示</summary>
@@ -104,12 +176,14 @@ namespace NewLife.Data
         public String ToHex(Int32 maxLength = 32, String separate = null, Int32 groupSize = 0)
         {
             if (Data == null) return null;
-            if (Count == 0) return String.Empty;
+            //if (Count == 0) return String.Empty;
 
-            var len = Math.Min(Count, maxLength);
-            var buf = Data;
-            if (Offset > 0) buf = Data.ReadBytes(Offset, len);
-            return buf.ToHex(separate, groupSize, len);
+            //var len = Math.Min(Count, maxLength);
+            //var buf = Data;
+            //if (Offset > 0) buf = Data.ReadBytes(Offset, len);
+            //return buf.ToHex(separate, groupSize, len);
+
+            return ReadBytes(0, maxLength).ToHex(separate, groupSize);
         }
         #endregion
 
