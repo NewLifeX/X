3d14ae628b7e9f4cee3862ee8d3d42fb39cadd53
大石头 <nnhy@vip.qq.com>
2017-04-17 22:26:46 +0800

    连接成功后，自动登录，然后自动打开定时心跳

--- NewLife.Cube/Scripts/ApiClient.js
+++ NewLife.Cube/Scripts/ApiClient.js
@@ -1,4 +1,14 @@
-﻿var ApiClient = (function (uri) {
+﻿/*
+使用流程：
+1，实例化
+2，设置用户名密码
+3，打开链接
+4，自动登录
+5，定时心跳
+6，调用业务动作
+*/
+
+var ApiClient = (function (uri) {
     var ApiClient = function (uri) {
         return new ApiClient.fn.init(uri);
     }
@@ -16,7 +26,7 @@
         PassWord: null,
 
         // 打开连接
-        Open: function () {
+        Open: function (callback) {
             var ws = this.Socket;
             if (ws && ws.readyState === WebSocket.OPEN) return true;
 
@@ -25,6 +35,11 @@
             ws = new WebSocket(this.Uri);
             ws.onopen = function (evt) {
                 console.log("已经建立连接");
+
+                if (callback) callback();
+
+                // 异步登录
+                if (this.UserName) setTimeout(this.Login, 100);
             };
 
             // 等到创建完成
@@ -42,9 +57,6 @@
 
             this.Socket = ws;
 
-            // 异步登录
-            if (this.UserName) setTimeout(this.Login, 100);
-
             return true;
         },
 
@@ -53,10 +65,11 @@
             var ws = this.Socket;
             if (ws && ws.readyState === WebSocket.OPEN) return ws;
 
-            return this.Open() ? this.Socket : null;
+            //return this.Open() ? this.Socket : null;
+            return null;
         },
 
-        // 注册
+        // 注册动作到指定函数
         requestCallbacks: new Array(),
         Register: function (action, callback) {
             this.requestCallbacks[action] = callback;
@@ -88,17 +101,23 @@
 
         // 发送数据
         Send: function (data) {
-            var ws = this.getSocket();
-            if (!ws) return false;
+            console.log('Send ' + data);
 
-            ws.send(data);
+            var ws = this.getSocket();
+            if (ws) return ws.send(data);
+
+            this.Open(function () {
+                var ws = this.getSocket();
+                if (ws) return ws.send(data);
+            });
 
             return true;
         },
 
-        // 调用
+        // 调用动作，并在收到响应时调用回调函数
         Invoke: function (action, args, callback) {
             var msg = { action: action, args: args };
+            //console.log('Invoke ' + msg);
 
             this.responseCallback = callback;
             this.Send(JSON.stringify(msg));
@@ -110,14 +129,15 @@
             var user = this.UserName;
             var pass = this.PassWord;
             var args = { user: this.UserName, pass: this.PassWord };
+            console.log('Login ' + args);
 
-            this.Invoke('Login');
-            /*this.Invoke('Login', args, function (msg) {
-                callback(msg);
+            //this.Invoke('Login');
+            this.Invoke('Login', args, function (msg) {
+                if (callback) callback(msg);
 
                 // 定时心跳
                 if (!this._timer) this._timer = setInterval(this.Ping, 30000);
-            });*/
+            });
         },
 
         // 心跳
