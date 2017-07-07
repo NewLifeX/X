1a480a673894e2e74c9c77d0a7494b6511ddb79c
大石头 <nnhy@vip.qq.com>
2017-07-07 13:39:35 +0800

    以时间为比较点的数据抽取器

---
+++ XCode/Transform/ExtractSetting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace XCode.Transform
+{
+    /// <summary>数据抽取参数</summary>
+    public interface IExtractSetting
+    {
+        /// <summary>开始。大于等于</summary>
+        DateTime Start { get; set; }
+
+        /// <summary>开始行。分页</summary>
+        Int32 Row { get; set; }
+
+        /// <summary>批大小</summary>
+        Int32 BatchSize { get; set; }
+
+        /// <summary>启用</summary>
+        Boolean Enable { get; set; }
+    }
+
+    /// <summary>数据抽取参数</summary>
+    public class ExtractSetting : IExtractSetting
+    {
+        /// <summary>开始。大于等于</summary>
+        public DateTime Start { get; set; }
+
+        /// <summary>开始行。分页</summary>
+        public Int32 Row { get; set; }
+
+        /// <summary>批大小</summary>
+        public Int32 BatchSize { get; set; } = 5000;
+
+        /// <summary>启用</summary>
+        public Boolean Enable { get; set; } = true;
+    }
+}
---
+++ XCode/Transform/TimeExtracter.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using NewLife.Log;
+using XCode;
+using XCode.Configuration;
+
+/*
+ *  时间点抽取流程：
+ *      验证时间start，越界则退出
+ *      抽取一批数据 list = FindAll(UpdateTime >= start, UpdateTime.Asc(), null, Row, 1000)
+ *      有数据list.Count > 0
+ *          last = list.Last().UpdateTime
+ *          满一批，后续还有数据 list.Count == 1000
+ *              最大时间行数 lastCount = list.Count(e=>UpdateTime==last)
+ *              Row += lastCount
+ *              滑动时间点 start = last
+ *          不满一批，后续没有数据
+ *              Row = 0;
+ *              滑动时间点 start = last + 1
+ *      返回这一批数据
+ */
+
+namespace XCode.Transform
+{
+    /// <summary>以时间为比较点的数据抽取器</summary>
+    public class TimeExtracter
+    {
+        #region 属性
+        /// <summary>名称</summary>
+        public String Name { get; set; }
+
+        /// <summary>设置</summary>
+        public IExtractSetting Setting { get; set; }
+
+        /// <summary>实体工厂</summary>
+        public IEntityOperate Factory { get; set; }
+
+        /// <summary>获取 或 设置 时间字段</summary>
+        public String FieldName { get; set; }
+
+        /// <summary>附加条件</summary>
+        public String Where { get; set; }
+
+        private FieldItem _Field;
+        /// <summary>时间字段</summary>
+        public FieldItem Field
+        {
+            get
+            {
+                if (_Field == null)
+                {
+                    _Field = Factory.Table.FindByName(Field);
+                    if (_Field == null) throw new ArgumentNullException(nameof(Field));
+                }
+                return _Field;
+            }
+        }
+        #endregion
+
+        #region 构造
+        public TimeExtracter()
+        {
+            Name = GetType().Name.TrimEnd("Extracter", "Worker");
+        }
+        #endregion
+
+        #region 抽取数据
+        /// <summary>抽取一批数据</summary>
+        /// <returns></returns>
+        public virtual IEntityList Fetch()
+        {
+            var set = Setting;
+            if (set == null) set = Setting = new ExtractSetting();
+            if (!set.Enable) return null;
+
+            // 验证
+            var start = set.Start;
+            if (start >= DateTime.Now) return null;
+
+            var size = set.BatchSize;
+            if (size <= 0) size = 1000;
+
+            // 分批获取数据，如果没有取到，则结束
+            var sw = Stopwatch.StartNew();
+            var list = FetchData(start, set.Row, size);
+            sw.Stop();
+
+            // 取到数据，需要滑动窗口
+            if (list.Count > 0)
+            {
+                var last = (DateTime)list.Last()[FieldName];
+                // 满一批，后续还有数据
+                if (list.Count >= 1000)
+                {
+                    // 最大时间行数
+                    var maxCount = list.Count(e => (DateTime)e[FieldName] == last);
+                    // 以最后时间为起点，跳过若干行。注意可能产生连续分页的情况
+                    set.Start = last;
+                    set.Row += maxCount;
+                }
+                else
+                {
+                    set.Start = last.AddSeconds(1);
+                    set.Row = 0;
+                }
+            }
+
+            return list;
+        }
+
+        /// <summary>分段分页抽取数据</summary>
+        /// <param name="start"></param>
+        /// <param name="startRow"></param>
+        /// <param name="maxRows"></param>
+        /// <returns></returns>
+        protected virtual IEntityList FetchData(DateTime start, Int32 startRow, Int32 maxRows)
+        {
+            var fi = Field;
+            var exp = new WhereExpression();
+            if (start > DateTime.MinValue) exp &= fi >= start;
+
+            if (!Where.IsNullOrEmpty()) exp &= Where;
+
+            return Factory.FindAll(exp, fi, null, startRow, maxRows);
+        }
+        #endregion
+
+        #region 日志
+        /// <summary>日志</summary>
+        public ILog Log { get; set; } = Logger.Null;
+        #endregion
+    }
+}
