626e3ea7f0e8acc223e6466cd4ee4fb4694197a4
大石头 <nnhy@vip.qq.com>
2017-07-13 10:34:29 +0800

    根据实体类和key来识别

--- NewLife.Cube/Common/EntityModelBinder.cs
+++ NewLife.Cube/Common/EntityModelBinder.cs
@@ -28,7 +28,7 @@ namespace NewLife.Cube
                     if (uk != null && rvs[uk.Name] != null)
                     {
                         // 查询实体对象用于编辑
-                        var entity = GetEntity(fact.EntityType) ?? fact.FindByKeyForEdit(rvs[uk.Name]) ?? fact.Create();
+                        var entity = GetEntity(fact.EntityType, rvs[uk.Name]) ?? fact.FindByKeyForEdit(rvs[uk.Name]) ?? fact.Create();
 
                         var fs = controllerContext.HttpContext.Request.Form;
                         // 提前填充动态字段的扩展属性
@@ -47,12 +47,9 @@ namespace NewLife.Cube
             return base.CreateModel(controllerContext, bindingContext, modelType);
         }
 
-        private static String GetKey(Type type)
+        private static String GetCacheKey(Type type, Object key)
         {
-            var ctx = HttpContext.Current;
-            var key = "CubeModel_{0}_{1}".F(type.FullName, ctx.Request.Url);
-
-            return key;
+            return "CubeModel_{0}_{1}".F(type.FullName, key);
         }
 
         /// <summary>呈现表单前，保存实体对象。提交时优先使用该对象而不是去数据库查找，避免脏写</summary>
@@ -60,15 +57,17 @@ namespace NewLife.Cube
         internal static void SetEntity(IEntity entity)
         {
             var ctx = HttpContext.Current;
-            var key = GetKey(entity.GetType());
-            ctx.Session[key] = entity;
+            var fact = EntityFactory.CreateOperate(entity.GetType());
+            var uk = fact.Unique;
+            var ckey = GetCacheKey(entity.GetType(), entity[uk.Name]);
+            ctx.Session[ckey] = entity;
         }
 
-        private static IEntity GetEntity(Type type)
+        private static IEntity GetEntity(Type type, Object key)
         {
             var ctx = HttpContext.Current;
-            var key = GetKey(type);
-            return ctx.Session[key] as IEntity;
+            var ckey = GetCacheKey(type, key);
+            return ctx.Session[ckey] as IEntity;
         }
     }
 
