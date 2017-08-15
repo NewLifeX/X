58cb6b38aa68ed8f032b9bad56843d43921ea4d2
大石头 <nnhy@vip.qq.com>
2017-08-15 17:57:24 +0800

    序列从1开始

--- XCode/DataAccessLayer/Database/Oracle.cs
+++ XCode/DataAccessLayer/Database/Oracle.cs
@@ -758,7 +758,7 @@ namespace XCode.DataAccessLayer
             if (String.IsNullOrEmpty(sql)) return sql;
 
             // 感谢@晴天（412684802）和@老徐（gregorius 279504479），这里的最小值开始必须是0，插入的时候有++i的效果，才会得到从1开始的编号
-            var sqlSeq = String.Format("Create Sequence SEQ_{0} Minvalue 0 Maxvalue 9999999999 Start With 0 Increment By 1 Cache 20", table.TableName);
+            var sqlSeq = String.Format("Create Sequence SEQ_{0} Minvalue 1 Maxvalue 9999999999 Start With 0 Increment By 1 Cache 20", table.TableName);
             //return sql + "; " + Environment.NewLine + sqlSeq;
             // 去掉分号后的空格，Oracle不支持同时执行多个语句
             return sql + ";" + Environment.NewLine + sqlSeq;
