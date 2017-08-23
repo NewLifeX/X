7fd40ff29ed48e2bfed6097364e30ed7c0eae61d
大石头 <nnhy@vip.qq.com>
2017-08-23 18:03:36 +0800

    拆分数据同步

--- XCode/Transform/ETL.cs
+++ XCode/Transform/ETL.cs
@@ -6,39 +6,6 @@ using XCode.Membership;
 
 namespace XCode.Transform
 {
-    /// <summary>数据同步</summary>
-    /// <typeparam name="TSource">源实体类</typeparam>
-    /// <typeparam name="TTarget">目标实体类</typeparam>
-    public class ETL<TSource, TTarget> : ETL
-        where TSource : Entity<TSource>, new()
-        where TTarget : Entity<TTarget>, new()
-    {
-        #region 构造
-        /// <summary>实例化数据抽取器</summary>
-        public ETL() : base(Entity<TSource>.Meta.Factory, Entity<TTarget>.Meta.Factory) { }
-        #endregion
-
-        /// <summary>处理单行数据</summary>
-        /// <param name="source">源实体</param>
-        /// <param name="target">目标实体</param>
-        /// <param name="isNew">是否新增</param>
-        protected override IEntity ProcessItem(IEntity source, IEntity target, Boolean isNew)
-        {
-            return ProcessItem(source as TSource, target as TTarget, isNew);
-        }
-
-        /// <summary>处理单行数据</summary>
-        /// <param name="source">源实体</param>
-        /// <param name="target">目标实体</param>
-        /// <param name="isNew">是否新增</param>
-        protected virtual IEntity ProcessItem(TSource source, TTarget target, Boolean isNew)
-        {
-            target.CopyFrom(source, true);
-
-            return target;
-        }
-    }
-
     /// <summary>数据分批统计</summary>
     /// <typeparam name="TSource">源实体类</typeparam>
     public class ETL<TSource> : ETL
@@ -46,24 +13,8 @@ namespace XCode.Transform
     {
         #region 构造
         /// <summary>实例化数据抽取器</summary>
-        public ETL() : base(Entity<TSource>.Meta.Factory, null) { }
+        public ETL() : base(Entity<TSource>.Meta.Factory) { }
         #endregion
-
-        /// <summary>处理单行数据</summary>
-        /// <param name="source">源实体</param>
-        /// <param name="target">目标实体</param>
-        /// <param name="isNew">是否新增</param>
-        protected override IEntity ProcessItem(IEntity source, IEntity target, Boolean isNew)
-        {
-            return ProcessItem(source as TSource);
-        }
-
-        /// <summary>处理单行数据</summary>
-        /// <param name="source">源实体</param>
-        protected virtual IEntity ProcessItem(TSource source)
-        {
-            return source;
-        }
     }
 
     /// <summary>数据抽取转换处理</summary>
@@ -79,12 +30,6 @@ namespace XCode.Transform
 
         /// <summary>数据源抽取器</summary>
         public IExtracter Extracter { get; set; }
-
-        /// <summary>目标实体工厂。分批统计时不需要设定</summary>
-        public IEntityOperate Target { get; set; }
-
-        ///// <summary>逐行处理后自动保存。默认true</summary>
-        //public Boolean AutoSave { get; set; } = true;
 
         /// <summary>最大错误数，连续发生多个错误时停止</summary>
         public Int32 MaxError { get; set; }
@@ -105,11 +50,9 @@ namespace XCode.Transform
 
         /// <summary>实例化数据抽取器</summary>
         /// <param name="source"></param>
-        /// <param name="target"></param>
-        public ETL(IEntityOperate source, IEntityOperate target) : this()
+        public ETL(IEntityOperate source) : this()
         {
             Extracter = new TimeExtracter { Factory = source };
-            Target = target;
         }
         #endregion
 
@@ -126,9 +69,7 @@ namespace XCode.Transform
         }
 
         /// <summary>停止</summary>
-        public virtual void Stop()
-        {
-        }
+        public virtual void Stop() { }
         #endregion
 
         #region 数据转换
@@ -192,9 +133,9 @@ namespace XCode.Transform
                 var ms = sw.ElapsedMilliseconds;
                 st.Speed = ms <= 0 ? 0 : (Int32)(count * 1000 / ms);
 
-                if (ext is TimeExtracter) end = (ext as TimeExtracter).BatchEnd;
+                if (ext is TimeExtracter time) end = time.BatchEnd;
                 var ends = end > DateTime.MinValue && end < DateTime.MaxValue ? ", {0}".F(end) : "";
-                var msg = "共同步{0}行，区间({1}, {2}{3})，{4:n0}ms，{5:n0}tps".F(count, start, row, ends, ms, st.Speed);
+                var msg = "共处理{0}行，区间({1}, {2}{3})，{4:n0}ms，{5:n0}tps".F(count, start, row, ends, ms, st.Speed);
                 WriteLog(msg);
                 //LogProvider.Provider.WriteLog(Name, "同步", msg);
             }
@@ -211,98 +152,27 @@ namespace XCode.Transform
         /// <param name="list"></param>
         protected virtual void ProcessList(IList<IEntity> list)
         {
-            // 批量事务提交
-            var fact = Target;
-            fact?.BeginTransaction();
-            try
+            foreach (var source in list)
             {
-                foreach (var source in list)
+                try
                 {
-                    try
-                    {
-                        // 有目标跟没有目标处理方式不同
-                        if (fact != null)
-                        {
-                            var target = GetItem(source, out var isNew);
-                            target = ProcessItem(source, target, isNew);
-                            SaveItem(target, isNew);
-                        }
-                        else
-                        {
-                            ProcessItem(source, null, false);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        ex = OnError(source, ex);
-                        if (ex != null) throw ex;
-                    }
+                    ProcessItem(source);
                 }
-                fact?.Commit();
+                catch (Exception ex)
+                {
+                    ex = OnError(source, ex);
+                    if (ex != null) throw ex;
+                }
             }
-            catch
-            {
-                fact?.Rollback();
-                throw;
-            }
-        }
-
-        /// <summary>根据源实体获取目标实体</summary>
-        /// <param name="source">源实体</param>
-        /// <param name="isNew">是否新增</param>
-        /// <returns></returns>
-        protected virtual IEntity GetItem(IEntity source, out Boolean isNew)
-        {
-            var key = source[Extracter.Factory.Unique.Name];
-
-            // 查找目标，如果不存在则创建
-            isNew = false;
-            var fact = Target;
-            var target = fact.FindByKey(key);
-            if (target == null)
-            {
-                target = fact.Create();
-                target[fact.Unique.Name] = key;
-                isNew = true;
-            }
-
-            return target;
         }
 
         /// <summary>处理单行数据</summary>
         /// <remarks>打开AutoSave时，上层ProcessList会自动保存数据</remarks>
         /// <param name="source">源实体</param>
-        /// <param name="target">目标实体</param>
-        /// <param name="isNew">是否新增</param>
         /// <returns></returns>
-        protected virtual IEntity ProcessItem(IEntity source, IEntity target, Boolean isNew)
+        protected virtual IEntity ProcessItem(IEntity source)
         {
-            // 同名字段对拷
-            target?.CopyFrom(source, true);
-
-            return target;
-        }
-
-        /// <summary>保存目标实体</summary>
-        /// <param name="target"></param>
-        /// <param name="isNew"></param>
-        protected virtual void SaveItem(IEntity target, Boolean isNew)
-        {
-            // 自动保存
-            //if (AutoSave)
-            //{
-            var st = Stat;
-            if (isNew)
-            {
-                target.Insert();
-                st.Total++;
-            }
-            else
-            {
-                target.Update();
-                st.TotalUpdate++;
-            }
-            //}
+            return source;
         }
 
         private Exception _lastError;
@@ -346,7 +216,7 @@ namespace XCode.Transform
         {
             Log?.Info(Name + " " + format, args);
 
-            Provider?.WriteLog(Name, "同步", format.F(args));
+            Provider?.WriteLog(Name, "处理", format.F(args));
         }
 
         /// <summary>写错误日志</summary>
---
+++ XCode/Transform/Sync.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NewLife.Log;
+using XCode.Membership;
+
+namespace XCode.Transform
+{
+    /// <summary>数据同步</summary>
+    /// <typeparam name="TSource">源实体类</typeparam>
+    /// <typeparam name="TTarget">目标实体类</typeparam>
+    public class Sync<TSource, TTarget> : Sync
+        where TSource : Entity<TSource>, new()
+        where TTarget : Entity<TTarget>, new()
+    {
+        #region 构造
+        /// <summary>实例化数据抽取器</summary>
+        public Sync() : base(Entity<TSource>.Meta.Factory, Entity<TTarget>.Meta.Factory) { }
+        #endregion
+
+        /// <summary>处理单行数据</summary>
+        /// <param name="source">源实体</param>
+        /// <param name="target">目标实体</param>
+        /// <param name="isNew">是否新增</param>
+        protected override IEntity ProcessItem(IEntity source, IEntity target, Boolean isNew)
+        {
+            return ProcessItem(source as TSource, target as TTarget, isNew);
+        }
+
+        /// <summary>处理单行数据</summary>
+        /// <param name="source">源实体</param>
+        /// <param name="target">目标实体</param>
+        /// <param name="isNew">是否新增</param>
+        protected virtual IEntity ProcessItem(TSource source, TTarget target, Boolean isNew)
+        {
+            target.CopyFrom(source, true);
+
+            return target;
+        }
+    }
+
+    public class Sync : ETL
+    {
+        #region 属性
+        /// <summary>目标实体工厂。分批统计时不需要设定</summary>
+        public IEntityOperate Target { get; set; }
+        #endregion
+
+        #region 构造
+        /// <summary>实例化数据抽取器</summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        public Sync(IEntityOperate source, IEntityOperate target) : base(source)
+        {
+            Target = target;
+        }
+        #endregion
+
+        #region 数据同步
+        /// <summary>处理列表，批量事务提交</summary>
+        /// <param name="list"></param>
+        protected override void ProcessList(IList<IEntity> list)
+        {
+            // 批量事务提交
+            var fact = Target;
+            fact?.BeginTransaction();
+            try
+            {
+                foreach (var source in list)
+                {
+                    try
+                    {
+                        // 有目标跟没有目标处理方式不同
+                        if (fact != null)
+                        {
+                            var target = GetItem(source, out var isNew);
+                            target = ProcessItem(source, target, isNew);
+                            SaveItem(target, isNew);
+                        }
+                        else
+                        {
+                            ProcessItem(source, null, false);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ex = OnError(source, ex);
+                        if (ex != null) throw ex;
+                    }
+                }
+                fact?.Commit();
+            }
+            catch
+            {
+                fact?.Rollback();
+                throw;
+            }
+        }
+
+        /// <summary>处理单行数据</summary>
+        /// <remarks>打开AutoSave时，上层ProcessList会自动保存数据</remarks>
+        /// <param name="source">源实体</param>
+        /// <param name="target">目标实体</param>
+        /// <param name="isNew">是否新增</param>
+        /// <returns></returns>
+        protected virtual IEntity ProcessItem(IEntity source, IEntity target, Boolean isNew)
+        {
+            // 同名字段对拷
+            target?.CopyFrom(source, true);
+
+            return target;
+        }
+
+        /// <summary>根据源实体获取目标实体</summary>
+        /// <param name="source">源实体</param>
+        /// <param name="isNew">是否新增</param>
+        /// <returns></returns>
+        protected virtual IEntity GetItem(IEntity source, out Boolean isNew)
+        {
+            var key = source[Extracter.Factory.Unique.Name];
+
+            // 查找目标，如果不存在则创建
+            isNew = false;
+            var fact = Target;
+            var target = fact.FindByKey(key);
+            if (target == null)
+            {
+                target = fact.Create();
+                target[fact.Unique.Name] = key;
+                isNew = true;
+            }
+
+            return target;
+        }
+
+        /// <summary>保存目标实体</summary>
+        /// <param name="target"></param>
+        /// <param name="isNew"></param>
+        protected virtual void SaveItem(IEntity target, Boolean isNew)
+        {
+            // 自动保存
+            //if (AutoSave)
+            //{
+            var st = Stat;
+            if (isNew)
+            {
+                target.Insert();
+                st.Total++;
+            }
+            else
+            {
+                target.Update();
+                st.TotalUpdate++;
+            }
+            //}
+        }
+        #endregion
+    }
+}
