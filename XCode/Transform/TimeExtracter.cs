f8cd18729116b93c0de97ea8fb124c33f1975455
大石头 <nnhy@vip.qq.com>
2017-09-20 11:24:17 +0800

    有可能时间字段为空

--- XCode/Transform/TimeExtracter.cs
+++ XCode/Transform/TimeExtracter.cs
@@ -132,6 +132,8 @@ namespace XCode.Transform
             if (list.Count > 0)
             {
                 var last = (DateTime)list.Last()[FieldName];
+                // 有可能时间字段为空
+                if (last <= DateTime.MinValue) last = list.Max(e => (DateTime)e[FieldName]);
                 // 满一批，后续还有数据
                 if (list.Count >= size)
                 {
