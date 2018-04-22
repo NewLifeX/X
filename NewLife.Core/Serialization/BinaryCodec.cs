09db300fdd6be6d6743f4ed230ea55bf9d947904
大石头 <nnhy@vip.qq.com>
2018-04-23 00:05:35 +0800

    序列化命名空间

--- NewLife.Core/Serialization/BinaryCodec.cs
+++ NewLife.Core/Serialization/BinaryCodec.cs
@@ -1,34 +1,33 @@
 ﻿using System;
 using NewLife.Data;
-using NewLife.Serialization;
+using NewLife.Net.Handlers;
 
-namespace NewLife.Net.Handlers
+namespace NewLife.Serialization
 {
-    /// <summary>Json编码解码器</summary>
-    public class JsonCodec<T> : Handler
+    /// <summary>二进制编码解码器</summary>
+    public class BinaryCodec<T> : Handler
     {
         /// <summary>使用7位编码整数。默认true使用</summary>
         public Boolean EncodedInt { get; set; } = true;
 
-        /// <summary>对象转Json</summary>
+        /// <summary>对象转二进制</summary>
         /// <param name="context"></param>
         /// <param name="message"></param>
         /// <returns></returns>
         public override Object Write(IHandlerContext context, Object message)
         {
-            if (message is T entity) return new Packet(entity.ToJson().GetBytes());
+            if (message is T entity) return Binary.FastWrite(entity, EncodedInt);
 
             return message;
         }
 
-        /// <summary>Json转对象</summary>
+        /// <summary>二进制转对象</summary>
         /// <param name="context"></param>
         /// <param name="message"></param>
         /// <returns></returns>
         public override Object Read(IHandlerContext context, Object message)
         {
-            if (message is Packet pk) message = pk.ToStr();
-            if (message is String str) return str.ToJsonEntity<T>();
+            if (message is Packet pk) return Binary.FastRead<T>(pk.GetStream(), EncodedInt);
 
             return message;
         }
