0445b7bb77205d45259df63665e5f4ecd03399fd
大石头 <nnhy@vip.qq.com>
2018-08-29 19:48:31 +0800

    RedisQueue转到完整版NewLife.Redis

--- NewLife.Core/Caching/Redis.cs
+++ NewLife.Core/Caching/Redis.cs
@@ -304,12 +304,6 @@ namespace NewLife.Caching
 
             Execute(rds => rds.SetAll(values));
         }
-
-        /// <summary>获取队列</summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="key"></param>
-        /// <returns></returns>
-        public override IProducerConsumer<T> GetQueue<T>(String key) => new RedisQueue<T>(this, key);
         #endregion
 
         #region 高级操作
@@ -471,53 +465,5 @@ namespace NewLife.Caching
         /// <param name="args"></param>
         public void WriteLog(String format, params Object[] args) => Log?.Info(format, args);
         #endregion
-    }
-
-    /// <summary>生产者消费者</summary>
-    /// <typeparam name="T"></typeparam>
-    class RedisQueue<T> : IProducerConsumer<T>
-    {
-        private Redis _Redis;
-        private String _Key;
-
-        public RedisQueue(Redis rds, String key)
-        {
-            _Redis = rds;
-            _Key = key;
-        }
-
-        /// <summary>生产添加</summary>
-        /// <param name="values"></param>
-        /// <returns></returns>
-        public Int32 Add(IEnumerable<T> values)
-        {
-            var ps = new List<Object>
-            {
-                _Key
-            };
-            foreach (var item in values)
-            {
-                ps.Add(item);
-            }
-            return _Redis.Execute(rc => rc.Execute<Int32>("RPUSH", ps.ToArray()));
-        }
-
-        /// <summary>消费获取</summary>
-        /// <param name="count"></param>
-        /// <returns></returns>
-        public IEnumerable<T> Take(Int32 count = 1)
-        {
-            if (count <= 0) yield break;
-
-            //return _Redis.Execute(rc => rc.Execute<T[]>("LRANGE", _Key, 0, count - 1));
-
-            for (var i = 0; i < count; i++)
-            {
-                var value = _Redis.Execute(rc => rc.Execute<T>("LPOP", _Key));
-                if (Equals(value, default(T))) break;
-
-                yield return value;
-            }
-        }
     }
 }
