ad9846ce797ad779601d8e1dcad607a50b207dc1
大石头 <nnhy@vip.qq.com>
2018-09-15 18:41:32 +0800

    恢复原来顺序，调整顺序后MySql报错，原因未知

--- XCode/DataAccessLayer/Database/MySql.cs
+++ XCode/DataAccessLayer/Database/MySql.cs
@@ -316,6 +316,17 @@ namespace XCode.DataAccessLayer
             if (updateColumns != null || addColumns != null)
             {
                 sb.Append(" On Duplicate Key Update ");
+                if (updateColumns != null)
+                {
+                    foreach (var dc in columns)
+                    {
+                        if (dc.Identity || dc.PrimaryKey) continue;
+
+                        if (updateColumns.Contains(dc.Name) && (addColumns == null || !addColumns.Contains(dc.Name)))
+                            sb.AppendFormat("{0}=Values({0}),", db.FormatName(dc.ColumnName));
+                    }
+                    sb.Length--;
+                }
                 if (addColumns != null)
                 {
                     sb.Append(",");
@@ -325,17 +336,6 @@ namespace XCode.DataAccessLayer
 
                         if (addColumns.Contains(dc.Name))
                             sb.AppendFormat("{0}={0}+Values({0}),", db.FormatName(dc.ColumnName));
-                    }
-                    sb.Length--;
-                }
-                if (updateColumns != null)
-                {
-                    foreach (var dc in columns)
-                    {
-                        if (dc.Identity || dc.PrimaryKey) continue;
-
-                        if (updateColumns.Contains(dc.Name) && (addColumns == null || !addColumns.Contains(dc.Name)))
-                            sb.AppendFormat("{0}=Values({0}),", db.FormatName(dc.ColumnName));
                     }
                     sb.Length--;
                 }
