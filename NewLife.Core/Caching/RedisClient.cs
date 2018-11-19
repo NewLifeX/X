93461426420589cf9ff0cca1dfdb250946d341af
大石头 <nnhy@vip.qq.com>
2018-11-19 11:05:53 +0800

    遍历两个数组时，要同时判断长度

--- NewLife.Core/Caching/RedisClient.cs
+++ NewLife.Core/Caching/RedisClient.cs
@@ -588,7 +588,7 @@ namespace NewLife.Caching
             var dic = new Dictionary<String, T>();
             if (rs == null) return dic;
 
-            for (var i = 0; i < rs.Length; i++)
+            for (var i = 0; i < ks.Length && i < rs.Length; i++)
             {
                 dic[ks[i]] = FromBytes<T>(rs[i] as Packet);
             }
