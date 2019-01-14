1b84bb1f9b50f69dbe6ef3941128baca25e59f2a
大石头 <nnhy@vip.qq.com>
2019-01-14 20:50:22 +0800

    新增数据包编码器PacketCodec，统一各种粘包处理逻辑

--- NewLife.Core/Net/Handlers/SplitDataCodec.cs
+++ NewLife.Core/Net/Handlers/SplitDataCodec.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using NewLife.Data;
+using NewLife.Messaging;
 using NewLife.Model;
-using NewLife.Threading;
 
 namespace NewLife.Net.Handlers
 {
@@ -58,97 +57,10 @@ namespace NewLife.Net.Handlers
         protected IList<Packet> Decode(IHandlerContext context, Packet pk)
         {
             var ss = context.Owner as IExtend;
-            var mcp = ss["CodecItem"] as CodecItem;
-            if (mcp == null) ss["CodecItem"] = mcp = new CodecItem();
+            var pc = ss["Codec"] as PacketCodec;
+            if (pc == null) ss["Codec"] = pc = new PacketCodec { MaxCache = MaxCacheDataLength, GetLength = GetLineLength };
 
-            return Parse(pk, mcp, pk1 => GetLineLength(pk1));
-        }
-        
-        /// <summary>分析数据流，得到一帧数据</summary>
-        /// <param name="pk">待分析数据包</param>
-        /// <param name="codec">参数</param>
-        /// <param name="getLength">获取长度</param>
-        /// <param name="expire">缓存有效期</param>
-        /// <returns></returns>
-        protected virtual IList<Packet> Parse(Packet pk, CodecItem codec, Func<Packet, Int32> getLength, Int32 expire = 5000)
-        {
-            var ms = codec.Stream;
-            var nodata = ms == null || ms.Position < 0 || ms.Position >= ms.Length;
-
-            var list = new List<Packet>();
-            // 内部缓存没有数据，直接判断输入数据流是否刚好一帧数据，快速处理，绝大多数是这种场景
-            if (nodata)
-            {
-                if (pk == null) return list.ToArray();
-
-                var idx = 0;
-                while (idx < pk.Total)
-                {
-                    // 切出来一片，计算长度
-                    var pk2 = pk.Slice(idx);
-                    var len = getLength(pk2);
-                    if (len <= 0 || len > pk2.Count) break;
-
-                    // 根据计算得到的长度，重新设置数据片正确长度
-                    pk2.Set(pk2.Data, pk2.Offset, len);
-                    list.Add(pk2);
-                    idx += len;
-                }
-                // 如果没有剩余，可以返回
-                if (idx == pk.Total) return list.ToArray();
-
-                // 剩下的
-                pk = pk.Slice(idx);
-            }
-
-            if (ms == null) codec.Stream = ms = new MemoryStream();
-
-            // 加锁，避免多线程冲突
-            lock (ms)
-            {
-                // 超过该时间后按废弃数据处理  2019-1-9 +待处理数据超过设定值也按废弃数据处理【重要】
-                var now = TimerX.Now;
-                if ((ms.Length > ms.Position && codec.Last.AddMilliseconds(expire) < now) || ms.Length >= MaxCacheDataLength)
-                {
-                    ms.SetLength(0);
-                    ms.Position = 0;
-                }
-                codec.Last = now;
-
-                // 合并数据到最后面
-                if (pk != null && pk.Total > 0)
-                {
-                    var p = ms.Position;
-                    ms.Position = ms.Length;
-                    pk.WriteTo(ms);
-                    ms.Position = p;
-                }
-
-                // 尝试解包
-                while (ms.Position < ms.Length)
-                {
-                    var pk2 = new Packet(ms);
-                    var len = getLength(pk2);
-
-                    // 资源不足一包
-                    if (len <= 0 || len > pk2.Total) break;
-
-                    // 解包成功
-                    pk2.Set(pk2.Data, pk2.Offset, len);
-                    list.Add(pk2);
-
-                    ms.Seek(len, SeekOrigin.Current);
-                }
-
-                // 如果读完了数据，需要重置缓冲区
-                if (ms.Position >= ms.Length)
-                {
-                    ms.SetLength(0);
-                    ms.Position = 0;
-                }
-
-                return list;
-            }
+            return pc.Parse(pk);
         }
 
         /// <summary>
