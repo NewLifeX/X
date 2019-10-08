6cc675fcced7494601a8c0e90acf703db4d1ce05
Killer <Soar360@live.com>
2019-10-08 14:52:41 +0800

    增加 MobileApp 演示项目

---
+++ Samples/MobileApp/MobileApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobileApp.Models;
+
+namespace MobileApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
