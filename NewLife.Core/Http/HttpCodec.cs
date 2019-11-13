75d97e9c7bc8df4a8517c8d41490c73c91330bf2
大石头 <nnhy@vip.qq.com>
2019-11-13 22:05:28 +0800

    HttpCodec支持请求包被拆为多部分

--- NewLife.Core/Http/HttpCodec.cs
+++ NewLife.Core/Http/HttpCodec.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NewLife.Data;
 using NewLife.Messaging;
 using NewLife.Model;
@@ -31,23 +32,71 @@ namespace NewLife.Http
             if (!(message is Packet pk)) return base.Read(context, message);
 
             // 是否Http请求
+            var isGet = pk.Count >= 4 && pk[0] == 'G' && pk[1] == 'E' && pk[2] == 'T' && pk[3] == ' ';
+            var isPost = pk.Count >= 5 && pk[0] == 'P' && pk[1] == 'O' && pk[2] == 'S' && pk[3] == 'T' && pk[4] == ' ';
+
+            // 该连接第一包检查是否Http
             var ext = context.Owner as IExtend;
             if (!(ext["Encoder"] is HttpEncoder))
             {
-                if (!(pk[0] == 'G' && pk[1] == 'E' && pk[2] == 'T') &&
-                    !(pk[0] == 'P' && pk[1] == 'O' && pk[2] == 'S' && pk[3] == 'T'))
-                    return base.Read(context, message);
+                // 第一个请求必须是GET/POST，才执行后续操作
+                if (!isGet && !isPost) return base.Read(context, message);
 
                 ext["Encoder"] = new HttpEncoder();
             }
 
-            // 解码得到消息
+            // 检查是否有未完成消息
+            if (!(ext["Message"] is HttpMessage msg))
+            {
+                // 解码得到消息
+                msg = new HttpMessage();
+                if (!msg.Read(pk)) throw new XException("Http请求头不完整");
 
-            var msg = new HttpMessage();
-            msg.Read(pk);
+                // GET请求一次性过来，暂时不支持头部被拆为多包的场景
+                if (isGet)
+                {
+                    // 匹配输入回调，让上层事件收到分包信息
+                    context.FireRead(msg);
+                }
+                // POST可能多次，最典型的是头部和主体分离
+                else
+                {
+                    if (!msg.ParseHeaders()) throw new XException("Http头部解码失败");
 
-            // 匹配输入回调，让上层事件收到分包信息
-            context.FireRead(msg);
+                    // 消息完整才允许上报
+                    if (msg.ContentLength == 0 || msg.ContentLength > 0 && msg.Payload != null && msg.Payload.Total >= msg.ContentLength)
+                    {
+                        // 匹配输入回调，让上层事件收到分包信息
+                        context.FireRead(msg);
+                    }
+                    else
+                    {
+                        // 请求不完整，拷贝一份，避免缓冲区重用
+                        if (msg.Header != null) msg.Header = msg.Header.Clone();
+                        if (msg.Payload != null) msg.Payload = msg.Payload.Clone();
+
+                        ext["Message"] = msg;
+                    }
+                }
+            }
+            else
+            {
+                // 数据包拼接到上一个未完整消息中
+                if (msg.Payload == null)
+                    msg.Payload = pk;
+                else
+                    msg.Payload.Append(pk);
+
+                // 消息完整才允许上报
+                if (msg.ContentLength == 0 || msg.ContentLength > 0 && msg.Payload != null && msg.Payload.Total >= msg.ContentLength)
+                {
+                    // 匹配输入回调，让上层事件收到分包信息
+                    context.FireRead(msg);
+
+                    // 移除消息
+                    ext.Items.Remove("Message");
+                }
+            }
 
             //if (pk.ToStr(null, 0, 4) == "HTTP")
             //{
@@ -73,6 +122,7 @@ namespace NewLife.Http
     /// <summary>Http消息</summary>
     public class HttpMessage : IMessage
     {
+        #region 属性
         /// <summary>是否响应</summary>
         public Boolean Reply { get; set; }
 
@@ -88,6 +138,14 @@ namespace NewLife.Http
         /// <summary>负载数据</summary>
         public Packet Payload { get; set; }
 
+        /// <summary>内容长度</summary>
+        public Int32 ContentLength { get; set; } = -1;
+
+        /// <summary>头部集合</summary>
+        public IDictionary<String, String> Headers { get; set; }
+        #endregion
+
+        #region 方法
         /// <summary>根据请求创建配对的响应消息</summary>
         /// <returns></returns>
         public IMessage CreateReply()
@@ -117,6 +175,33 @@ namespace NewLife.Http
             return true;
         }
 
+        /// <summary>解码头部</summary>
+        public virtual Boolean ParseHeaders()
+        {
+            var pk = Header;
+            if (pk == null || pk.Total == 0) return false;
+
+            var dic = new Dictionary<String, String>(StringComparer.OrdinalIgnoreCase);
+            var ss = pk.ToStr().Split(Environment.NewLine);
+            foreach (var item in ss)
+            {
+                var kv = item.Split(":");
+                if (kv != null && kv.Length >= 2)
+                {
+                    dic[kv[0].Trim()] = kv[1].Trim();
+                }
+            }
+            Headers = dic;
+
+            // 内容长度
+            if (dic.TryGetValue("Content-Length", out var str))
+                ContentLength = str.ToInt();
+            else
+                ContentLength = 0;
+
+            return true;
+        }
+
         /// <summary>把消息转为封包</summary>
         /// <returns></returns>
         public virtual Packet ToPacket()
@@ -130,5 +215,6 @@ namespace NewLife.Http
 
             return pk;
         }
+        #endregion
     }
 }
