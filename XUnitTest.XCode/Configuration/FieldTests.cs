213a8ce0571dad6a55dedf92328df7416903597c
大石头 <nnhy@vip.qq.com>
2019-11-25 22:06:45 +0800

    字段单元测试完善

--- XUnitTest.XCode/Configuration/FieldTests.cs
+++ XUnitTest.XCode/Configuration/FieldTests.cs
@@ -14,6 +14,7 @@ namespace XUnitTest.XCode.Configuration
             var fi = UserX._.Password;
             Assert.Equal("Password", fi.Name);
             Assert.Equal("密码", fi.DisplayName);
+            Assert.Equal("密码", fi.Description);
             Assert.Equal(typeof(String), fi.Type);
             Assert.False(fi.IsIdentity);
             Assert.False(fi.PrimaryKey);
@@ -21,12 +22,15 @@ namespace XUnitTest.XCode.Configuration
             Assert.True(fi.IsNullable);
             Assert.Equal(50, fi.Length);
             Assert.True(fi.IsDataObjectField);
+            Assert.False(fi.IsDynamic);
             Assert.Equal("Password", fi.ColumnName);
             Assert.False(fi.ReadOnly);
             Assert.NotNull(fi.Table);
             Assert.NotNull(fi.Field);
             Assert.NotNull(fi.Factory);
+            Assert.Equal("Password", fi.FormatedName);
             Assert.Null(fi.OriField);
+            Assert.Null(fi.Map);
 
             Assert.True(UserX._.ID.IsIdentity);
             Assert.True(UserX._.ID.PrimaryKey);
