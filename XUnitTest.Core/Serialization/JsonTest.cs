1c8b8eca63d47b4c12959be0082c84e8aebab568
大石头 <nnhy@vip.qq.com>
2020-01-09 14:54:16 +0800

    调整单元测试目录结构

---
+++ XUnitTest.Core/Serialization/JsonTest.cs
@@ -0,0 +1,28 @@
+﻿using NewLife;
+using NewLife.Log;
+using NewLife.Serialization;
+using Xunit;
+
+namespace XUnitTest.Serialization
+{
+    public class JsonTest
+    {
+        [Fact(DisplayName = "基础测试")]
+        public void Test1()
+        {
+            var set = new Setting
+            {
+                LogLevel = LogLevel.Error,
+                LogPath = "xxx",
+            };
+
+            var js = set.ToJson(true, false, false);
+            Assert.True(js.StartsWith("{") && js.EndsWith("}"));
+
+            var set2 = js.ToJsonEntity<Setting>();
+
+            Assert.Equal(LogLevel.Error, set2.LogLevel);
+            Assert.Equal("xxx", set2.LogPath);
+        }
+    }
+}
---
+++ XUnitTest.Core/Serialization/XmlTest.cs
@@ -0,0 +1,32 @@
+﻿using NewLife;
+using NewLife.Log;
+using NewLife.Xml;
+using Xunit;
+
+namespace XUnitTest.Serialization
+{
+    public class XmlTest
+    {
+        [Fact(DisplayName = "基础测试")]
+        public void Test1()
+        {
+            var set = new Setting
+            {
+                LogLevel = LogLevel.Error,
+                LogPath = "xxx",
+            };
+
+            var xml = set.ToXml();
+            Assert.Contains("<Setting>", xml);
+            Assert.Contains("</Setting>", xml);
+
+            var xml2 = set.ToXml(null, false, true);
+            Assert.Contains("<Setting ", xml2);
+
+            var set2 = xml.ToXmlEntity<Setting>();
+
+            Assert.Equal(LogLevel.Error, set2.LogLevel);
+            Assert.Equal("xxx", set2.LogPath);
+        }
+    }
+}
