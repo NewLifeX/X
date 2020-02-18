8e3c39c68749334caecc94381d5d7a96f48a5698
丁川 <2505111990@qq.com>
2020-02-18 22:54:30 +0800

    GetService方法与微软方法调用冲突，因功能特性一致，故取消项目中GetService方法

--- NewLife.Core/Model/ModelExtension.cs
+++ NewLife.Core/Model/ModelExtension.cs
@@ -1,22 +1,22 @@
-﻿using NewLife.Reflection;
+﻿//using NewLife.Reflection;
 
-namespace System
-{
-    /// <summary>模型扩展</summary>
-    public static class ModelExtension
-    {
-        /// <summary>获取指定类型的服务对象</summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="provider"></param>
-        /// <returns></returns>
-        public static T GetService<T>(this IServiceProvider provider)
-        {
-            if (provider == null) return default(T);
+//namespace System
+//{
+//    /// <summary>模型扩展</summary>
+//    public static class ModelExtension
+//    {
+//        /// <summary>获取指定类型的服务对象</summary>
+//        /// <typeparam name="T"></typeparam>
+//        /// <param name="provider"></param>
+//        /// <returns></returns>
+//        public static T GetService<T>(this IServiceProvider provider)
+//        {
+//            if (provider == null) return default(T);
 
-            // 服务类是否当前类的基类
-            if (provider.GetType().As<T>()) return (T)provider;
+//            // 服务类是否当前类的基类
+//            if (provider.GetType().As<T>()) return (T)provider;
 
-            return (T)provider.GetService(typeof(T));
-        }
-    }
-}
+//            return (T)provider.GetService(typeof(T));
+//        }
+//    }
+//}
