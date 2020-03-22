634fcacd1bd1c257dd4e7315c8f00fb46217bd81
大石头 <nnhy@vip.qq.com>
2020-03-22 17:58:53 +0800

    日志头输出系统临时目录

--- Test/Program.cs
+++ Test/Program.cs
@@ -74,6 +74,7 @@ namespace Test
         {
             XTrace.WriteLine("FullPath:{0}", ".".GetFullPath());
             XTrace.WriteLine("BasePath:{0}", ".".GetBasePath());
+            XTrace.WriteLine("TempPath:{0}", Path.GetTempPath());
 
             var mi = MachineInfo.Current ?? MachineInfo.RegisterAsync().Result;
 
