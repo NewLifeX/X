24bc079fa744c7743f780ade128d3e5c3a3056da
大石头 <nnhy@vip.qq.com>
2020-03-28 00:12:14 +0800

    systemd安装和卸载服务

--- NewLife.Agent/Systemd.cs
+++ NewLife.Agent/Systemd.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using NewLife.Log;
 
 namespace NewLife.Agent
 {
@@ -15,14 +18,18 @@ namespace NewLife.Agent
         /// <summary>实例化</summary>
         public Systemd()
         {
-            var p = "/usr/lib/systemd/system/";
-            if (Directory.Exists(p))
-                _path = p;
-            else
+            var ps = new[] {
+                "/etc/systemd/system",
+                "/run/systemd/system",
+                "/usr/lib/systemd/system",
+                "/lib/systemd/system" };
+            foreach (var p in ps)
             {
-                p = "/etc/systemd/system";
                 if (Directory.Exists(p))
+                {
                     _path = p;
+                    break;
+                }
             }
         }
 
@@ -38,12 +45,26 @@ namespace NewLife.Agent
         /// <summary>服务是否已安装</summary>
         /// <param name="serviceName">服务名</param>
         /// <returns></returns>
-        public override Boolean IsInstalled(String serviceName) => false;
+        public override Boolean IsInstalled(String serviceName)
+        {
+            var file = _path.CombinePath($"{serviceName}.service");
+
+            return File.Exists(file);
+        }
 
         /// <summary>服务是否已启动</summary>
         /// <param name="serviceName">服务名</param>
         /// <returns></returns>
-        public override Boolean IsRunning(String serviceName) => false;
+        public override Boolean IsRunning(String serviceName)
+        {
+            var file = _path.CombinePath($"{serviceName}.service");
+            if (!File.Exists(file)) return false;
+
+            var str = Execute("systemctl", $"status {serviceName}", false);
+            if (!str.IsNullOrEmpty() && str.Contains("running")) return true;
+
+            return false;
+        }
 
         /// <summary>安装服务</summary>
         /// <param name="serviceName">服务名</param>
@@ -51,21 +72,80 @@ namespace NewLife.Agent
         /// <param name="binPath">文件路径</param>
         /// <param name="description">描述信息</param>
         /// <returns></returns>
-        public override Boolean Install(String serviceName, String displayName, String binPath, String description) => false;
+        public override Boolean Install(String serviceName, String displayName, String binPath, String description)
+        {
+            XTrace.WriteLine("{0}.Install {1}, {2}, {3}, {4}", GetType().Name, serviceName, displayName, binPath, description);
+
+            var file = _path.CombinePath($"{serviceName}.service");
+            XTrace.WriteLine(file);
+
+            //var asm = Assembly.GetEntryAssembly();
+            var des = !displayName.IsNullOrEmpty() ? displayName : description;
+
+            var sb = new StringBuilder();
+            sb.AppendLine("[Unit]");
+            sb.AppendLine($"Description={des}");
+
+            sb.AppendLine();
+            sb.AppendLine("[Service]");
+            sb.AppendLine("Type=simple");
+            //sb.AppendLine($"ExecStart=/usr/bin/dotnet {asm.Location}");
+            sb.AppendLine($"ExecStart={binPath}");
+            sb.AppendLine("Restart=on-failure");
+
+            sb.AppendLine();
+            sb.AppendLine("[Install]");
+            sb.AppendLine("WantedBy=multi-user.target");
+
+            File.WriteAllText(file, sb.ToString());
+
+            // sudo systemctl daemon-reload
+            // sudo systemctl enable StarAgent
+            // sudo systemctl start StarAgent
+
+            Execute("systemctl", "daemon-reload");
+            Execute("systemctl", $"enable {serviceName}");
+            //Execute("systemctl", $"start {serviceName}");
+
+            return true;
+        }
 
         /// <summary>卸载服务</summary>
         /// <param name="serviceName">服务名</param>
         /// <returns></returns>
-        public override Boolean Remove(String serviceName) => false;
+        public override Boolean Remove(String serviceName)
+        {
+            XTrace.WriteLine("{0}.Remove {1}", GetType().Name, serviceName);
+
+            var file = _path.CombinePath($"{serviceName}.service");
+            if (File.Exists(file)) File.Delete(file);
+
+            return true;
+        }
 
         /// <summary>启动服务</summary>
         /// <param name="serviceName">服务名</param>
         /// <returns></returns>
-        public override Boolean Start(String serviceName) => false;
+        public override Boolean Start(String serviceName) => Execute("systemctl", $"start {serviceName}") != null;
 
         /// <summary>停止服务</summary>
         /// <param name="serviceName">服务名</param>
         /// <returns></returns>
-        public override Boolean Stop(String serviceName) => false;
+        public override Boolean Stop(String serviceName) => Execute("systemctl", $"stop {serviceName}") != null;
+
+        private static String Execute(String cmd, String arguments, Boolean writeLog = true)
+        {
+            if (writeLog) XTrace.WriteLine("{0} {1}", cmd, arguments);
+
+            var psi = new ProcessStartInfo(cmd, arguments) { RedirectStandardOutput = true };
+            var process = Process.Start(psi);
+            if (!process.WaitForExit(3_000))
+            {
+                process.Kill();
+                return null;
+            }
+
+            return process.StandardOutput.ReadToEnd();
+        }
     }
 }
