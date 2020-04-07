3d443bc99cf58a9992cf2f5d204448a1c4d0bc4f
大石头 <nnhy@vip.qq.com>
2020-04-07 18:24:54 +0800

    ApiException暂时不加错误码

--- NewLife.Core/Remoting/ApiException.cs
+++ NewLife.Core/Remoting/ApiException.cs
@@ -8,8 +8,8 @@ namespace NewLife.Remoting
         /// <summary>代码</summary>
         public Int32 Code { get; set; }
 
-        /// <summary>异常消息。已重置，加上错误码前缀</summary>
-        public override String Message => $"[{Code}]{base.Message}";
+        ///// <summary>异常消息。已重载，加上错误码前缀</summary>
+        //public override String Message => $"[{Code}]{base.Message}";
 
         /// <summary>实例化远程调用异常</summary>
         /// <param name="code"></param>
