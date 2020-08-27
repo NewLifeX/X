46119c225d09d8a96ff3890306787c13007865e0
大石头 <nnhy@vip.qq.com>
2020-08-27 08:20:36 +0800

    ClassBuilder.Save

--- XUnitTest.XCode/Code/ClassBuilderTests.cs
+++ XUnitTest.XCode/Code/ClassBuilderTests.cs
@@ -5,6 +5,7 @@ using XCode.Code;
 using XCode.Membership;
 using Xunit;
 using System.IO;
+using NewLife.Security;
 
 namespace XUnitTest.XCode.Code
 {
@@ -73,6 +74,32 @@ namespace XUnitTest.XCode.Code
 
             var target = File.ReadAllText("Code\\class_user_interface.cs".GetFullPath());
             Assert.Equal(target, rs);
+        }
+
+        [Fact]
+        public void Save()
+        {
+            var builder = new ClassBuilder();
+            builder.Table = UserX.Meta.Table.DataTable;
+            builder.Pure = true;
+            builder.Output = ".\\Output\\" + Rand.NextString(8);
+
+            builder.Execute();
+
+            var file = (builder.Output + "\\" + builder.Table.DisplayName + ".cs").GetFullPath();
+            if (File.Exists(file)) File.Delete(file);
+
+            builder.Save();
+            Assert.True(File.Exists(file));
+
+            file = (builder.Output + "\\" + builder.Table.Name + ".xs").GetFullPath();
+            if (File.Exists(file)) File.Delete(file);
+
+            builder.Save(".xs", false, false);
+            Assert.True(File.Exists(file));
+
+            // 清理
+            Directory.Delete(builder.Output.GetFullPath(), true);
         }
     }
 }
