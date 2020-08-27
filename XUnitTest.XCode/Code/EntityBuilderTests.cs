8518b7e4fcc81da747c558c55cf46e5981337f9f
大石头 <nnhy@vip.qq.com>
2020-08-27 08:41:02 +0800

    EntityBuilder 单元测试

---
+++ XUnitTest.XCode/Code/EntityBuilderTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using XCode.Code;
+using XCode.Membership;
+using Xunit;
+
+namespace XUnitTest.XCode.Code
+{
+    public class EntityBuilderTests
+    {
+        [Fact]
+        public void Normal()
+        {
+            var builder = new EntityBuilder();
+            builder.Table = UserX.Meta.Table.DataTable;
+            builder.ConnName = "MyConn";
+            builder.Namespace = "Company.MyName";
+            builder.Usings.Add("NewLife.Remoting");
+
+            builder.Execute();
+
+            var rs = builder.ToString();
+            Assert.NotEmpty(rs);
+
+            var target = File.ReadAllText("Code\\entity_user_normal.cs".GetFullPath());
+            Assert.Equal(target, rs);
+        }
+
+        [Fact]
+        public void GenericType()
+        {
+            var builder = new EntityBuilder();
+            builder.Table = UserX.Meta.Table.DataTable;
+            builder.GenericType = true;
+            builder.Namespace = "Company.MyName";
+
+            builder.Execute();
+
+            var rs = builder.ToString();
+            Assert.NotEmpty(rs);
+
+            var target = File.ReadAllText("Code\\entity_user_generictype.cs".GetFullPath());
+            Assert.Equal(target, rs);
+        }
+    }
+}
