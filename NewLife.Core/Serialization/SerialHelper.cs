16435520a1dbe3e316cff61e27965bf658c472d1
智能石头 <nnhy@vip.qq.com>
2021-01-15 15:47:21 +0800

    依据 Json/Xml 字典生成实体模型类

--- NewLife.Core/Serialization/SerialHelper.cs
+++ NewLife.Core/Serialization/SerialHelper.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.Serialization;
+using System.Text;
 using System.Xml.Serialization;
+using NewLife.Reflection;
 
 namespace NewLife.Serialization
 {
@@ -32,6 +35,55 @@ namespace NewLife.Serialization
             _cache.TryAdd(pi, name);
 
             return name;
+        }
+
+        /// <summary>依据 Json/Xml 字典生成实体模型类</summary>
+        /// <param name="dic"></param>
+        /// <param name="className"></param>
+        /// <returns></returns>
+        public static String BuildModelClass(this IDictionary<String, Object> dic, String className = "Model")
+        {
+            if (dic == null || dic.Count == 0) return null;
+
+            var sb = new StringBuilder();
+
+            BuildModel(sb, dic, className, null);
+
+            return sb.ToString();
+        }
+
+        private static void BuildModel(StringBuilder sb, IDictionary<String, Object> dic, String className, String prefix)
+        {
+            sb.AppendLine($"{prefix}public class {className}");
+            sb.AppendLine($"{prefix}{{");
+
+            var line = 0;
+            foreach (var item in dic)
+            {
+                var name = item.Key;
+                if (Char.IsLower(name[0])) name = Char.ToUpper(name[0]) + name.Substring(1);
+
+                if (line++ > 0) sb.AppendLine();
+
+                var type = item.Value?.GetType() ?? typeof(Object);
+                if (type.GetTypeCode() != TypeCode.Object)
+                    sb.AppendLine($"{prefix}\tpublic {type.Name} {name} {{ get; set; }}");
+                else if (item.Value is IDictionary<String, Object> sub)
+                {
+                    var subclassName = name + "Model";
+                    sb.AppendLine($"{prefix}\tpublic {subclassName} {name} {{ get; set; }}");
+                    sb.AppendLine();
+
+                    BuildModel(sb, sub, subclassName, prefix + "\t");
+                }
+                else if (item.Value is IList<Object> list)
+                {
+                    var elmType = list.Count > 0 ? list[0].GetType() : type.GetElementTypeEx();
+                    sb.AppendLine($"{prefix}\tpublic {elmType.Name}[] {name} {{ get; set; }}");
+                }
+            }
+
+            sb.AppendLine($"{prefix}}}");
         }
     }
 }
---
+++ XUnitTest.Core/Serialization/SerialHelperTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Serialization;
+using Xunit;
+
+namespace XUnitTest.Serialization
+{
+    public class SerialHelperTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var json = @"{
+    ""Debug"":  ""True"",
+    ""LogLevel"":  1234,
+    ""LogPath"":  ""xxx"",
+    ""NetworkLog"":  ""255.255.255.255:514"",
+    ""LogFileFormat"":  ""{0:yyyy_MM_dd}.log"",
+    ""TempMoney"":  56.78,
+    ""PluginPath"":  ""Plugins"",
+    ""PluginServers"": [ ""http://x.newlifex.com/"", ""http://127.0.0.1"" ],
+    ""Sys"":  {
+        ""Name"":  ""NewLife.Cube"",
+        ""Version"":  """",
+        ""DisplayName"":  ""魔方平台"",
+        ""Company"":  ""新生命开发团队"",
+        ""Develop"":  ""True"",
+        ""Enable"":  ""True"",
+        ""InstallTime"":  ""2019-12-30 21:05:09"",
+        ""xxx"": {
+            ""yyy"": ""zzz""
+        }
+    }
+}";
+            var dic = JsonParser.Decode(json);
+
+            var cls = dic.BuildModelClass("MyModel");
+
+            var model = @"public class MyModel
+{
+	public String Debug { get; set; }
+
+	public Int32 LogLevel { get; set; }
+
+	public String LogPath { get; set; }
+
+	public String NetworkLog { get; set; }
+
+	public String LogFileFormat { get; set; }
+
+	public Double TempMoney { get; set; }
+
+	public String PluginPath { get; set; }
+
+	public String[] PluginServers { get; set; }
+
+	public SysModel Sys { get; set; }
+
+	public class SysModel
+	{
+		public String Name { get; set; }
+
+		public String Version { get; set; }
+
+		public String DisplayName { get; set; }
+
+		public String Company { get; set; }
+
+		public String Develop { get; set; }
+
+		public String Enable { get; set; }
+
+		public String InstallTime { get; set; }
+
+		public XxxModel Xxx { get; set; }
+
+		public class XxxModel
+		{
+			public String Yyy { get; set; }
+		}
+	}
+}
+";
+            Assert.Equal(model, cls);
+        }
+    }
+}
