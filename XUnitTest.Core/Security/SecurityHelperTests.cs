3d772be9bc2bed0eac690d9b0bba7d893f9ede04
智能石头 <nnhy@vip.qq.com>
2021-01-19 09:54:12 +0800

    DES单元测试

---
+++ XUnitTest.Core/Security/SecurityHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using NewLife;
+
+namespace XUnitTest.Security
+{
+    public class SecurityHelperTests
+    {
+        [Fact]
+        public void DES_Test()
+        {
+            var text = "111111";
+            var key = "16621235";
+
+            var des = DES.Create();
+            var text2 = des.Encrypt(text.GetBytes(), key.GetBytes(), CipherMode.ECB).ToBase64();
+            Assert.Equal("kgAdQRZ6w20=", text2);
+
+            var des2 = DES.Create();
+            var text3 = des2.Decrypt(text2.ToBase64(), key.GetBytes(), CipherMode.ECB).ToStr();
+            Assert.Equal(text, text3);
+        }
+    }
+}
