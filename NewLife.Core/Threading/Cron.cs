d0a7f23be5a0bebbe6657faf066428d815c502ad
智能石头 <nnhy@vip.qq.com>
2021-02-01 22:37:05 +0800

    Cron表达式

---
+++ NewLife.Core/Threading/Cron.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewLife.Threading
+{
+    /// <summary>Cron表达式</summary>
+    public class Cron
+    {
+        #region 属性
+        /// <summary>秒数集合</summary>
+        public List<Int32> Seconds;
+
+        /// <summary>分钟集合</summary>
+        public List<Int32> Minutes;
+
+        /// <summary>小时集合</summary>
+        public List<Int32> Hours;
+
+        /// <summary>日期集合</summary>
+        public List<Int32> DaysOfMonth;
+
+        /// <summary>月份集合</summary>
+        public List<Int32> Months;
+
+        /// <summary>星期集合</summary>
+        public List<Int32> DaysOfWeek;
+        #endregion
+
+        #region 构造
+        /// <summary>实例化Cron表达式</summary>
+        public Cron() { }
+
+        /// <summary>实例化Cron表达式</summary>
+        /// <param name="expressions"></param>
+        public Cron(String expressions) => Parse(expressions);
+        #endregion
+
+        #region 方法
+        /// <summary>指定时间是否位于表达式之内</summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public Boolean IsTime(DateTime time)
+        {
+            return Seconds.Contains(time.Second) &&
+                   Minutes.Contains(time.Minute) &&
+                   Hours.Contains(time.Hour) &&
+                   DaysOfMonth.Contains(time.Day) &&
+                   Months.Contains(time.Month) &&
+                   DaysOfWeek.Contains((Int32)time.DayOfWeek);
+        }
+
+        /// <summary>分析表达式</summary>
+        /// <param name="expressions"></param>
+        /// <returns></returns>
+        public Boolean Parse(String expressions)
+        {
+            var ss = expressions.Split(" ");
+            if (ss.Length == 0) return false;
+
+            Seconds = BuildValues(ss[0], 0, 60);
+
+            if (ss.Length > 1)
+                Minutes = BuildValues(ss[1], 0, 60);
+            else
+                Minutes = BuildValues("*", 0, 60);
+
+            if (ss.Length > 2)
+                Hours = BuildValues(ss[2], 0, 24);
+            else
+                Hours = BuildValues("*", 0, 24);
+
+            if (ss.Length > 3)
+                DaysOfMonth = BuildValues(ss[3], 1, 32);
+            else
+                DaysOfMonth = BuildValues("*", 1, 32);
+
+            if (ss.Length > 4)
+                Months = BuildValues(ss[4], 1, 13);
+            else
+                Months = BuildValues("*", 1, 13);
+
+            if (ss.Length > 5)
+                DaysOfWeek = BuildValues(ss[5], 0, 7);
+            else
+                DaysOfWeek = BuildValues("*", 0, 7);
+
+            return true;
+        }
+
+        private List<Int32> BuildValues(String value, Int32 start, Int32 max)
+        {
+            if (value == "*") return Enumerable.Range(start, max - start).ToList();
+            if (value.StartsWith("*/"))
+            {
+                var divisor = value.Substring(2).ToInt();
+                return Enumerable.Range(start, max - start).Where(e => e % divisor == 0).ToList();
+            }
+            if (value.Contains(',')) return value.SplitAsInt(",").ToList();
+
+            var ss = value.SplitAsInt("-", "/");
+            if (ss.Length >= 2)
+            {
+                start = ss[0];
+                max = ss[1];
+
+                if (ss.Length > 2)
+                    return Enumerable.Range(start, max - start).Where(e => e % ss[2] == 0).ToList();
+                else
+                    return Enumerable.Range(start, max - start).ToList();
+            }
+
+            return new List<Int32>();
+        }
+        #endregion
+    }
+}
---
+++ XUnitTest.Core/Threading/CronTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Threading;
+using Xunit;
+
+namespace XUnitTest.Threading
+{
+    public class CronTests
+    {
+        [Theory]
+        [InlineData("*/2")]
+        [InlineData("* * * * *")]
+        [InlineData("0 * * * *")]
+        [InlineData("0,1,2 * * * *")]
+        [InlineData("*/2 * * * *")]
+        [InlineData("1-4 * * * *")]
+        [InlineData("1-55/3 * * * *")]
+        [InlineData("1,10,20 * * * *")]
+        [InlineData("* 1,10,20 * * *")]
+        public void Valid(String expression)
+        {
+            var cron = new Cron();
+            Assert.True(cron.Parse(expression));
+        }
+    }
+}
