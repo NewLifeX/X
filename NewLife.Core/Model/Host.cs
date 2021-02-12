f2970ce159058f359190b93b8b8f0f2c98a68a3e
大石头 <nnhy@vip.qq.com>
2021-02-12 12:00:52 +0800

    新增BackgroundService

--- NewLife.Core/Model/Host.cs
+++ NewLife.Core/Model/Host.cs
@@ -140,4 +140,58 @@ namespace NewLife.Model
         }
         #endregion
     }
+
+#if !NET4
+    /// <summary>后台任务</summary>
+    public abstract class BackgroundService : IHostedService, IDisposable
+    {
+        private Task _executingTask;
+
+        private CancellationTokenSource _stoppingCts;
+
+        /// <summary>执行</summary>
+        /// <param name="stoppingToken"></param>
+        /// <returns></returns>
+        protected abstract Task ExecuteAsync(CancellationToken stoppingToken);
+
+        /// <summary>开始</summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public virtual Task StartAsync(CancellationToken cancellationToken)
+        {
+            _stoppingCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            _executingTask = ExecuteAsync(_stoppingCts.Token);
+            if (_executingTask.IsCompleted)
+            {
+                return _executingTask;
+            }
+#if NET45
+            return Task.FromResult(0);
+#else
+            return Task.CompletedTask;
+#endif
+        }
+
+        /// <summary>停止</summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public virtual async Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (_executingTask != null)
+            {
+                try
+                {
+                    _stoppingCts.Cancel();
+                }
+                finally
+                {
+                    await Task.WhenAny(_executingTask, Task.Delay(-1, cancellationToken)).ConfigureAwait(continueOnCapturedContext: false);
+                }
+            }
+        }
+
+        /// <summary>销毁</summary>
+        public virtual void Dispose() => _stoppingCts?.Cancel();
+    }
+#endif
 }
