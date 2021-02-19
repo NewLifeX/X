61db26aa7c4e6414ff0ec9774cf7dc7d2a5d32e1
智能石头 <nnhy@vip.qq.com>
2021-02-19 14:24:57 +0800

    代码整理

--- NewLife.Core/Yun/Driving.cs
+++ NewLife.Core/Yun/Driving.cs
@@ -5,7 +5,7 @@ namespace NewLife.Yun
     /// <summary>驾车距离和时间</summary>
     public class Driving
     {
-        /// <summary>距离。单位千米</summary>
+        /// <summary>距离。单位米</summary>
         public Int32 Distance { get; set; }
 
         /// <summary>路线耗时。单位秒</summary>
