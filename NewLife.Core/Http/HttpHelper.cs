3fdf6369ca2bdc0ccb107fac29bb346e953d3dc8
智能石头 <nnhy@vip.qq.com>
2021-03-11 12:46:46 +0800

    修正Http下载文件的bug

--- NewLife.Core/Http/HttpHelper.cs
+++ NewLife.Core/Http/HttpHelper.cs
@@ -330,10 +330,10 @@ namespace NewLife.Http
             using var fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
 #if NET4
             rs.CopyTo(fs);
-            rs.Flush();
+            //fs.Flush();
 #else
             await rs.CopyToAsync(fs);
-            await rs.FlushAsync();
+            //await fs.FlushAsync();
 #endif
         }
         #endregion
