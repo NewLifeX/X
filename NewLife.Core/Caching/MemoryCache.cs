7ac5af5586a6454d8160a6b6b67c5c3808c113bf
大石头 <nnhy@vip.qq.com>
2021-04-15 00:26:57 +0800

    MemoryQueue支持异步阻塞等待

--- NewLife.Core/Caching/MemoryCache.cs
+++ NewLife.Core/Caching/MemoryCache.cs
@@ -745,30 +745,48 @@ namespace NewLife.Caching
 
     /// <summary>生产者消费者</summary>
     /// <typeparam name="T"></typeparam>
-    public class MemoryQueue<T> : IProducerConsumer<T>
+    public class MemoryQueue<T> : DisposeBase, IProducerConsumer<T>
     {
-        private readonly IProducerConsumerCollection<T> _Collection;
+        private readonly IProducerConsumerCollection<T> _collection;
+        private SemaphoreSlim _occupiedNodes;
 
         /// <summary>实例化内存队列</summary>
-        public MemoryQueue() => _Collection = new ConcurrentQueue<T>();
+        public MemoryQueue()
+        {
+            _collection = new ConcurrentQueue<T>();
+            _occupiedNodes = new SemaphoreSlim(0);
+        }
 
         /// <summary>实例化内存队列</summary>
         /// <param name="collection"></param>
-        public MemoryQueue(IProducerConsumerCollection<T> collection) => _Collection = collection;
+        public MemoryQueue(IProducerConsumerCollection<T> collection)
+        {
+            _collection = collection;
+            _occupiedNodes = new SemaphoreSlim(collection.Count);
+        }
 
         /// <summary>元素个数</summary>
-        public Int32 Count => _Collection.Count;
+        public Int32 Count => _collection.Count;
 
         /// <summary>集合是否为空</summary>
         public Boolean IsEmpty
         {
             get
             {
-                if (_Collection is ConcurrentQueue<T> queue) return queue.IsEmpty;
-                if (_Collection is ConcurrentStack<T> stack) return stack.IsEmpty;
+                if (_collection is ConcurrentQueue<T> queue) return queue.IsEmpty;
+                if (_collection is ConcurrentStack<T> stack) return stack.IsEmpty;
 
                 throw new NotSupportedException();
             }
+        }
+
+        /// <summary>销毁</summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(Boolean disposing)
+        {
+            base.Dispose(disposing);
+
+            _occupiedNodes.TryDispose();
         }
 
         /// <summary>生产添加</summary>
@@ -779,7 +797,11 @@ namespace NewLife.Caching
             var count = 0;
             foreach (var item in values)
             {
-                if (_Collection.TryAdd(item)) count++;
+                if (_collection.TryAdd(item))
+                {
+                    count++;
+                    _occupiedNodes.Release();
+                }
             }
 
             return count;
@@ -794,7 +816,8 @@ namespace NewLife.Caching
 
             for (var i = 0; i < count; i++)
             {
-                if (!_Collection.TryTake(out var item)) break;
+                if (!_occupiedNodes.Wait(0)) break;
+                if (!_collection.TryTake(out var item)) break;
 
                 yield return item;
             }
@@ -803,12 +826,62 @@ namespace NewLife.Caching
         /// <summary>消费一个</summary>
         /// <param name="timeout">超时。默认0秒，永久等待</param>
         /// <returns></returns>
-        public T TakeOne(Int32 timeout = 0) => _Collection.TryTake(out var item) ? item : default;
+        public T TakeOne(Int32 timeout = 0)
+        {
+            if (!_occupiedNodes.Wait(0))
+            {
+                if (timeout <= 0 || !_occupiedNodes.Wait(timeout * 1000)) return default;
+            }
 
-        /// <summary>消费获取</summary>
+            return _collection.TryTake(out var item) ? item : default;
+        }
+
+#if NET4
+        /// <summary>消费获取，同步阻塞</summary>
         /// <param name="timeout">超时。默认0秒，永久等待</param>
         /// <returns></returns>
-        public Task<T> TakeOneAsync(Int32 timeout = 0) => TaskEx.FromResult(TakeOne(timeout));
+        public Task<T> TakeOneAsync(Int32 timeout = 0)
+        {
+            if (!_occupiedNodes.Wait(0))
+            {
+                if (timeout <= 0 || !_occupiedNodes.Wait(timeout * 1000)) return default;
+            }
+
+            var rs = _collection.TryTake(out var item) ? item : default;
+            return TaskEx.FromResult(rs);
+        }
+#else
+        /// <summary>消费获取，异步阻塞</summary>
+        /// <param name="timeout">超时。默认0秒，永久等待</param>
+        /// <returns></returns>
+        public async Task<T> TakeOneAsync(Int32 timeout = 0)
+        {
+            if (!_occupiedNodes.Wait(0))
+            {
+                if (timeout <= 0) return default;
+
+                if (!await _occupiedNodes.WaitAsync(timeout * 1000)) return default;
+            }
+
+            return _collection.TryTake(out var item) ? item : default;
+        }
+
+        /// <summary>消费获取，异步阻塞</summary>
+        /// <param name="timeout">超时。默认0秒，永久等待</param>
+        /// <param name="cancellationToken">取消令牌</param>
+        /// <returns></returns>
+        public async Task<T> TakeOneAsync(Int32 timeout, CancellationToken cancellationToken)
+        {
+            if (!_occupiedNodes.Wait(0))
+            {
+                if (timeout <= 0) return default;
+
+                if (!await _occupiedNodes.WaitAsync(timeout * 1000, cancellationToken)) return default;
+            }
+
+            return _collection.TryTake(out var item) ? item : default;
+        }
+#endif
 
         /// <summary>确认消费</summary>
         /// <param name="keys"></param>
---
+++ XUnitTest.Core/Caching/MemoryQueueTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NewLife.Caching;
+using NewLife.Threading;
+using Xunit;
+
+namespace XUnitTest.Caching
+{
+    public class MemoryQueueTests
+    {
+        [Fact]
+        public async void Test1()
+        {
+            var q = new MemoryQueue<String>();
+
+            Assert.True(q.IsEmpty);
+            Assert.Equal(0, q.Count);
+
+            q.Add("test");
+            q.Add("newlife", "stone");
+
+            Assert.False(q.IsEmpty);
+            Assert.Equal(3, q.Count);
+
+            var s1 = q.TakeOne();
+            Assert.Equal("test", s1);
+
+            var ss = q.Take(3).ToArray();
+            Assert.Equal(2, ss.Length);
+
+            ThreadPoolX.QueueUserWorkItem(() =>
+            {
+                Thread.Sleep(1100);
+                q.Add("delay");
+            });
+
+            var s2 = await q.TakeOneAsync(1500);
+            Assert.Equal("delay", s2);
+        }
+    }
+}
