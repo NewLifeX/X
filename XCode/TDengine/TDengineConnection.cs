7f5429d81428659fefcfa5139d7ecd97df080f47
Stone <nnhy@vip.qq.com>
2021-06-09 17:07:24 +0800

    新增TDengine驱动，参考SQLite驱动建立架构

---
+++ XCode/TDengine/TDengineConnection.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Runtime.InteropServices;
+using NewLife;
+using TDengineDriver;
+using XCode.DataAccessLayer;
+using TD = TDengineDriver.TDengine;
+
+namespace XCode.TDengine
+{
+    /// <summary>数据库连接</summary>
+    public partial class TDengineConnection : DbConnection
+    {
+        #region 属性
+        internal IntPtr _handler;
+        internal Int32 _transactionLevel;
+
+        /// <summary>连接字符串</summary>
+        public override String ConnectionString { get; set; }
+
+        private readonly String _version = String.Empty;
+        /// <summary>服务器版本</summary>
+        public override String ServerVersion
+        {
+            get
+            {
+                if (_handler == IntPtr.Zero) throw new XCodeException("连接未打开");
+
+                //if (_version.IsNullOrEmpty())
+                //    _version = Marshal.PtrToStringAnsi(TD.GetServerInfo(_handler));
+
+                return _version;
+            }
+        }
+
+        private ConnectionState _state;
+        /// <summary>状态</summary>
+        public override ConnectionState State => _state;
+
+        /// <summary>数据提供者工厂</summary>
+        protected override DbProviderFactory DbProviderFactory => TDengineFactory.Instance;
+
+        /// <summary>事务</summary>
+        protected internal virtual TDengineTransaction Transaction { get; set; }
+
+        private readonly String _Database;
+        /// <summary>数据库</summary>
+        public override String Database => _Database;
+
+        private String _DataSource;
+        /// <summary>数据源</summary>
+        public override String DataSource => _DataSource;
+        #endregion
+
+        #region 构造
+        static TDengineConnection()
+        {
+            var configPath = "C:/TDengine/cfg";
+
+#if !(NET40 || NET45)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                configPath = "/etc/taos";
+#endif
+
+            TD.Options((Int32)TDengineInitOption.TDDB_OPTION_CONFIGDIR, configPath);
+            TD.Options((Int32)TDengineInitOption.TDDB_OPTION_SHELL_ACTIVITY_TIMER, "60");
+            TD.Init();
+
+            AppDomain.CurrentDomain.DomainUnload += (s, e) => TD.Cleanup();
+        }
+
+        /// <summary>销毁</summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(Boolean disposing)
+        {
+            if (disposing) Close();
+
+            base.Dispose(disposing);
+        }
+        #endregion
+
+        #region 核心方法
+        private void SetState(ConnectionState value)
+        {
+            var originalState = _state;
+            if (originalState != value)
+            {
+                _state = value;
+                OnStateChange(new StateChangeEventArgs(originalState, value));
+            }
+        }
+
+        /// <summary>打开连接</summary>
+        public override void Open()
+        {
+            if (State == ConnectionState.Open) return;
+
+            var connStr = ConnectionString;
+            if (connStr.IsNullOrEmpty()) throw new InvalidOperationException("未设置连接字符串");
+
+            var builder = new ConnectionStringBuilder(connStr);
+            _DataSource = builder["DataSource"];
+
+            _handler = TD.Connect(_DataSource, builder["Username"], builder["Password"], "", (Int16)builder["Port"].ToInt());
+            if (_handler == IntPtr.Zero) throw new XCodeException("打开数据库连接失败");
+
+            SetState(ConnectionState.Open);
+
+            ChangeDatabase(builder["Database"]);
+        }
+
+        /// <summary>关闭连接</summary>
+        public override void Close()
+        {
+            if (State != ConnectionState.Closed) TD.Close(_handler);
+
+            Transaction?.Dispose();
+
+            SetState(ConnectionState.Closed);
+        }
+
+        /// <summary>执行</summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public Int32 Execute(String sql)
+        {
+            using var cmd = CreateCommand();
+            cmd.CommandText = sql;
+            return cmd.ExecuteNonQuery();
+        }
+        #endregion
+
+        #region 辅助方法
+        /// <summary>创建命令</summary>
+        /// <returns></returns>
+        protected override DbCommand CreateDbCommand() => new TDengineCommand { Connection = this, Transaction = Transaction };
+
+        /// <summary>开始事务</summary>
+        /// <param name="isolationLevel"></param>
+        /// <returns></returns>
+        protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel) => Transaction = new TDengineTransaction(this, isolationLevel);
+
+        /// <summary>改变数据库</summary>
+        /// <param name="databaseName"></param>
+        public override void ChangeDatabase(String databaseName)
+        {
+            if (_Database.IsNullOrEmpty() || _Database != databaseName)
+            {
+                //Int32 result = TD.SelectDatabase(_handler, databaseName);
+                //if (result == 0) _Database = databaseName;
+            }
+        }
+        #endregion
+    }
+}
---
+++ XCode/TDengine/TDengineFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+
+namespace XCode.TDengine
+{
+    /// <summary>TDengine工厂</summary>
+    /// <remarks>
+    /// 参考SQLite驱动建立架构
+    /// </remarks>
+    public class TDengineFactory : DbProviderFactory
+    {
+        private TDengineFactory() { }
+
+        /// <summary>实例</summary>
+        public static readonly TDengineFactory Instance = new TDengineFactory();
+
+        /// <summary>创建命令</summary>
+        /// <returns></returns>
+        public override DbCommand CreateCommand() => new TDengineCommand();
+
+        /// <summary>创建连接</summary>
+        /// <returns></returns>
+        public override DbConnection CreateConnection() => new TDengineConnection();
+
+        /// <summary>创建连接字符串生成器</summary>
+        /// <returns></returns>
+        public override DbConnectionStringBuilder CreateConnectionStringBuilder() => new DbConnectionStringBuilder();
+
+        /// <summary>创建参数</summary>
+        /// <returns></returns>
+        public override DbParameter CreateParameter() => new TDengineParameter();
+    }
+}
