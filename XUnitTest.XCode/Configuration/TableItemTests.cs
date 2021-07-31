66869a808739f9fe6d7ccc91a02fd2f929e83a60
大石头 <nnhy@vip.qq.com>
2021-07-31 10:55:19 +0800

    XCode支持重复索引检测。实体类[XCode.Membership.Log2]/数据表[Log2]的索引重复，可去除(Action,Category)，保留(Action,Category,ID)

---
+++ XUnitTest.XCode/Configuration/TableItemTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewLife.Log;
+using XCode.Configuration;
+using XCode.Membership;
+using Xunit;
+
+namespace XUnitTest.XCode.Configuration
+{
+    public class TableItemTests
+    {
+        [Fact]
+        public void TrimIndex()
+        {
+            var ti = TableItem.Create(typeof(Log2));
+            XTrace.WriteLine(ti.TableName);
+            Assert.Equal(5, ti.DataTable.Indexes.Count);
+        }
+    }
+}
