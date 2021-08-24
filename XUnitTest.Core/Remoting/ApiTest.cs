c01156e1ddc16a10cc7ce991b3f005244035d61a
智能大石头 <nnhy@vip.qq.com>
2021-08-24 12:40:02 +0800

    tcp

--- XUnitTest.Core/Remoting/ApiTest.cs
+++ XUnitTest.Core/Remoting/ApiTest.cs
@@ -137,7 +137,7 @@ namespace XUnitTest.Remoting
             server.Register<BigController>();
             server.Start();
 
-            using var client = new ApiClient("http://127.0.0.1:12399");
+            using var client = new ApiClient("tcp://127.0.0.1:12399");
 
             var buf = new Byte[5 * 8 * 1024];
             var rs = await client.InvokeAsync<Packet>("big/test", buf);
