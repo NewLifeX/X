c2cca85fadd7e46e4ee669b698950a5e9a9ca1be
智能大石头 <nnhy@vip.qq.com>
2022-01-14 22:18:56 +0800

    恢复数据和同步数据时，支持忽略单页错误，继续处理下一页

--- XCode/DataAccessLayer/DbPackage.cs
+++ XCode/DataAccessLayer/DbPackage.cs
@@ -33,6 +33,9 @@ namespace XCode.DataAccessLayer
         /// <summary>批量处理时，忽略单表错误，继续处理下一个。默认true</summary>
         public Boolean IgnoreError { get; set; } = true;
 
+        /// <summary>批量处理时，忽略单页错误，继续处理下一个。默认false</summary>
+        public Boolean IgnorePageError { get; set; }
+
         /// <summary>
         /// 性能追踪器
         /// </summary>
@@ -267,6 +270,7 @@ namespace XCode.DataAccessLayer
             {
                 Table = table,
                 Dal = Dal,
+                IgnorePageError = IgnorePageError,
                 Log = Log,
                 Tracer = Tracer,
 
@@ -459,6 +463,7 @@ namespace XCode.DataAccessLayer
             {
                 Table = table,
                 Dal = dal,
+                IgnorePageError = IgnorePageError,
                 Log = Log,
                 Tracer = Tracer,
 
@@ -649,6 +654,9 @@ namespace XCode.DataAccessLayer
             /// </summary>
             public IDataTable Table { get; set; }
 
+            /// <summary>批量处理时，忽略单页错误，继续处理下一个。默认false</summary>
+            public Boolean IgnorePageError { get; set; }
+
             /// <summary>
             /// 日志
             /// </summary>
@@ -681,7 +689,21 @@ namespace XCode.DataAccessLayer
 
                 // 批量插入
                 using var span = Tracer?.NewSpan($"db:{Dal.ConnName}:BatchInsert:{Table.TableName}");
-                Dal.Session.Insert(Table, _Columns, dt.Cast<IExtend>());
+                if (IgnorePageError)
+                {
+                    try
+                    {
+                        Dal.Session.Insert(Table, _Columns, dt.Cast<IExtend>());
+                    }
+                    catch (Exception ex)
+                    {
+                        span?.SetError(ex, dt.Rows?.Count);
+                    }
+                }
+                else
+                {
+                    Dal.Session.Insert(Table, _Columns, dt.Cast<IExtend>());
+                }
 
                 return null;
             }
