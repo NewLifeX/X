207ae46f465f75e11b370d0257e4e3acfc5f4f5f
大石头 <nnhy@vip.qq.com>
2022-01-16 18:18:02 +0800

    2022.0116-beta1

--- XUnitTest.XCode/EntityTests/SqlTests.cs
+++ XUnitTest.XCode/EntityTests/SqlTests.cs
@@ -158,38 +158,38 @@ namespace XUnitTest.XCode.EntityTests
             Assert.Equal(@"Select * From `user` Where name='Stone' And display_name='大石头' And logins>0 And register_time<'2020-09-01 00:00:00' Order By update_user_id Desc", sql);
         }
 
-        [Fact]
-        public void SplitTestSQLite()
-        {
-            User2.Meta.ShardTableName = e => $"User_{e.RegisterTime:yyyyMM}";
+        //[Fact]
+        //public void SplitTestSQLite()
+        //{
+        //    User2.Meta.ShardTableName = e => $"User_{e.RegisterTime:yyyyMM}";
 
-            var user = new User2
-            {
-                Name = "Stone",
-                DisplayName = "大石头",
-                Enable = true,
+        //    var user = new User2
+        //    {
+        //        Name = "Stone",
+        //        DisplayName = "大石头",
+        //        Enable = true,
 
-                RegisterTime = new DateTime(2020, 8, 22),
-                UpdateTime = new DateTime(2020, 9, 1),
-            };
-            User2.Meta.CreateShard(user);
+        //        RegisterTime = new DateTime(2020, 8, 22),
+        //        UpdateTime = new DateTime(2020, 9, 1),
+        //    };
+        //    User2.Meta.CreateShard(user);
 
-            var factory = User2.Meta.Factory;
-            var session = User2.Meta.Session;
+        //    var factory = User2.Meta.Factory;
+        //    var session = User2.Meta.Session;
 
-            var sql = factory.Persistence.GetSql(session, user, DataObjectMethodType.Insert);
-            Assert.Equal(@"Insert Into User_202008(Name,Password,DisplayName,Sex,Mail,Mobile,Code,Avatar,RoleID,RoleIds,DepartmentID,Online,Enable,Logins,LastLogin,LastLoginIP,RegisterTime,RegisterIP,Ex1,Ex2,Ex3,Ex4,Ex5,Ex6,UpdateUser,UpdateUserID,UpdateIP,UpdateTime,Remark) Values('Stone',null,'大石头',0,null,null,null,null,0,null,0,0,1,0,null,null,'2020-08-22 00:00:00',null,0,0,0,null,null,null,null,0,null,'2020-09-01 00:00:00',null)", sql);
+        //    var sql = factory.Persistence.GetSql(session, user, DataObjectMethodType.Insert);
+        //    Assert.Equal(@"Insert Into User_202008(Name,Password,DisplayName,Sex,Mail,Mobile,Code,Avatar,RoleID,RoleIds,DepartmentID,Online,Enable,Logins,LastLogin,LastLoginIP,RegisterTime,RegisterIP,Ex1,Ex2,Ex3,Ex4,Ex5,Ex6,UpdateUser,UpdateUserID,UpdateIP,UpdateTime,Remark) Values('Stone',null,'大石头',0,null,null,null,null,0,null,0,0,1,0,null,null,'2020-08-22 00:00:00',null,0,0,0,null,null,null,null,0,null,'2020-09-01 00:00:00',null)", sql);
 
-            user.ID = 2;
-            sql = factory.Persistence.GetSql(session, user, DataObjectMethodType.Update);
-            Assert.Equal(@"Update User_202008 Set Name='Stone',DisplayName='大石头',Enable=1,RegisterTime='2020-08-22 00:00:00',UpdateTime='2020-09-01 00:00:00' Where ID=2", sql);
+        //    user.ID = 2;
+        //    sql = factory.Persistence.GetSql(session, user, DataObjectMethodType.Update);
+        //    Assert.Equal(@"Update User_202008 Set Name='Stone',DisplayName='大石头',Enable=1,RegisterTime='2020-08-22 00:00:00',UpdateTime='2020-09-01 00:00:00' Where ID=2", sql);
 
-            sql = factory.Persistence.GetSql(session, user, DataObjectMethodType.Delete);
-            Assert.Equal(@"Delete From User_202008 Where ID=2", sql);
+        //    sql = factory.Persistence.GetSql(session, user, DataObjectMethodType.Delete);
+        //    Assert.Equal(@"Delete From User_202008 Where ID=2", sql);
 
-            // 恢复现场，避免影响其它测试用例
-            User2.Meta.ShardTableName = null;
-        }
+        //    // 恢复现场，避免影响其它测试用例
+        //    User2.Meta.ShardTableName = null;
+        //}
 
         [Fact]
         public void ShardTestSQLite()
