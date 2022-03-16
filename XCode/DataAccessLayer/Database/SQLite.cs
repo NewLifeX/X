d9577ed562a406f47017040d7335fca51727aaa2
大石头 <nnhy@vip.qq.com>
2022-03-16 08:26:12 +0800

    生成批量插入语句时检查字段匹配，DbTable中不存在名为[{dc.Name}/{dc.ColumnName}]的字段

--- XCode/DataAccessLayer/Database/SQLite.cs
+++ XCode/DataAccessLayer/Database/SQLite.cs
@@ -370,68 +370,12 @@ namespace XCode.DataAccessLayer
             var db = Database as DbBase;
 
             // 字段列表
-            //if (columns == null) columns = table.Columns.ToArray();
-            sb.AppendFormat("{0} {1}(", action, db.FormatName(table));
-            foreach (var dc in columns)
-            {
-                //if (dc.Identity) continue;
-
-                sb.Append(db.FormatName(dc));
-                sb.Append(',');
-            }
-            sb.Length--;
-            sb.Append(')');
+            if (columns == null) columns = table.Columns.ToArray();
+            BuildInsert(sb, db, action, table, columns);
 
             // 值列表
             sb.Append(" Values");
-
-            // 优化支持DbTable
-            if (list.FirstOrDefault() is DbRow)
-            {
-                // 提前把列名转为索引，然后根据索引找数据。外部确保数据列在数据源中都存在
-                DbTable dt = null;
-                Int32[] ids = null;
-                foreach (DbRow dr in list)
-                {
-                    if (dr.Table != dt)
-                    {
-                        dt = dr.Table;
-                        var cs = new List<Int32>();
-                        foreach (var dc in columns)
-                        {
-                            var idx = dt.GetColumn(dc.Name);
-                            if (idx < 0) idx = dt.GetColumn(dc.ColumnName);
-                            cs.Add(idx);
-                        }
-                        ids = cs.ToArray();
-                    }
-
-                    sb.Append('(');
-                    var row = dt.Rows[dr.Index];
-                    for (var i = 0; i < columns.Length; i++)
-                    {
-                        sb.Append(db.FormatValue(columns[i], row[ids[i]]));
-                        sb.Append(',');
-                    }
-                    sb.Length--;
-                    sb.Append("),");
-                }
-            }
-            else
-            {
-                foreach (var entity in list)
-                {
-                    sb.Append('(');
-                    foreach (var dc in columns)
-                    {
-                        sb.Append(db.FormatValue(dc, entity[dc.Name]));
-                        sb.Append(',');
-                    }
-                    sb.Length--;
-                    sb.Append("),");
-                }
-            }
-            sb.Length--;
+            BuildBatchValues(sb, db, action, table, columns, list);
 
             // 重复键执行update
             if (updateColumns != null || addColumns != null)
