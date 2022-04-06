75e103922e5c147c605382b62f8b85a57893ee6d
大石头 <nnhy@vip.qq.com>
2022-04-06 15:08:30 +0800

    ToDateTime支持yyyyMMddHHmmss的解析

--- NewLife.Core/Common/Utility.cs
+++ NewLife.Core/Common/Utility.cs
@@ -432,8 +432,10 @@ namespace System
                 }
 
                 if (!DateTime.TryParse(str, out var dt) &&
-                    !str.Contains("-") && DateTime.TryParseExact(str, "yyyy-M-d", null, DateTimeStyles.None, out dt) &&
-                    !str.Contains("/") && DateTime.TryParseExact(str, "yyyy/M/d", null, DateTimeStyles.None, out dt) &&
+                    !(str.Contains('-') && DateTime.TryParseExact(str, "yyyy-M-d", null, DateTimeStyles.None, out dt)) &&
+                    !(str.Contains('/') && DateTime.TryParseExact(str, "yyyy/M/d", null, DateTimeStyles.None, out dt)) &&
+                    !DateTime.TryParseExact(str, "yyyyMMddHHmmss", null, DateTimeStyles.None, out dt) &&
+                    !DateTime.TryParseExact(str, "yyyyMMdd", null, DateTimeStyles.None, out dt) &&
                     !DateTime.TryParse(str, out dt))
                 {
                     dt = defaultValue;
@@ -477,8 +479,8 @@ namespace System
                 if (str.IsNullOrEmpty()) return defaultValue;
 
                 if (DateTimeOffset.TryParse(str, out var dt)) return dt;
-                if (str.Contains("-") && DateTimeOffset.TryParseExact(str, "yyyy-M-d", null, DateTimeStyles.None, out dt)) return dt;
-                if (str.Contains("/") && DateTimeOffset.TryParseExact(str, "yyyy/M/d", null, DateTimeStyles.None, out dt)) return dt;
+                if (str.Contains('-') && DateTimeOffset.TryParseExact(str, "yyyy-M-d", null, DateTimeStyles.None, out dt)) return dt;
+                if (str.Contains('/') && DateTimeOffset.TryParseExact(str, "yyyy/M/d", null, DateTimeStyles.None, out dt)) return dt;
 
                 return defaultValue;
             }
