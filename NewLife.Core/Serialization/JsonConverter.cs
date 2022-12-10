8158f485833e53e6de369854e9c29f9925c0dd30
大石头 <nnhy@vip.qq.com>
2022-12-11 00:13:16 +0800

    增加JsonConverter，Json反序列化时进行类型绑定。用于指定接口的实现类

---
+++ NewLife.Core/Serialization/JsonConverter.cs
@@ -0,0 +1,25 @@
+﻿#if NETCOREAPP
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NewLife.Serialization;
+
+/// <summary>Json反序列化时进行类型绑定。用于指定接口的实现类</summary>
+/// <typeparam name="TService"></typeparam>
+/// <typeparam name="TImplementation"></typeparam>
+public class JsonConverter<TService, TImplementation> : JsonConverter<TService> where TImplementation : TService
+{
+    /// <summary>读取</summary>
+    /// <param name="reader"></param>
+    /// <param name="typeToConvert"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public override TService Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => JsonSerializer.Deserialize<TImplementation>(ref reader, options);
+
+    /// <summary>写入</summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="options"></param>
+    public override void Write(Utf8JsonWriter writer, TService value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, options);
+}
+#endif
