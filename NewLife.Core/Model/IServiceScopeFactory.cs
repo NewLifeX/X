ba633359e5b22cec5d1869437a6cfb098e99e6a1
大石头 <nnhy@vip.qq.com>
2023-03-29 15:41:25 +0800

    feat: 新增支持IServiceScope，兼容标准DependencyInjection

---
+++ NewLife.Core/Model/IServiceScopeFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace NewLife.Model;
+
+/// <summary>范围服务工厂</summary>
+public interface IServiceScopeFactory
+{
+    /// <summary>创建范围服务</summary>
+    /// <returns></returns>
+    IServiceScope CreateScope();
+}
+
+class MyServiceScopeFactory : IServiceScopeFactory
+{
+    public IServiceProvider ServiceProvider { get; set; }
+
+    public IServiceScope CreateScope() => new MyServiceScope { MyServiceProvider = ServiceProvider };
+}
