977a04d23dd9b74b99edb34152f67e718234fc61
大石头 <nnhy@vip.qq.com>
2023-04-01 18:16:49 +0800

    feat: 新增SM4国密算法

---
+++ NewLife.Core/Security/CbcTransform.cs
@@ -0,0 +1,133 @@
+﻿using System.Security.Cryptography;
+
+namespace NewLife.Security;
+
+/// <summary>CBC块密码模式</summary>
+/// <remarks>
+/// 密码块链 (CBC) 模式引入了反馈。 每个纯文本块在加密前，通过按位“异或”操作与前一个块的密码文本结合。
+/// 这样确保了即使纯文本包含许多相同的块，这些块中的每一个也会加密为不同的密码文本块。
+/// 在加密块之前，初始化向量通过按位“异或”操作与第一个纯文本块结合。
+/// 如果密码文本块中有一个位出错，相应的纯文本块也将出错。
+/// 此外，后面的块中与原出错位的位置相同的位也将出错。
+/// </remarks>
+public sealed class CbcTransform : ICryptoTransform
+{
+    #region 属性
+    private readonly ICryptoTransform _transform;
+    private readonly Boolean _encryptMode;
+    private readonly Byte[] _iv;
+    private readonly Byte[] _lastBlock;
+
+    /// <summary>获取一个值，该值指示是否可以转换多个块。</summary>
+    public Boolean CanTransformMultipleBlocks => true;
+
+    /// <summary>获取一个值，该值指示是否可重复使用当前转换。</summary>
+    public Boolean CanReuseTransform => _transform.CanReuseTransform;
+
+    /// <summary>获取输入块大小。</summary>
+    public Int32 InputBlockSize => _transform.InputBlockSize;
+
+    /// <summary>获取输出块大小。</summary>
+    public Int32 OutputBlockSize => _transform.OutputBlockSize;
+    #endregion
+
+    #region 构造
+    /// <summary>实例化</summary>
+    /// <param name="transform"></param>
+    /// <param name="iv"></param>
+    /// <param name="encryptMode"></param>
+    /// <exception cref="CryptographicException"></exception>
+    public CbcTransform(ICryptoTransform transform, Byte[] iv, Boolean encryptMode)
+    {
+        _transform = transform;
+        _encryptMode = encryptMode;
+        _lastBlock = new Byte[transform.InputBlockSize];
+
+        if (transform.InputBlockSize != transform.OutputBlockSize) throw new CryptographicException();
+
+        if (iv.Length != transform.InputBlockSize) throw new CryptographicException("IV length mismatch");
+
+        Array.Copy(iv, _lastBlock, transform.InputBlockSize);
+        _iv = (Byte[])_lastBlock.Clone();
+    }
+
+    /// <summary>销毁</summary>
+    public void Dispose() => _transform.Dispose();
+    #endregion
+
+    /// <summary>转换输入字节数组的指定区域，并将所得到的转换复制到输出字节数组的指定区域。</summary>
+    /// <param name="inputBuffer"></param>
+    /// <param name="inputOffset"></param>
+    /// <param name="inputCount"></param>
+    /// <param name="outputBuffer"></param>
+    /// <param name="outputOffset"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public Int32 TransformBlock(Byte[] inputBuffer, Int32 inputOffset, Int32 inputCount, Byte[] outputBuffer, Int32 outputOffset)
+    {
+        if (inputCount % InputBlockSize != 0)
+            throw new ArgumentOutOfRangeException(nameof(inputCount));
+
+        var blocks = inputCount / InputBlockSize;
+        while (blocks > 0)
+        {
+            TransformOneBlock(inputBuffer, inputOffset, outputBuffer, outputOffset, false);
+
+            blocks -= 1;
+            inputOffset += InputBlockSize;
+            outputOffset += OutputBlockSize;
+        }
+
+        return inputCount;
+    }
+
+    private void TransformOneBlock(Byte[] inputBuffer, Int32 inputOffset, Byte[] outputBuffer, Int32 outputOffset, Boolean signalFinalBlock)
+    {
+        var imm = new Byte[InputBlockSize];
+        Array.Copy(inputBuffer, inputOffset, imm, 0, InputBlockSize);
+        if (_encryptMode)
+            for (var i = 0; i < InputBlockSize; i++)
+                imm[i] ^= _lastBlock[i];
+
+        if (signalFinalBlock)
+        {
+            var lastBlock = _transform.TransformFinalBlock(imm, 0, InputBlockSize);
+            Array.Copy(lastBlock, 0, outputBuffer, outputOffset, InputBlockSize);
+        }
+        else
+            _transform.TransformBlock(imm, 0, InputBlockSize, outputBuffer, outputOffset);
+
+        if (!_encryptMode)
+        {
+            for (var i = 0; i < InputBlockSize; i++)
+                outputBuffer[outputOffset + i] ^= _lastBlock[i];
+            Array.Copy(imm, 0, _lastBlock, 0, InputBlockSize);
+        }
+        else
+            Array.Copy(outputBuffer, outputOffset, _lastBlock, 0, InputBlockSize);
+    }
+
+    /// <summary>转换指定字节数组的指定区域。</summary>
+    /// <param name="inputBuffer"></param>
+    /// <param name="inputOffset"></param>
+    /// <param name="inputCount"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public Byte[] TransformFinalBlock(Byte[] inputBuffer, Int32 inputOffset, Int32 inputCount)
+    {
+        if (inputCount == 0) return new Byte[0];
+
+        var blocks = inputCount / InputBlockSize;
+        var output = new Byte[blocks * OutputBlockSize];
+        if (blocks > 1)
+            TransformBlock(inputBuffer, inputOffset, inputCount - InputBlockSize, output, 0);
+
+        if (blocks >= 1)
+            TransformOneBlock(inputBuffer, inputOffset + inputCount - InputBlockSize, output, output.Length - InputBlockSize, true);
+        else
+            output = _transform.TransformFinalBlock(inputBuffer, inputOffset, inputCount);
+
+        Array.Copy(_iv, _lastBlock, InputBlockSize);
+        return output;
+    }
+}
---
+++ XUnitTest.Core/Security/SM4Tests.cs
@@ -0,0 +1,142 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using NewLife;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto;
+using Xunit;
+using System;
+
+namespace XUnitTest.Security;
+
+public class SM4Tests
+{
+    [Fact]
+    public void SM4_Test()
+    {
+        var text = "111111";
+        var key = "16621235";
+
+        var sm4 = SM4.Create();
+        var text2 = sm4.Encrypt(text.GetBytes(), key.GetBytes(), CipherMode.ECB).ToBase64();
+        Assert.Equal("kgAdQRZ6w20=", text2);
+
+        var des2 = SM4.Create();
+        var text3 = des2.Decrypt(text2.ToBase64(), key.GetBytes(), CipherMode.ECB).ToStr();
+        Assert.Equal(text, text3);
+    }
+
+    [Fact]
+    public void SM4_Test2()
+    {
+        var text = "快乐编码 , 生活愉快!";
+        var key = "5m28850d763e8748ff2f8d83530e0cf2";
+
+        var sm4 = SM4.Create();
+        var text2 = sm4.Encrypt(text.GetBytes(), key.GetBytes(), CipherMode.ECB).ToHex();
+        Assert.Equal("738bc5aebe06f16ba95aec7c2e6cde104d498400829d9ea74492fcf15ec98c94", text2);
+
+        var des2 = SM4.Create();
+        var text3 = des2.Decrypt(text2.ToBase64(), key.GetBytes(), CipherMode.ECB).ToStr();
+        Assert.Equal(text, text3);
+    }
+
+    [Fact]
+    public void SM4Transform_Test()
+    {
+        var plain = "0123456789abcdeffedcba9876543210".ToHex();
+        var key = "0123456789abcdeffedcba9876543210".ToHex();
+        var cipher = "595298c7c6fd271f0402f804c33d3f66".ToHex();
+        var buf = new Byte[16];
+
+        IBlockCipher engine = new SM4Engine();
+
+        engine.Init(true, new KeyParameter(key));
+
+        Array.Copy(plain, 0, buf, 0, buf.Length);
+
+        for (var i = 0; i != 1000000; i++)
+        {
+            engine.ProcessBlock(buf, 0, buf, 0);
+        }
+
+        Assert.Equal(cipher, buf);
+
+        engine.Init(false, new KeyParameter(key));
+
+        for (var i = 0; i != 1000000; i++)
+        {
+            engine.ProcessBlock(buf, 0, buf, 0);
+        }
+
+        Assert.Equal(plain, buf);
+
+        // 底层转换方法
+        {
+            var sm4 = new SM4Transform(key, null, true);
+            buf = plain;
+            var tmp = new Byte[16];
+            for (var i = 0; i != 1000000; i++)
+            {
+                var rs = sm4.EncryptData(buf, 0, tmp, 0);
+                Assert.Equal(16, rs);
+                buf = tmp;
+            }
+            Assert.Equal(cipher, buf);
+        }
+        {
+            var sm4 = new SM4Transform(key, null, false);
+            buf = cipher;
+            var tmp = new Byte[16];
+            for (var i = 0; i != 1000000; i++)
+            {
+                var rs = sm4.EncryptData(buf, 0, tmp, 0);
+                Assert.Equal(16, rs);
+                buf = tmp;
+            }
+            Assert.Equal(plain, buf);
+        }
+
+        // 顶层加解密
+        {
+            var sm4 = SM4.Create();
+            buf = plain;
+            for (var i = 0; i != 1000000; i++)
+            {
+                buf = sm4.Encrypt(buf, key, CipherMode.ECB);
+            }
+            Assert.Equal(cipher, buf);
+        }
+        {
+            var sm4 = SM4.Create();
+            buf = cipher;
+            for (var i = 0; i != 1000000; i++)
+            {
+                buf = sm4.Decrypt(buf, key, CipherMode.ECB);
+            }
+            Assert.Equal(plain, buf);
+        }
+    }
+
+    [Fact]
+    public void SM4_CBC_Test()
+    {
+        var buf = "123456".ToHex();
+        var key = "86CAD727DEB54263B73960AA79C5D9B7".ToHex();
+        var data = "";
+
+        // CBC加密解密
+        {
+            var aes = SM4.Create();
+            data = aes.Encrypt(buf, key, CipherMode.CBC, PaddingMode.PKCS7).ToHex();
+
+            Assert.Equal("50A7CF869354EC317327671B34543AD8", data);
+        }
+        {
+            var aes = SM4.Create();
+            data = aes.Decrypt(data.ToHex(), key, CipherMode.CBC, PaddingMode.PKCS7).ToHex();
+
+            Assert.Equal("123456", data);
+        }
+    }
+}
