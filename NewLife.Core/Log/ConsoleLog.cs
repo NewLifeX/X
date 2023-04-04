0a49225cffae8cabb52726e76181c455cf68b026
大石头 <nnhy@vip.qq.com>
2023-04-04 17:50:08 +0800

    吃掉异常，避免应用崩溃

--- NewLife.Core/Log/ConsoleLog.cs
+++ NewLife.Core/Log/ConsoleLog.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Concurrent;
 
-namespace NewLife.Log
-{
-    /// <summary>控制台输出日志</summary>
-    public class ConsoleLog : Logger
-    {
-        /// <summary>是否使用多种颜色，默认使用</summary>
-        public Boolean UseColor { get; set; } = true;
+namespace NewLife.Log;
 
-        /// <summary>写日志</summary>
-        /// <param name="level"></param>
-        /// <param name="format"></param>
-        /// <param name="args"></param>
-        protected override void OnWrite(LogLevel level, String format, params Object[] args)
+/// <summary>控制台输出日志</summary>
+public class ConsoleLog : Logger
+{
+    /// <summary>是否使用多种颜色，默认使用</summary>
+    public Boolean UseColor { get; set; } = true;
+
+    /// <summary>写日志</summary>
+    /// <param name="level"></param>
+    /// <param name="format"></param>
+    /// <param name="args"></param>
+    protected override void OnWrite(LogLevel level, String format, params Object[] args)
+    {
+        // 吃掉异常，避免应用崩溃
+        try
         {
             var e = WriteLogEventArgs.Current.Set(level).Set(Format(format, args), null);
 
@@ -38,20 +41,21 @@ namespace NewLife.Log
                 Console.ForegroundColor = old;
             }
         }
-
-        static readonly ConcurrentDictionary<Int32, ConsoleColor> dic = new();
-        static readonly ConsoleColor[] colors = new ConsoleColor[] {
-            ConsoleColor.Green, ConsoleColor.Cyan, ConsoleColor.Magenta, ConsoleColor.White, ConsoleColor.Yellow,
-            ConsoleColor.DarkGreen, ConsoleColor.DarkCyan, ConsoleColor.DarkMagenta, ConsoleColor.DarkRed, ConsoleColor.DarkYellow };
-        private static ConsoleColor GetColor(Int32 threadid)
-        {
-            if (threadid == 1) return ConsoleColor.Gray;
-
-            return dic.GetOrAdd(threadid, k => colors[k % colors.Length]);
-        }
-
-        /// <summary>已重载。</summary>
-        /// <returns></returns>
-        public override String ToString() => $"{GetType().Name} UseColor={UseColor}";
+        catch { }
     }
+
+    static readonly ConcurrentDictionary<Int32, ConsoleColor> dic = new();
+    static readonly ConsoleColor[] colors = new ConsoleColor[] {
+        ConsoleColor.Green, ConsoleColor.Cyan, ConsoleColor.Magenta, ConsoleColor.White, ConsoleColor.Yellow,
+        ConsoleColor.DarkGreen, ConsoleColor.DarkCyan, ConsoleColor.DarkMagenta, ConsoleColor.DarkRed, ConsoleColor.DarkYellow };
+    private static ConsoleColor GetColor(Int32 threadid)
+    {
+        if (threadid == 1) return ConsoleColor.Gray;
+
+        return dic.GetOrAdd(threadid, k => colors[k % colors.Length]);
+    }
+
+    /// <summary>已重载。</summary>
+    /// <returns></returns>
+    public override String ToString() => $"{GetType().Name} UseColor={UseColor}";
 }
