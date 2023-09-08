9681eedc1c8e2be95b6e6bbb155d10ab90c28139
大石头 <nnhy@vip.qq.com>
2023-09-09 07:16:48 +0800

    解决 args 变量没有赋值导致的参数传递失败的问题。by Soar360

--- NewLife.Core/Remoting/ApiServer.cs
+++ NewLife.Core/Remoting/ApiServer.cs
@@ -213,6 +213,7 @@ public class ApiServer : ApiHost, IServer
             {
                 message = enc.Decode(msg);
                 if (message == null) return null;
+                args = message.Data;
 
                 // 根据动作名，开始跟踪
                 span = Tracer?.NewSpan("rps:" + message.Action, args);
