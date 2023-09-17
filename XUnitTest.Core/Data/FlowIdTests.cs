c6fa9fec2b0de1e7af6902f51393d2f5cae5954d
大石头 <nnhy@vip.qq.com>
2023-09-17 13:12:09 +0800

    业务id作为workerId，保留12位序列号。即传感器按1024分组，每组每毫秒最多生成4096个Id

--- XUnitTest.Core/Data/FlowIdTests.cs
+++ XUnitTest.Core/Data/FlowIdTests.cs
@@ -8,142 +8,173 @@ using NewLife.Log;
 using NewLife.Security;
 using Xunit;
 
-namespace XUnitTest.Data
+namespace XUnitTest.Data;
+
+public class FlowIdTests
 {
-    public class FlowIdTests
+    [Fact]
+    public void NewId()
     {
-        [Fact]
-        public void NewId()
+        var f = new Snowflake();
+        var id = f.NewId();
+
+        var time = id >> 22;
+        var tt = f.StartTimestamp.AddMilliseconds(time);
+        Assert.True(tt <= DateTime.Now);
+
+        var wid = (id >> 12) & 0x3FF;
+        Assert.Equal(f.WorkerId, wid);
+
+        var seq = id & 0x0FFF;
+        //Assert.Equal(f.Sequence, seq);
+
+        // 时间转编号
+        var id2 = f.GetId(tt);
+        Assert.Equal(id >> 22, id2 >> 22);
+
+        // 分析
+        var rs = f.TryParse(id, out var t, out var w, out var s);
+        Assert.True(rs);
+        Assert.Equal(tt, t);
+        Assert.Equal(wid, w);
+        Assert.Equal(seq, s);
+    }
+
+    [Fact]
+    public void NewIdByUid()
+    {
+        var time = DateTime.Now;
+        var uid = Rand.Next(1_000_000);
+
+        XTrace.WriteLine("time: {0} uid: {1}/{1:X8}", time, uid);
+
+        var f = new Snowflake();
+        var id1 = f.NewId(time, uid);
+        XTrace.WriteLine("id1: {0}", id1);
+
+        var id2 = f.NewId(time, uid);
+        Assert.NotEqual(id1, id2);
+        XTrace.WriteLine("id2: {0}", id2);
+
+        var id3 = f.NewId(time, uid);
+        Assert.NotEqual(id1, id3);
+        Assert.NotEqual(id2, id3);
+        XTrace.WriteLine("id3: {0}", id3);
+
+        var id4 = f.NewId(time, uid);
+        Assert.NotEqual(id1, id4);
+        Assert.NotEqual(id2, id4);
+        Assert.NotEqual(id3, id4);
+        XTrace.WriteLine("id4: {0}", id4);
+
+        var id5 = f.NewId(time, uid);
+        Assert.NotEqual(id1, id5);
+        XTrace.WriteLine("id5: {0}", id5);
+    }
+
+    [Fact]
+    public void ValidRepeat()
+    {
+        var sw = Stopwatch.StartNew();
+
+        //var ws = new ConcurrentBag<Int32>();
+        var ws = new ConcurrentDictionary<Int32, Snowflake>();
+        //var repeat = new ConcurrentBag<Int64>();
+        var hash = new ConcurrentDictionary<Int64, Snowflake>();
+
+        var ts = new List<Task>();
+        var ss = new Int64[2];
+        var rs = new List<Int64>[ss.Length];
+        for (var k = 0; k < ss.Length; k++)
         {
-            var f = new Snowflake();
-            var id = f.NewId();
-
-            var time = id >> 22;
-            var tt = f.StartTimestamp.AddMilliseconds(time);
-            Assert.True(tt <= DateTime.Now);
-
-            var wid = (id >> 12) & 0x3FF;
-            Assert.Equal(f.WorkerId, wid);
-
-            var seq = id & 0x0FFF;
-            //Assert.Equal(f.Sequence, seq);
-
-            // 时间转编号
-            var id2 = f.GetId(tt);
-            Assert.Equal(id >> 22, id2 >> 22);
-
-            // 分析
-            var rs = f.TryParse(id, out var t, out var w, out var s);
-            Assert.True(rs);
-            Assert.Equal(tt, t);
-            Assert.Equal(wid, w);
-            Assert.Equal(seq, s);
-        }
-
-        [Fact]
-        public void ValidRepeat()
-        {
-            var sw = Stopwatch.StartNew();
-
-            //var ws = new ConcurrentBag<Int32>();
-            var ws = new ConcurrentDictionary<Int32, Snowflake>();
-            //var repeat = new ConcurrentBag<Int64>();
-            var hash = new ConcurrentDictionary<Int64, Snowflake>();
-
-            var ts = new List<Task>();
-            var ss = new Int64[2];
-            var rs = new List<Int64>[ss.Length];
-            for (var k = 0; k < ss.Length; k++)
+            // 提前计算workerId到本地变量，避免匿名函数闭包里面产生重复
+            var wid = (k + 1) & 0x3FF;
+            var idx = k;
+            ts.Add(Task.Run(() =>
             {
-                // 提前计算workerId到本地变量，避免匿名函数闭包里面产生重复
-                var wid = (k + 1) & 0x3FF;
-                var idx = k;
-                ts.Add(Task.Run(() =>
+                var repeat = new List<Int64>();
+                var f = new Snowflake { StartTimestamp = new DateTime(2020, 1, 1), WorkerId = wid };
+                //ws.Add(f.WorkerId);
+                Assert.True(ws.TryAdd(f.WorkerId, f));
+                //if (!ws.TryAdd(f.WorkerId, f)) Assert.True(false);
+
+                for (var i = 0; i < 100_000; i++)
                 {
-                    var repeat = new List<Int64>();
-                    var f = new Snowflake { StartTimestamp = new DateTime(2020, 1, 1), WorkerId = wid };
-                    //ws.Add(f.WorkerId);
-                    Assert.True(ws.TryAdd(f.WorkerId, f));
-                    //if (!ws.TryAdd(f.WorkerId, f)) Assert.True(false);
-
-                    for (var i = 0; i < 100_000; i++)
+                    var id = f.NewId();
+                    if (!hash.TryAdd(id, f))
                     {
-                        var id = f.NewId();
-                        if (!hash.TryAdd(id, f))
-                        {
-                            hash.TryGetValue(id, out var f2);
-                            repeat.Add(id);
-                        }
-
-                        ss[wid - 1]++;
+                        hash.TryGetValue(id, out var f2);
+                        repeat.Add(id);
                     }
-                    rs[wid - 1] = repeat;
-                }));
-            }
-            Task.WaitAll(ts.ToArray());
 
-            sw.Stop();
-
-            Assert.True(sw.ElapsedMilliseconds < 10_000);
-            //var count = repeat.Count;
-            //Assert.Equal(0, count);
-            for (var i = 0; i < ss.Length; i++)
-            {
-                Assert.Empty(rs[i]);
-            }
+                    ss[wid - 1]++;
+                }
+                rs[wid - 1] = repeat;
+            }));
         }
+        Task.WaitAll(ts.ToArray());
 
-        [Fact]
-        public void Benchmark()
+        sw.Stop();
+
+        Assert.True(sw.ElapsedMilliseconds < 10_000);
+        //var count = repeat.Count;
+        //Assert.Equal(0, count);
+        for (var i = 0; i < ss.Length; i++)
         {
-            var sw = Stopwatch.StartNew();
+            Assert.Empty(rs[i]);
+        }
+    }
 
-            var cpu = Environment.ProcessorCount * 4;
-            var count = 10_000_000L;
+    [Fact]
+    public void Benchmark()
+    {
+        var sw = Stopwatch.StartNew();
 
-            var ts = new List<Task>();
-            for (var i = 0; i < cpu; i++)
+        var cpu = Environment.ProcessorCount * 4;
+        var count = 10_000_000L;
+
+        var ts = new List<Task>();
+        for (var i = 0; i < cpu; i++)
+        {
+            ts.Add(Task.Run(() =>
             {
-                ts.Add(Task.Run(() =>
+                var f = new Snowflake();
+
+                for (var i = 0; i < count; i++)
                 {
-                    var f = new Snowflake();
-
-                    for (var i = 0; i < count; i++)
-                    {
-                        var id = f.NewId();
-                    }
-                }));
-            }
-
-            Task.WaitAll(ts.ToArray());
-
-            sw.Stop();
-
-            count *= ts.Count;
-            XTrace.WriteLine("生成 {0:n0}，耗时 {1}，速度 {2:n0}tps", count, sw.Elapsed, count * 1000 / sw.ElapsedMilliseconds);
-
-            Assert.True(sw.ElapsedMilliseconds < 20_000);
+                    var id = f.NewId();
+                }
+            }));
         }
 
-        [Fact]
-        public void GlobalWorkerId()
+        Task.WaitAll(ts.ToArray());
+
+        sw.Stop();
+
+        count *= ts.Count;
+        XTrace.WriteLine("生成 {0:n0}，耗时 {1}，速度 {2:n0}tps", count, sw.Elapsed, count * 1000 / sw.ElapsedMilliseconds);
+
+        Assert.True(sw.ElapsedMilliseconds < 20_000);
+    }
+
+    [Fact]
+    public void GlobalWorkerId()
+    {
         {
-            {
-                var n = Rand.Next(0x400);
-                Snowflake.GlobalWorkerId = n;
+            var n = Rand.Next(0x400);
+            Snowflake.GlobalWorkerId = n;
 
-                var sn = new Snowflake();
-                sn.NewId();
-                Assert.Equal(n, sn.WorkerId);
-            }
-            {
-                var n = Rand.Next(0x400, Int32.MaxValue);
-                Snowflake.GlobalWorkerId = n;
+            var sn = new Snowflake();
+            sn.NewId();
+            Assert.Equal(n, sn.WorkerId);
+        }
+        {
+            var n = Rand.Next(0x400, Int32.MaxValue);
+            Snowflake.GlobalWorkerId = n;
 
-                var sn = new Snowflake();
-                sn.NewId();
-                Assert.Equal(n & 0x3FF, sn.WorkerId);
-            }
+            var sn = new Snowflake();
+            sn.NewId();
+            Assert.Equal(n & 0x3FF, sn.WorkerId);
         }
     }
 }
