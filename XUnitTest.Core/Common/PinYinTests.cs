67f0b29c44aa6041a07869665b16b8183db3a203
大石头 <nnhy@vip.qq.com>
2024-01-11 20:19:31 +0800

    恢复2013/7/23排除掉的二级汉字库，让拼音库更完整

---
+++ XUnitTest.Core/Common/PinYinTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NewLife.Common;
+using Xunit;
+
+namespace XUnitTest.Common;
+
+public class PinYinTests
+{
+    [Fact]
+    public void Get()
+    {
+        var py = PinYin.Get('重');
+        //Assert.Equal("ChongQing", py);
+        Assert.Equal("Zhong", py);
+    }
+
+    [Fact]
+    public void Get2()
+    {
+        var py = PinYin.Get("重庆");
+        //Assert.Equal("ChongQing", py);
+        Assert.Equal("ZhongQing", py);
+    }
+
+    [Theory]
+    [InlineData("重庆", "ZhongQing")]
+    [InlineData("东莞", "DongGuan")]
+    [InlineData("畲江", "SheJiang")]
+    [InlineData("漯河", "LuoHe")]
+    public void GetAll(String name, String pinyin)
+    {
+        var py = PinYin.Get(name);
+        Assert.Equal(pinyin, py);
+    }
+}
