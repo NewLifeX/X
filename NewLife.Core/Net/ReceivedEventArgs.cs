f57392c834d3e179e7b483e84a29e1fba51761a6
大石头 <nnhy@vip.qq.com>
2024-01-19 11:22:09 +0800

    [feat] INetSession增加Connected/Disconnected事件

--- NewLife.Core/Net/ReceivedEventArgs.cs
+++ NewLife.Core/Net/ReceivedEventArgs.cs
@@ -2,23 +2,29 @@
 using System.Net;
 using NewLife.Data;
 
-namespace NewLife.Net
+namespace NewLife.Net;
+
+/// <summary>收到数据时的事件参数</summary>
+public class ReceivedEventArgs : EventArgs, IData
 {
-    /// <summary>收到数据时的事件参数</summary>
-    public class ReceivedEventArgs : EventArgs, IData
-    {
-        #region 属性
-        /// <summary>原始数据包</summary>
-        public Packet Packet { get; set; }
+    #region 属性
+    /// <summary>原始数据包</summary>
+    /// <remarks>
+    /// Packet内部的Data可能是网络缓冲区，并非全部数据都属于当前消息，需要ReadBytes得到有效数据。
+    /// </remarks>
+    public Packet? Packet { get; set; }
 
         /// <summary>远程地址</summary>
         public IPEndPoint Remote { get; set; }
 
-        /// <summary>解码后的消息</summary>
-        public Object Message { get; set; }
+    /// <summary>管道处理器解码后的消息，一般就是业务消息</summary>
+    public Object? Message { get; set; }
 
-        /// <summary>用户自定义数据</summary>
-        public Object UserState { get; set; }
-        #endregion
-    }
+    /// <summary>用户自定义数据</summary>
+    public Object? UserState { get; set; }
+    #endregion
+
+    /// <summary>获取当前事件的原始数据。避免用户错误使用Packet.Data</summary>
+    /// <returns></returns>
+    public Byte[]? GetBytes() => Packet?.ReadBytes();
 }
--- NewLife.Core/Net/Setting.cs
+++ NewLife.Core/Net/Setting.cs
@@ -17,29 +17,29 @@ public class SocketSetting : Config<SocketSetting>
     [Description("网络调试")]
     public Boolean Debug { get; set; }
 
-    /// <summary>会话超时时间。默认20*60秒</summary>
-    [Description("会话超时时间。默认20*60秒")]
+    /// <summary>会话超时时间。每个Tcp/Udp连接会话，超过一定时间不活跃时做超时下线处理，默认20*60秒</summary>
+    [Description("会话超时时间。每个Tcp/Udp连接会话，超过一定时间不活跃时做超时下线处理，默认20*60秒")]
     public Int32 SessionTimeout { get; set; } = 20 * 60;
 
-    /// <summary>缓冲区大小。默认8k</summary>
-    [Description("缓冲区大小。默认8k")]
+    /// <summary>缓冲区大小。每个IOCP异步接收缓冲区的大小，较大的值能减少小包合并，但是当连接数很多时会浪费大量内存，默认8k</summary>
+    [Description("缓冲区大小。每个IOCP异步接收缓冲区的大小，较大的值能减少小包合并，但是当连接数很多时会浪费大量内存，默认8k")]
     public Int32 BufferSize { get; set; } = 8 * 1024;
 
-    /// <summary>收发日志数据体长度。默认64</summary>
-    [Description("收发日志数据体长度。默认64")]
+    /// <summary>收发日志数据体长度。应用于LogSend/LogReceive时的数据HEX长度，默认64字节</summary>
+    [Description("收发日志数据体长度。应用于LogSend/LogReceive时的数据HEX长度，默认64字节")]
     public Int32 LogDataLength { get; set; } = 64;
 
-    /// <summary>启用Http压缩。内部新建的HttpClient将自动添加接受压缩的头部，并在响应中对压缩进行解码，默认true</summary>
-    [Description("启用Http压缩。内部新建的HttpClient将自动添加接受压缩的头部，并在响应中对压缩进行解码，默认true")]
-    public Boolean EnableHttpCompression { get; set; } = true;
+    ///// <summary>启用Http压缩。内部新建的HttpClient将自动添加接受压缩的头部，并在响应中对压缩进行解码，默认true</summary>
+    //[Description("启用Http压缩。内部新建的HttpClient将自动添加接受压缩的头部，并在响应中对压缩进行解码，默认true")]
+    //public Boolean EnableHttpCompression { get; set; } = true;
 
-    /// <summary>自动启用GZip压缩的请求体大小。默认1024，用0表示不压缩</summary>
-    [Description("自动启用GZip压缩的请求体大小。默认1024，用0表示不压缩")]
+    /// <summary>自动启用GZip压缩的请求体大小。应用于HttpHelper/ApiHttpClient发起的请求，默认1024，用0表示不压缩</summary>
+    [Description("自动启用GZip压缩的请求体大小。应用于HttpHelper/ApiHttpClient发起的请求，默认1024，用0表示不压缩")]
     public Int32 AutoGZip { get; set; } = 1024;
     #endregion
 
     #region 方法
-    /// <summary>实例化</summary>
-    public SocketSetting() { }
+    ///// <summary>实例化</summary>
+    //public SocketSetting() { }
     #endregion
 }
