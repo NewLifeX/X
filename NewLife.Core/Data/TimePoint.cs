c5506b0b4e234a40f665bb8c422136755e443231
智能大石头 <nnhy@vip.qq.com>
2024-03-29 00:18:39 +0800

    优化CreateInstance创建对象，支持列表和字典

--- NewLife.Core/Data/TimePoint.cs
+++ NewLife.Core/Data/TimePoint.cs
@@ -1,42 +1,41 @@
 ﻿using System;
 
-namespace NewLife.Data
+namespace NewLife.Data;
+
+/// <summary>
+/// 时序点，用于时序数据计算
+/// </summary>
+public struct TimePoint
 {
     /// <summary>
-    /// 时序点，用于时序数据计算
+    /// 时间
     /// </summary>
-    public struct TimePoint
-    {
-        /// <summary>
-        /// 时间
-        /// </summary>
-        public Int64 Time;
+    public Int64 Time;
 
-        /// <summary>
-        /// 数值
-        /// </summary>
-        public Double Value;
+    /// <summary>
+    /// 数值
+    /// </summary>
+    public Double Value;
 
-        /// <summary>
-        /// 已重载
-        /// </summary>
-        /// <returns></returns>
-        public override String ToString() => $"({Time}, {Value})";
-    }
-
-    ///// <summary>
-    ///// 时序点，用于时序数据计算
-    ///// </summary>
-    //public struct LongTimePoint
-    //{
-    //    /// <summary>
-    //    /// 时间
-    //    /// </summary>
-    //    public Int64 Time;
-
-    //    /// <summary>
-    //    /// 数值
-    //    /// </summary>
-    //    public Double Value;
-    //}
+    /// <summary>
+    /// 已重载
+    /// </summary>
+    /// <returns></returns>
+    public override String ToString() => $"({Time}, {Value})";
 }
+
+///// <summary>
+///// 时序点，用于时序数据计算
+///// </summary>
+//public struct LongTimePoint
+//{
+//    /// <summary>
+//    /// 时间
+//    /// </summary>
+//    public Int64 Time;
+
+//    /// <summary>
+//    /// 数值
+//    /// </summary>
+//    public Double Value;
+//}
