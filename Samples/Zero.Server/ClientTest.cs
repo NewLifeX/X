b4d9bc0cf806f4597c0c9d6b6775499e0b6c6689
石头 <nnhy@vip.qq.com>
2024-04-21 00:30:18 +0800

    增加NetServer示例项目，同时可作为新项目模版

---
+++ Samples/Zero.Server/ClientTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using NewLife;
+using NewLife.Log;
+
+namespace Zero.Server;
+
+static class ClientTest
+{
+    public static async void TcpClientTest()
+    {
+        await Task.Delay(1_000);
+
+        var client = new TcpClient();
+        await client.ConnectAsync("127.0.0.1", 12345);
+        var ns = client.GetStream();
+
+        // 接收服务端握手
+        var buf = new Byte[1024];
+        var count = await ns.ReadAsync(buf);
+        XTrace.WriteLine("<={0}", buf.ToStr(null, 0, count));
+
+        // 发送数据
+        var str = "Hello NewLife";
+        XTrace.WriteLine("=>{0}", str);
+        await ns.WriteAsync(str.GetBytes());
+
+        // 接收数据
+        count = await ns.ReadAsync(buf);
+        XTrace.WriteLine("<={0}", buf.ToStr(null, 0, count));
+    }
+
+    public static async void UdpClientTest()
+    {
+        await Task.Delay(1_500);
+
+        var client = new UdpClient();
+        client.Connect("127.0.0.1", 12345);
+
+        // 发送数据
+        var str = "Hello NewLife";
+        XTrace.WriteLine("=>{0}", str);
+        await client.SendAsync(str.GetBytes());
+
+        // 接收数据
+        var result = await client.ReceiveAsync();
+        XTrace.WriteLine("<={0}", result.Buffer.ToStr());
+    }
+}
