3cc8e2f7148c34ce23d26506560962d779790099
智能大石头 <nnhy@vip.qq.com>
2024-04-28 00:11:25 +0800

    WebSocket客户端服务端增加心跳PingPong支持，默认120秒

--- Samples/Zero.HttpServer/ClientTest.cs
+++ Samples/Zero.HttpServer/ClientTest.cs
@@ -79,6 +79,8 @@ static class ClientTest
         var rs = await client.ReceiveMessageAsync(default);
         client.WriteLog(rs.Payload.ToStr());
 
+        await TaskEx.Delay(6_000);
+
         // 关闭连接
         await client.CloseAsync(1000, "通信完成", default);
         client.WriteLog("Close");
