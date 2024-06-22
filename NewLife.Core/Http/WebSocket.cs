ec373c36cb64608b4d163108a5f0c221d40cf77e
石头 <nnhy@vip.qq.com>
2024-06-22 11:10:50 +0800

    [fix]WebSocket

--- NewLife.Core/Http/WebSocket.cs
+++ NewLife.Core/Http/WebSocket.cs
@@ -1,9 +1,10 @@
 ﻿using System.Net;
 using System.Security.Cryptography;
-using NewLife;
 using NewLife.Data;
-using NewLife.Http;
 using NewLife.Net;
+using NewLife.Security;
+
+namespace NewLife.Http;
 
 /// <summary>WebSocket消息处理</summary>
 /// <param name="socket"></param>
@@ -18,13 +19,13 @@ public class WebSocket
     public Boolean Connected { get; set; }
 
     /// <summary>消息处理器</summary>
-    public WebSocketDelegate Handler { get; set; }
+    public WebSocketDelegate? Handler { get; set; }
 
     /// <summary>Http上下文</summary>
-    public IHttpContext Context { get; set; }
+    public IHttpContext? Context { get; set; }
 
     /// <summary>版本</summary>
-    public String Version { get; set; }
+    public String? Version { get; set; }
 
     /// <summary>活跃时间</summary>
     public DateTime ActiveTime { get; set; }
@@ -34,7 +35,7 @@ public class WebSocket
     /// <summary>WebSocket 握手</summary>
     /// <param name="context"></param>
     /// <returns></returns>
-    public static WebSocket Handshake(IHttpContext context)
+    public static WebSocket? Handshake(IHttpContext context)
     {
         var request = context.Request;
         if (!request.Headers.TryGetValue("Sec-WebSocket-Key", out var key) || key.IsNullOrEmpty()) return null;
@@ -129,17 +130,17 @@ public class WebSocket
     /// <param name="data"></param>
     /// <param name="type"></param>
     /// <param name="predicate"></param>
-    public void SendAll(Packet data, WebSocketMessageType type, Func<INetSession, Boolean> predicate = null)
+    public void SendAll(Packet data, WebSocketMessageType type, Func<INetSession, Boolean>? predicate = null)
     {
+        var session = (Context?.Connection) ?? throw new ObjectDisposedException(nameof(Context));
         var msg = new WebSocketMessage { Type = type, Payload = data };
-        var session = Context.Connection;
         session.Host.SendAllAsync(msg.ToPacket(), predicate);
     }
 
     /// <summary>想所有连接发送文本消息</summary>
     /// <param name="message"></param>
     /// <param name="predicate"></param>
-    public void SendAll(String message, Func<INetSession, Boolean> predicate = null) => SendAll(message.GetBytes(), WebSocketMessageType.Text, predicate);
+    public void SendAll(String message, Func<INetSession, Boolean>? predicate = null) => SendAll(message.GetBytes(), WebSocketMessageType.Text, predicate);
 
     /// <summary>发送关闭连接</summary>
     /// <param name="closeStatus"></param>
