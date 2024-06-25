4eef6a02d3402675ad21b51fcf1b7db46ef6f75d
大石头 <nnhy@vip.qq.com>
2024-06-25 16:49:36 +0800

    [fix]GetNext

--- NewLife.Core/Threading/Cron.cs
+++ NewLife.Core/Threading/Cron.cs
@@ -329,7 +329,7 @@ public class Cron
         {
             var cron = new Cron(item);
             var dt = cron.GetNext(time);
-            if (dt < next) dt = next;
+            if (dt < next) next = dt;
         }
         return next;
     }
@@ -345,7 +345,7 @@ public class Cron
         {
             var cron = new Cron(item);
             var dt = cron.GetPrevious(time);
-            if (dt > prev) dt = prev;
+            if (dt > prev) prev = dt;
         }
         return prev;
     }
