f75dfce462314e74313aa762ec4ef715a85028d9
大石头 <nnhy@vip.qq.com>
2024-07-19 15:40:54 +0800

    [fix]配置文件损坏时，要能够忽略错误，强行加载，避免影响系统正常运行

--- NewLife.Core/Configuration/Config.cs
+++ NewLife.Core/Configuration/Config.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.Serialization;
 using System.Xml.Serialization;
+using NewLife.Log;
 
 namespace NewLife.Configuration;
 
@@ -61,14 +62,36 @@ public class Config<TConfig> where TConfig : Config<TConfig>, new()
                 var config = new TConfig();
                 var prv = Provider;
 
-                // 绑定提供者数据到配置对象
-                prv.Bind(config, true);
+                // 配置文件损坏时，要能够忽略错误，强行加载，避免影响系统正常运行
+                try
+                {
+                    // 绑定提供者数据到配置对象
+                    prv.Bind(config, true);
+                }
+                catch (Exception ex)
+                {
+                    XTrace.Log?.Error(ex.Message);
+                }
 
-                config.OnLoaded();
+                try
+                {
+                    config.OnLoaded();
+                }
+                catch (Exception ex)
+                {
+                    XTrace.Log?.Error(ex.Message);
+                }
 
-                // OnLoad 中可能有变化，存回去
-                //prv.Save(config);
-                config.Save();
+                try
+                {
+                    // OnLoad 中可能有变化，存回去
+                    //prv.Save(config);
+                    config.Save();
+                }
+                catch (Exception ex)
+                {
+                    XTrace.Log?.Error(ex.Message);
+                }
 
                 return _Current = config;
             }
