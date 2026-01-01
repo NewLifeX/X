1866eca0319cbfb8ff4a8da2ca901aabda579f31
石头 <nnhy@vip.qq.com>
2026-01-02 01:10:08 +0800

    对象存储接口异步化与元数据增强
    本次提交对对象存储相关接口和实现进行了大幅增强，全面引入异步方法（如 GetAsync、PutAsync、DeleteAsync、SearchAsync 等），并新增了 ExistsAsync、CopyAsync、批量删除等能力。IObjectInfo 增加了 ContentType、Hash、Url、RawUrl 等属性，丰富了文件元数据。所有方法注释和文档化显著提升，原有同步方法保留但已标记过时。此改动提升了接口的功能性、类型安全性和现代云存储适配能力。

--- NewLife.Core/IO/IObjectStorage.cs
+++ NewLife.Core/IO/IObjectStorage.cs
@@ -29,35 +29,89 @@ public interface IObjectStorage
 
     /// <summary>是否支持搜索</summary>
     Boolean CanSearch { get; }
+
+    /// <summary>是否支持复制</summary>
+    Boolean CanCopy { get; }
     #endregion
 
     #region 方法
     /// <summary>获取文件对象</summary>
-    /// <param name="id">对象文件名</param>
-    /// <returns></returns>
+    /// <param name="id">对象文件名，支持斜杠目录结构</param>
+    /// <returns>文件对象信息，不存在时返回null</returns>
+    Task<IObjectInfo?> GetAsync(String id);
+
+    /// <summary>获取文件直接访问Url</summary>
+    /// <param name="id">对象文件名，支持斜杠目录结构</param>
+    /// <returns>可直接访问的Url地址</returns>
+    Task<String?> GetUrlAsync(String id);
+
+    /// <summary>检查文件是否存在</summary>
+    /// <param name="id">对象文件名，支持斜杠目录结构</param>
+    /// <returns>存在返回true，不存在返回false</returns>
+    Task<Boolean> ExistsAsync(String id);
+
+    /// <summary>上传文件对象</summary>
+    /// <param name="id">对象文件名，可以为空，此时自动生成文件名。支持斜杠目录结构</param>
+    /// <param name="data">数据内容</param>
+    /// <returns>文件对象信息，可能包含自动生成的新文件名</returns>
+    Task<IObjectInfo?> PutAsync(String id, IPacket data);
+
+    /// <summary>删除文件对象</summary>
+    /// <param name="id">对象文件名，支持斜杠目录结构</param>
+    /// <returns>删除成功的数量</returns>
+    Task<Int32> DeleteAsync(String id);
+
+    /// <summary>批量删除文件对象</summary>
+    /// <param name="ids">对象文件名列表</param>
+    /// <returns>删除成功的数量</returns>
+    Task<Int32> DeleteAsync(String[] ids);
+
+    /// <summary>复制文件对象</summary>
+    /// <param name="sourceId">源对象文件名</param>
+    /// <param name="destId">目标对象文件名</param>
+    /// <returns>复制后的文件对象信息</returns>
+    Task<IObjectInfo?> CopyAsync(String sourceId, String destId);
+
+    /// <summary>搜索文件</summary>
+    /// <param name="pattern">匹配模式。如/202304/*.jpg</param>
+    /// <param name="start">开始序号，0开始</param>
+    /// <param name="count">最大返回个数</param>
+    /// <returns>文件对象信息列表</returns>
+    Task<IList<IObjectInfo>?> SearchAsync(String? pattern = null, Int32 start = 0, Int32 count = 100);
+    #endregion
+
+    #region 兼容旧版
+    /// <summary>获取文件对象</summary>
+    /// <param name="id">对象文件名，支持斜杠目录结构</param>
+    /// <returns>文件对象信息，不存在时返回null</returns>
+    [Obsolete("请使用 GetAsync")]
     Task<IObjectInfo?> Get(String id);
 
     /// <summary>获取文件直接访问Url</summary>
-    /// <param name="id">对象文件名</param>
-    /// <returns></returns>
+    /// <param name="id">对象文件名，支持斜杠目录结构</param>
+    /// <returns>可直接访问的Url地址</returns>
+    [Obsolete("请使用 GetUrlAsync")]
     Task<String?> GetUrl(String id);
 
     /// <summary>上传文件对象</summary>
-    /// <param name="id">对象文件名。可以为空，此时自动生成文件名</param>
+    /// <param name="id">对象文件名，可以为空，此时自动生成文件名。支持斜杠目录结构</param>
     /// <param name="data">数据内容</param>
-    /// <returns>可能是自动生成的新文件名</returns>
+    /// <returns>文件对象信息，可能包含自动生成的新文件名</returns>
+    [Obsolete("请使用 PutAsync")]
     Task<IObjectInfo?> Put(String id, IPacket data);
 
     /// <summary>删除文件对象</summary>
-    /// <param name="id">对象文件名</param>
-    /// <returns></returns>
+    /// <param name="id">对象文件名，支持斜杠目录结构</param>
+    /// <returns>删除成功的数量</returns>
+    [Obsolete("请使用 DeleteAsync")]
     Task<Int32> Delete(String id);
 
     /// <summary>搜索文件</summary>
     /// <param name="pattern">匹配模式。如/202304/*.jpg</param>
-    /// <param name="start">开始序号。0开始</param>
-    /// <param name="count">最大个数</param>
-    /// <returns></returns>
+    /// <param name="start">开始序号，0开始</param>
+    /// <param name="count">最大返回个数</param>
+    /// <returns>文件对象信息列表</returns>
+    [Obsolete("请使用 SearchAsync")]
     Task<IList<IObjectInfo>?> Search(String? pattern = null, Int32 start = 0, Int32 count = 100);
     #endregion
 }
