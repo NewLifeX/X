033e1aebeb24ee0bd12705ef99e7d5ef4a691fa3
石头 <nnhy@vip.qq.com>
2026-05-16 19:04:48 +0800

    高版本框架补充类型转发，消除类型冲突
    为 netstandard2.1/netcoreapp3.1/net5+ 增加异步接口、Nullable 注解、MemberNotNull/IsExternalInit 等类型的 TypeForwardedTo 声明，确保低版本类库在高版本环境下类型一致，避免 TypeLoadException。

--- NewLife.Core/Stub/AsyncInterfacesForwards.cs
+++ NewLife.Core/Stub/AsyncInterfacesForwards.cs
@@ -1,18 +1,21 @@
 #if !NETFRAMEWORK && !NETSTANDARD2_0
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Sources;
 
-// 将 Stub 文件（NETFRAMEWORK / NETSTANDARD2_0）中直接定义的异步接口类型，
+// 将 Stub 文件（NETFRAMEWORK / NETSTANDARD2_0）中直接定义的类型，
 // 在高版本框架（netstandard2.1 / netcoreapp3.1 / net5+）的程序集中转发到 BCL。
 //
 // 背景：
 //   当一个 netstandard2.0 类库（AppLib）引用 NewLife.Core，
-//   编译后 AppLib.dll 的元数据里，IAsyncEnumerable<T> 等类型的程序集引用指向 "NewLife.Core"。
+//   编译后 AppLib.dll 的元数据里，这些类型的程序集引用指向 "NewLife.Core"。
 //   若该 AppLib 在 net10 应用（AppWeb）中运行，CLR 会加载 NewLife.Core (net10) 版本；
 //   若 net10 版本中找不到这些类型，就会抛出 TypeLoadException。
 //   此文件通过 TypeForwardedTo 将查找请求重定向到 BCL，彻底消除类型标识冲突。
+
+// 异步接口（netstandard2.1 / netcoreapp3.0+ / net5+）
 [assembly: TypeForwardedTo(typeof(IAsyncDisposable))]
 [assembly: TypeForwardedTo(typeof(IAsyncEnumerable<>))]
 [assembly: TypeForwardedTo(typeof(IAsyncEnumerator<>))]
@@ -23,4 +26,19 @@ using System.Threading.Tasks.Sources;
 [assembly: TypeForwardedTo(typeof(EnumeratorCancellationAttribute))]
 [assembly: TypeForwardedTo(typeof(TaskAsyncEnumerableExtensions))]
 [assembly: TypeForwardedTo(typeof(ManualResetValueTaskSourceCore<>))]
+
+// Nullable 注解属性（netstandard2.1 / netcoreapp3.0+）
+[assembly: TypeForwardedTo(typeof(MaybeNullAttribute))]
+[assembly: TypeForwardedTo(typeof(MaybeNullWhenAttribute))]
+[assembly: TypeForwardedTo(typeof(NotNullAttribute))]
+[assembly: TypeForwardedTo(typeof(NotNullIfNotNullAttribute))]
+[assembly: TypeForwardedTo(typeof(NotNullWhenAttribute))]
+#endif
+
+#if NET5_0_OR_GREATER
+// MemberNotNull / MemberNotNullWhen / IsExternalInit 从 .NET 5 起才进入 BCL，
+// 对应 Stub 守卫为 NETFRAMEWORK || NETSTANDARD || NETCOREAPP3_1
+[assembly: TypeForwardedTo(typeof(System.Diagnostics.CodeAnalysis.MemberNotNullAttribute))]
+[assembly: TypeForwardedTo(typeof(System.Diagnostics.CodeAnalysis.MemberNotNullWhenAttribute))]
+[assembly: TypeForwardedTo(typeof(System.Runtime.CompilerServices.IsExternalInit))]
 #endif
